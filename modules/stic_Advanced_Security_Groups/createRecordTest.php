d7bf68fa8d79733fe4a08e6d5b2a074a790fd261
juanSTIC <juan@sinergiacrm.org>
2024-01-03 11:49:56 +0100

    test file (for deletion after)

--- modules/stic_Advanced_Security_Groups/createRecordTest.php
+++ modules/stic_Advanced_Security_Groups/createRecordTest.php
@@ -1,8 +1,11 @@
 <?php
 global $current_user;
-$bean=BeanFactory::newBean($_REQUEST['modulo']);
-$bean->name=rand();
-$bean->assigned_user_id=2;
-$bean->save();
+$bean = BeanFactory::newBean($_REQUEST['modulo']);
+$bean->set_created_by=false;
 
+$bean->name = rand();
+$bean->assigned_user_id = 2;
+$bean->created_by = 1;
+$bean->save();
+// var_dump($bean);die();
 SugarApplication::redirect("index.php?module={$_REQUEST['modulo']}&action=detailview&record={$bean->id}");
