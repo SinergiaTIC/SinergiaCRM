b47e92abb8965355228d06efc1aae6783883ed99
juanSTIC <juan@sinergiacrm.org>
2024-01-04 16:25:40 +0100

    documentation & format

--- custom/modules/SecurityGroups/CustomSecurityGroupsInherit.php
+++ custom/modules/SecurityGroups/CustomSecurityGroupsInherit.php
@@ -2,30 +2,42 @@
 class CustomSecurityGroupsInherit
 {
 
+    /**
+     * after_save function for SuiteCRM, handling security group inheritance.
+     * This function applies security group inheritance rules to newly created records
+     * based on conditions such as the assigned user, creator, and parent records.
+     *
+     * @param SugarBean $bean The current bean being processed.
+     * @param string $event The event that triggers the function.
+     * @param array $arguments Additional arguments passed to the function.
+     */
     public function after_save(&$bean, $event, $arguments)
     {
-
+        // Skip processing for the 'SugarFeed' module
         if (in_array($bean->module_name, ['SugarFeed'])) {
             return;
         }
 
+        // Process only if it's a new record
         if (!$bean->fetched_row) {
 
+            // Retrieve module-specific inheritance rules
             $rulesBean = self::getModuleRule($bean->module_name);
 
-            // Comprobamos si hay una regla definida y activa de herencia para el módulo.
+            // Check if there is a defined and active inheritance rule for the module
             if (!empty($rulesBean) && $rulesBean->active == 1) {
 
+                // Initialize an array to store candidate security groups for inheritance
                 $securityGroupsCandidatesToInherit = [];
 
+                // Include necessary modules for security group processing
                 require_once 'modules/SecurityGroups/SecurityGroup.php';
                 require_once 'SticInclude/Utils.php';
                 require_once 'modules/stic_Advanced_Security_Groups/Utils.php';
 
-                // Get security groups for assigned user if enabled
+                // Inherit security groups from the assigned user, if enabled
                 if ($rulesBean->inherit_assigned == 1) {
                     $userGroups = SecurityGroup::getUserSecurityGroups($bean->assigned_user_id);
-
                     if (!empty($userGroups)) {
                         foreach ($userGroups as $group) {
                             $securityGroupsCandidatesToInherit = array_merge(
@@ -36,10 +48,9 @@ class CustomSecurityGroupsInherit
                     }
                 }
 
-                // Get security groups for creator user if enabled
+                // Inherit security groups from the creator user, if enabled
                 if ($rulesBean->inherit_creator == 1) {
                     $userGroups = SecurityGroup::getUserSecurityGroups($bean->created_by);
-
                     if (!empty($userGroups)) {
                         foreach ($userGroups as $group) {
                             $securityGroupsCandidatesToInherit = array_merge(
@@ -50,7 +61,7 @@ class CustomSecurityGroupsInherit
                     }
                 }
 
-                // get security groups for parent records if the inheritance of the parent is enabled (for all modules or only for some modules)
+                // Inherit security groups from parent records, if enabled
                 $allRelatedModules = stic_Advanced_Security_GroupsUtils::getRelatedModulesList($bean->module_dir, 'module_names');
                 $filteredRelatedModules = unencodeMultienum($rulesBean->inherit_from_modules);
                 foreach ($allRelatedModules as $value) {
@@ -63,20 +74,19 @@ class CustomSecurityGroupsInherit
                                     [['record_id' => $bean->id, 'securitygroup_id' => $val2]]
                                 );
                             }
-
                         }
                     }
                 }
 
-                // Creamos un array con los grupos que no son heredables en ningún caso para el módulo actual
+                // Create an array of security groups that are not inheritable under any circumstances for the current module
                 $notInheritableGroups = unencodeMultienum($rulesBean->non_inherit_from_security_groups);
-                // Añadimos los grupos de seguridad definidos como NO heredables de forma global
-                $result = $bean->db->query("SELECT id FROM securitygroups where deleted=0 and noninheritable=1");
+                // Add globally defined non-inheritable security groups
+                $result = $bean->db->query("SELECT id FROM securitygroups WHERE deleted=0 AND noninheritable=1");
                 foreach ($result as $row) {
                     $notInheritableGroups[] = $row['id'];
                 }
 
-                // Añadimos los grupos de seguridad al registro, exluyendo los no heredables
+                // Add security groups to the record, excluding the non-inheritable ones
                 $securityGroupBean = BeanFactory::newBean('SecurityGroups');
                 foreach ($securityGroupsCandidatesToInherit as $key => $item) {
                     if (!in_array($item['securitygroup_id'], $notInheritableGroups)) {
@@ -84,75 +94,77 @@ class CustomSecurityGroupsInherit
                     }
                 }
 
-                // Añadimos los grupos por defecto definidos en la configuración general para el módulo
-                SecurityGroup::assign_default_groups($bean, $false);
-
-                // This code is only for develop purposes and must be comment in production
-                // foreach ($securityGroupsCandidatesToInherit as $key => $sg) {
-                //     $securityGroupsCandidatesToInherit[$key]['grupo'] = $bean->db->getOne("SELECT name from securitygroups WHERE id='{$sg['securitygroup_id']}'");
-                // }
-                // var_dump('$securityGroupsCandidatesToInherit', $securityGroupsCandidatesToInherit);
-                // var_dump('$notInheritableGroups', $notInheritableGroups);
-                // die();
+                // Assign default groups defined in the general configuration for the module
+                SecurityGroup::assign_default_groups($bean, false);
 
             }
         }
     }
 
-
     /**
- * Retrieves a list of security group IDs associated with a given record ID.
- *
- * This function executes a database query to fetch distinct security group IDs from the 
- * 'securitygroups_records' and 'securitygroups' tables. It selects IDs where the record's 
- * ID matches the specified 'relatedRecordID', and where both 'deleted' and 'noninheritable' flags are set to 0.
- *
- * @param int|string $relatedRecordID The ID of the record for which related security group IDs are to be fetched.
- * @return array An array of security group IDs associated with the specified record ID.
- * @global object $databaseConnection A global database connection object used to execute the query.
- */
-public static function getRelatedSecurityGroupIDs($relatedRecordID)
-{
-    global $db;
-    $securityGroupIDs = [];
-    $queryResult = $db->query("SELECT DISTINCT securitygroup_id
+     * Retrieves a list of security group IDs associated with a given record ID.
+     *
+     * This function executes a database query to fetch distinct security group IDs from the
+     * 'securitygroups_records' and 'securitygroups' tables. It selects IDs where the record's
+     * ID matches the specified 'relatedRecordID', and where both 'deleted' and 'noninheritable' flags are set to 0.
+     *
+     * @param int|string $relatedRecordID The ID of the record for which related security group IDs are to be fetched.
+     * @return array An array of security group IDs associated with the specified record ID.
+     * @global object $databaseConnection A global database connection object used to execute the query.
+     */
+    public static function getRelatedSecurityGroupIDs($relatedRecordID)
+    {
+        global $db;
+        $securityGroupIDs = [];
+        $queryResult = $db->query("SELECT DISTINCT securitygroup_id
                                                FROM securitygroups_records
                                                LEFT JOIN securitygroups ON securitygroups_records.securitygroup_id = securitygroups.id
                                                WHERE securitygroups_records.record_id = '{$relatedRecordID}'
                                                AND securitygroups_records.deleted = 0
                                                AND securitygroups.noninheritable = 0");
 
-    foreach ($queryResult as $row) {
-        $securityGroupIDs[] = $row['securitygroup_id'];
+        foreach ($queryResult as $row) {
+            $securityGroupIDs[] = $row['securitygroup_id'];
+        }
+
+        return $securityGroupIDs;
     }
 
-    return $securityGroupIDs;
-}
-    
-
-
-
     /**
-     * Obtener la regla definida para el módulo
+     * Retrieve the defined rule for a specified module.
      *
-     * @param String $moduleName
-     * @return Object
+     * This method searches the database for a rule associated with a module
+     * and returns a bean with the rule configuration.
+     *
+     * @param String $moduleName The name of the module for which the rule is sought.
+     * @return Object The bean of the found rule, or null if not found.
      */
     public static function getModuleRule($moduleName)
     {
         global $db;
+        // Query to obtain the rule ID based on the module name
         $ruleId = $db->getOne("SELECT id FROM stic_advanced_security_groups WHERE name='{$moduleName}' AND deleted=0 AND active=true");
+
+        // Retrieve and return the rule bean using the obtained ID
         $rulesBean = BeanFactory::getBean('stic_Advanced_Security_Groups', $ruleId);
         return $rulesBean;
-
     }
 
-    public static function checkrelatedField($relatedID, $bean)
+    /**
+     * Verify and obtain the related field for a specific bean.
+     *
+     * This method retrieves the value of a related field of a specific bean
+     * based on its ID. It is useful for verifying relationships in SuiteCRM.
+     *
+     * @param String $relatedID The ID of the related field to be verified.
+     * @param Object $bean The bean for which the related field is sought.
+     * @return String The value of the related field, or an empty string if not found.
+     */
+    public static function checkRelatedField($relatedID, $bean)
     {
         global $db;
-        $relID = '';
+        // Query to obtain the value of the related field based on the bean's ID
         $relID = $db->getOne("SELECT {$relatedID} FROM contacts_cstm WHERE id_c='{$bean->id}' LIMIT 1");
-        //$relID = $db->getOne("SELECT id FROM securitygroups WHERE id='{$relatedID}' AND deleted=0 LIMIT 1");
 
         return $relID;
     }
--- modules/stic_Advanced_Security_Groups/Utils.php
+++ modules/stic_Advanced_Security_Groups/Utils.php
@@ -1,20 +1,21 @@
- <?php
+<?php
+// Including necessary dependencies
 global $current_user, $app_strings;
+require_once 'modules/SecurityGroups/SecurityGroup.php';
+require_once 'modules/MySettings/TabController.php';
 
+// Checking if the current user is an administrator
 if (!is_admin($current_user)) {
     sugar_die($app_strings['ERR_NOT_ADMIN']);
 }
 
-require_once 'modules/SecurityGroups/SecurityGroup.php';
-require_once 'modules/MySettings/TabController.php';
-
+// Class stic_Advanced_Security_GroupsUtils
 class stic_Advanced_Security_GroupsUtils
 {
-
     /**
-     * Genera y devuelve un array con los módulos filtrados.
+     * Generates and returns an array with filtered modules.
      *
-     * @return array Array de módulos filtrados.
+     * @return array Array of filtered modules.
      */
     public static function generateFilteredModuleArray()
     {
@@ -22,6 +23,7 @@ class stic_Advanced_Security_GroupsUtils
         $systemTabs = TabController::get_system_tabs();
         $resArray = array();
 
+        // Filtering modules based on specific criteria
         foreach ($app_list_strings['moduleList'] as $key => $val) {
             if (!in_array($key, $GLOBALS['modInvisList']) && in_array($key, $systemTabs) && $key != 'Home') {
                 $resArray[$key] = $val;
@@ -34,23 +36,22 @@ class stic_Advanced_Security_GroupsUtils
         return $resArray;
     }
 
+    /**
+     * Sets the custom filtered module list.
+     */
     public static function setCustomFilteredModuleList()
     {
         $resArray = self::generateFilteredModuleArray();
-
-        // Nombre de la lista que se generará
         $overridedListName = 'dynamic_filtered_module_list';
-
-        // Población de la lista global con los módulos filtrados y ordenados
         global $app_list_strings;
         $app_list_strings[$overridedListName] = $resArray;
     }
 
     /**
-     * Genera y devuelve un array con opciones de módulos relacionados.
+     * Generates and returns an array with related module options.
      *
-     * @param string $mainModule El módulo principal.
-     * @return array devuelve un array por cada módulo relacionado, que inclutye el nombre del módulo, el nombre de la relación, el campo relacionado y la etiqueta traducida de la relación.
+     * @param string $mainModule The main module.
+     * @return array Returns an array for each related module, including the module name, relationship name, related field, and the translated label of the relationship.
      */
     public static function getRelatedModulesList($mainModule)
     {
@@ -60,11 +61,20 @@ class stic_Advanced_Security_GroupsUtils
         if (!empty($mainModuleBean)) {
             foreach ($mainModuleBean->getFieldDefinitions() as $val) {
                 if (isset($val['type']) && $val['type'] === 'relate' && ($val['ext2'] && $val['module'] !== 'SecurityGroups') && $val['id_name']) {
-                    $options[] = ['relationship' => $mainModule . $val['id_name'], 'field' => $val['id_name'], 'module' => $val['module'], 'label' => translate($val['vname'], $mainModule)];
-
+                    $options[] = [
+                        'relationship' => $mainModule . $val['id_name'],
+                        'field' => $val['id_name'],
+                        'module' => $val['module'],
+                        'label' => translate($val['vname'], $mainModule)
+                    ];
                 } else if (isset($val['type']) && in_array($val['type'], ['link', 'relate']) && !in_array($val['module'], ['SecurityGroups', 'Users'])) {
                     if (!empty($val['link'])) {
-                        $options[] = ['relationship' => $val['link'], 'field' => $val['id_name'], 'module' => $val['module'], 'label' => translate($val['vname'], $mainModule)];
+                        $options[] = [
+                            'relationship' => $val['link'],
+                            'field' => $val['id_name'],
+                            'module' => $val['module'],
+                            'label' => translate($val['vname'], $mainModule)
+                        ];
                     }
                 }
             }
@@ -74,18 +84,14 @@ class stic_Advanced_Security_GroupsUtils
     }
 
     /**
-     * Establece la lista personalizada de módulos relacionados.
+     * Sets the custom related module list.
      *
-     * @param string $mainModule El módulo principal.
+     * @param string $mainModule The main module.
      */
     public static function setCustomRelatedModuleList($mainModule)
     {
         $options = array_column(self::getRelatedModulesList($mainModule), 'label', 'relationship');
-
-        // Nombre de la lista que se generará
         $overridedListName = 'dynamic_related_module_list';
-
-        // Población de la lista global con las opciones
         global $app_list_strings;
         $app_list_strings[$overridedListName] = $options;
     }
@@ -98,25 +104,15 @@ class stic_Advanced_Security_GroupsUtils
     public static function setAllRelatedModuleList()
     {
         $systemTabs = TabController::get_system_tabs();
-        foreach ($systemTabs as $key => $value) {
-
-        }
         $options = array();
-        foreach ($systemTabs as $key => $mainModule) {
 
+        foreach ($systemTabs as $key => $mainModule) {
             $mainModuleBean = BeanFactory::newBean($mainModule);
             if (!empty($mainModuleBean)) {
                 foreach ($mainModuleBean->getFieldDefinitions() as $val) {
-                    // Add to options if the field is a 'relate' type and relates to SecurityGroups
                     if (isset($val['type']) && $val['type'] === 'relate' && ($val['ext2'] && $val['module'] !== 'SecurityGroups') && $val['id_name']) {
                         $options[$mainModule . $val['id_name']] = translate($val['vname'], $mainModule);
-                        if (empty($val['id_name'])) {
-                            echo '';
-                        }
-                    }
-
-                    // Add other related or linked modules, excluding SecurityGroups and Users
-                    else if (isset($val['type']) && in_array($val['type'], ['link', 'relate']) && !in_array($val['module'], ['SecurityGroups', 'Users'])) {
+                    } else if (isset($val['type']) && in_array($val['type'], ['link', 'relate']) && !in_array($val['module'], ['SecurityGroups', 'Users'])) {
                         if (!empty($val['link'])) {
                             $options[$val['link']] = translate($val['vname'], $mainModule);
                         }
@@ -126,11 +122,7 @@ class stic_Advanced_Security_GroupsUtils
         }
 
         global $app_list_strings;
-
-        // Name of the list to be generated
         $overridedListName = 'dynamic_related_module_list';
-
-        // Populate the global list with the options
         $app_list_strings[$overridedListName] = $options;
     }
 
@@ -142,25 +134,15 @@ class stic_Advanced_Security_GroupsUtils
     public static function setCustomSecurityGroupList()
     {
         global $db, $app_list_strings;
-
-        // Name of the list to be generated
         $overridedListName = 'dynamic_security_group_list';
-
-        // SQL query to retrieve active security groups
         $sql = "SELECT id, name as 'value' FROM securitygroups WHERE deleted = 0";
 
-        // Clear any existing values in the list
         unset($app_list_strings[$overridedListName]);
-
-        // Execute the query and build the list
         $result = $db->query($sql);
         
-        // Initialize the list with an empty option
         $app_list_strings[$overridedListName][''] = '';
         while ($row = $db->fetchByAssoc($result)) {
-            // Populate the list with security group ID and name
             $app_list_strings[$overridedListName][$row['id']] = $row['value'];
         }
     }
-
 }
