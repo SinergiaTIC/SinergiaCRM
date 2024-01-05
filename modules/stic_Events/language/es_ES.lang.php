d62798e6505fc62d81882e83cc71ef24c19d8926
Jaume Albaigès <45965117+jalbaiges@users.noreply.github.com>
2024-01-05 13:13:10 +0100

    Improving translation

--- modules/stic_Events/language/es_ES.lang.php
+++ modules/stic_Events/language/es_ES.lang.php
@@ -119,7 +119,7 @@ $mod_strings = array(
     'LBL_TIME_START' => 'Fecha y hora de inicio de la 1ª sesión',
     'LBL_TITLE' => 'Crear sesiones periódicas',
     'LBL_SESSION_DURATION' => 'Duración de la sesión',
-    'LBL_SESSION_ENABLE_FIELDS' => 'Muestra más campos de Sesión',
-    'LBL_SESSION_ENABLE_FIELDS_INFO' => 'Establece un valor para cada uno de los siguientes campos de Sesión',
-    'LBL_SESSION_NAME_INFO' => 'Se puede añadir un contador de tipo numérico (Sessión 1, Sessión 2, Sessión N...) utilizando el parámetro {{$counter}}',
+    'LBL_SESSION_ENABLE_FIELDS' => 'Mostrar otros campos de Sesiones',
+    'LBL_SESSION_ENABLE_FIELDS_INFO' => 'Aparte de los campos obligatorios de inicio y final de la primera sesión y de la frecuencia de repetición, es posible establecer también valores por defecto para otros campos.',
+    'LBL_SESSION_NAME_INFO' => 'El nombre de la sesión puede incluir un contador numérico utilizando el parámetro {{$counter}}. Ejemplo: si se indica el valor "Sesión {{$counter}}" las sesiones se denominarán "Sesión 1", "Sesión 2", etc.',
 );
