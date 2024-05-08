35b3e6fcc35dca6610f5e0ff32a349a40d124b4a
Enric Prats <enric@sinergiacrm.org>
2024-05-08 05:24:57 +0000

    Añadidos valores por defecto en la definición de la función sendMail

--- modules/AOW_Actions/actions/actionSendEmail.php
+++ modules/AOW_Actions/actions/actionSendEmail.php
@@ -673,7 +673,7 @@ class actionSendEmail extends actionBase
     // STIC-Custom 20240307 EPS - Improve send mail action
     // https://github.com/SinergiaTIC/SinergiaCRM/issues/117
     // public function sendEmail($emailTo, $emailSubject, $emailBody, $altemailBody, SugarBean $relatedBean = null, $emailCc = array(), $emailBcc = array(), $attachments = array())
-    public function sendEmail($templateData, $emailTo, $mailerName = 'system', $user = '', $fromEmail, $fromName, $replyto, $replytoName, SugarBean $relatedBean = null, $emailCc = array(), $emailBcc = array(), $attachments = array())
+    public function sendEmail($templateData, $emailTo, $mailerName = 'system', $user = '', $fromEmail = '', $fromName = '', $replyto = '', $replytoName = '', SugarBean $relatedBean = null, $emailCc = array(), $emailBcc = array(), $attachments = array())
     // END STIC-Custom
     {
         require_once('modules/Emails/Email.php');
