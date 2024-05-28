68f849a461ce5e0c6bac7bb1b52ef8acc720e9c2
Jaume Albaigès <45965117+jalbaiges@users.noreply.github.com>
2024-05-28 17:58:15 +0200

    Improving translation

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/WorkCalendar_MainData/language/gl_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/WorkCalendar_MainData/language/gl_ES.lang.php
@@ -21,10 +21,10 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-    'NAME' => 'Registro horario - Revisión de los registros del día anterior', // Esta etiqueta es obligatoria. Indica el nombre de la función
+    'NAME' => 'Calendario laboral - Revisión de los registros del día anterior', // Esta etiqueta es obligatoria. Indica el nombre de la función
     'EMPLOYEE_NO_ACTIVATE_TIME_TRACKER'=> 'La funcionalidad de Registro horario no está activada en el usuario: ',
     'EMPLOYEE_NO_ACTIVATE_WORK_CALENDAR'=> 'La funcionalidad de Calendario laboral no está activada en el usuario: ',
     'NO_RECORD_IN_TIME_TRACKER' => 'Al registro de calendario laboral teórico de tipo laborable no le corresponde ningún registro horario efectivo.',
-    'EMAIL_SUBJECT' => 'Revisión de registro de Calendario laboral',
+    'EMAIL_SUBJECT' => 'Calendario laboral - Revisión de los registros del día anterior',
     'EMAIL_BODY' => 'Se ha encontrado un error al validar el siguiente registro de Calendario laboral: ',
 );
