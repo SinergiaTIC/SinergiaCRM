7510d099025d1c20de6b22327cd0693e55db1c69
Jaume Albaigès <45965117+jalbaiges@users.noreply.github.com>
2024-05-31 10:15:29 +0200

    Minor fix

--- custom/Extension/modules/Calendar/Ext/Language/gl_ES.SticLang.php
+++ custom/Extension/modules/Calendar/Ext/Language/gl_ES.SticLang.php
@@ -44,7 +44,7 @@ $mod_strings['LBL_FILTERS_STIC_WORK_CALENDAR_TITLE'] = 'Calendario laboral';
 $mod_strings['LBL_FILTERS_STIC_WORK_CALENDAR_TYPE'] = 'Tipo';
 $mod_strings['LBL_FILTERS_STIC_WORK_CALENDAR_DEPARTMENT'] = 'Departamento';
 
-$mod_strings['LBL_SETTINGS_SHARED_CALENDAR_SEPARATE_INFO'] = 'Con la opción activada los calendarios laborales de los diferentes usuarios pueden solaparse. Para optimizar su visualización se recomienda mostrar sus calendarios por separado.';
+$mod_strings['LBL_SETTINGS_SHARED_CALENDAR_SEPARATE_INFO'] = 'Con la opción activada los calendarios laborales de los diferentes usuarios pueden solaparse. Para optimizar su visualización se recomienda mostrarlos por separado.';
 $mod_strings['LBL_SETTINGS_STIC_WORK_CALENDAR_DISPLAY'] = 'Ver registros de Calendario laboral';
 $mod_strings['LBL_SETTINGS_STIC_WORK_CALENDAR_WORKING'] = 'Calendario laboral - Disponible';
 $mod_strings['LBL_SETTINGS_STIC_WORK_CALENDAR_NOWORKING'] = 'Calendario laboral - No disponible';
