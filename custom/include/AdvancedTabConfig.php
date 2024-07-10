841d212fafebbe85e8ababd603950a00949f4ad0
juanSTIC <juan@sinergiacrm.org>
2024-07-10 16:14:06 +0200

    tunnig icon width

---
+++ custom/include/AdvancedTabConfig.php
@@ -0,0 +1,22 @@
+<?php 
+$GLOBALS["SticTabStructure"] =array (
+  0 => 
+  array (
+    'id' => 'Ir_a_netflix|https://netflix.com',
+  ),
+  1 => 
+  array (
+    'id' => 'LBL_GROUPTAB_MAIN',
+    'children' => 
+    array (
+      0 => 
+      array (
+        'id' => 'stic_Centers',
+      ),
+    ),
+  ),
+  2 => 
+  array (
+    'id' => 'Un_informe_concreto|http://localhost:2000/sinergiacrm/index.php?module=KReports&offset=2&stamp=1720619019080915100&return_module=KReports&action=DetailView&record=be539e69-225c-5eba-97b1-5da6de6bd381',
+  ),
+);
