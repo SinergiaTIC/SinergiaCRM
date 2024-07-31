866419990bc28ddf26ff1ede90c23d52a5bc0085
Enric Prats <enric@sinergiacrm.org>
2024-07-31 15:58:38 +0000

    Cleaning seven modules

--- custom/Extension/application/Ext/EntryPointRegistry/seven.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// $entry_point_registry['seven'] = [
-//     'auth' => true,
-//     'file' => 'modules/seven/sendSMS.php',
-// ];
--- custom/Extension/application/Ext/EntryPointRegistry/seven_inbound.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// $entry_point_registry['seven_inbound'] = [
-//     'auth' => false,
-//     'file' => 'modules/seven/receiveSMS.php',
-// ];
