d8fc1b8a9f48b86b80a0db92ce8787be7d9accb1
ainaraRT <ainaretework@gmail.com>
2024-08-28 11:02:10 +0000

    change searchs

--- custom/modules/Trackers/metadata/SearchFields.php
+++ custom/modules/Trackers/metadata/SearchFields.php
@@ -48,19 +48,22 @@ array(
         'vname' => 'LBL_CURRENT_USER_FILTER',
         'type' => 'bool'
     ),
-    'tracker_user' => array('query_type' => 'default'),
 
     //Range Search Support
-    'range_date_modified' => array('query_type' => 'default', 'enable_range_search' => true, 'is_date_field' => true),
+    'range_date_modified' => array(
+        'query_type' => 'default',
+        'enable_range_search' => true,
+        'is_date_field' => true,
+    ),
     'start_range_date_modified' => array(
         'query_type' => 'default',
         'enable_range_search' => true,
-        'is_date_field' => true
+        'is_date_field' => true,
     ),
     'end_range_date_modified' => array(
         'query_type' => 'default',
         'enable_range_search' => true,
-        'is_date_field' => true
+        'is_date_field' => true,
     ),
-    //Range Search Support
+    // Range Search Support
 );
