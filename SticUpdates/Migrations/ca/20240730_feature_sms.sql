76139966cd4c1c217bb6c2ce6d4feb04213ff51d
Enric Prats <enric@sinergiacrm.org>
2024-09-09 05:13:54 +0000

    INSERT vs REPLACE

--- SticUpdates/Migrations/ca/20240730_feature_sms.sql
+++ SticUpdates/Migrations/ca/20240730_feature_sms.sql
@@ -1,4 +1,4 @@
-INSERT INTO `stic_settings` (`id`, `date_entered`, `date_modified`, `modified_user_id`, `created_by`, `deleted`, `assigned_user_id`, `type`, `name`, `value`, `description`) VALUES
+REPLACE INTO `stic_settings` (`id`, `date_entered`, `date_modified`, `modified_user_id`, `created_by`, `deleted`, `assigned_user_id`, `type`, `name`, `value`, `description`) VALUES
 ('652c33e1-522e-4f06-8a66-755d05a318e6', NOW(), NOW(), '1', '1', 0, '1', 'SEVEN', 'SEVEN_ACTIVE', '0', "0 indica que no s'han d'enviar missatges. 1 indica que s'han d'enviar missatges."),
 ('d9f0118c-b752-444d-9057-456b18f9f360', NOW(), NOW(), '1', '1', 0, '1', 'SEVEN', 'SEVEN_API_KEY', 'XXXXX', "Clau per connectar amb Seven. S'obté al panell de control de Seven."),
 ('c8d720bd-2c7b-47a6-98ce-9664529559ca', NOW(), NOW(), '1', '1', 0, '1', 'SEVEN', 'SEVEN_SENDER', '<ORGANITZACIÓ>', "Nom que apareixerà com a remitent en els missatges enviats"),
--- SticUpdates/Migrations/es/20240730_feature_sms.sql
+++ SticUpdates/Migrations/es/20240730_feature_sms.sql
@@ -1,5 +1,5 @@
-INSERT INTO `stic_settings` (`id`, `date_entered`, `date_modified`, `modified_user_id`, `created_by`, `deleted`, `assigned_user_id`, `type`, `name`, `value`, `description`) VALUES
+REPLACE INTO `stic_settings` (`id`, `date_entered`, `date_modified`, `modified_user_id`, `created_by`, `deleted`, `assigned_user_id`, `type`, `name`, `value`, `description`) VALUES
 ('652c33e1-522e-4f06-8a66-755d05a318e6', NOW(), NOW(), '1', '1', 0, '1', 'SEVEN', 'SEVEN_ACTIVE', '0', "0 indica que no se enviarán mensajes. 1 indica que s´q se enviarán mensajes."),
 ('d9f0118c-b752-444d-9057-456b18f9f360', NOW(), NOW(), '1', '1', 0, '1', 'SEVEN', 'SEVEN_API_KEY', 'XXXXX', "Clave para conectar con Seven. Se obtiene en el panel de control de Seven."),
 ('c8d720bd-2c7b-47a6-98ce-9664529559ca', NOW(), NOW(), '1', '1', 0, '1', 'SEVEN', 'SEVEN_SENDER', '<ORGANITZACIÓ>', "Nombre que aparecerá como remitente en los mensajes enviados"),
-('64b1554a-cf95-423b-ae78-7f75951f3976', NOW(), NOW(), '1', '1', 0, '1', 'MESSAGES', 'MESSAGES_LIMIT', '10', "Nñumero máximo de mensajes permitidos en envío masivo");
+('64b1554a-cf95-423b-ae78-7f75951f3976', NOW(), NOW(), '1', '1', 0, '1', 'MESSAGES', 'MESSAGES_LIMIT', '10', "Número máximo de mensajes permitidos en envío masivo");
