978cb2e1020c725df4a802c0619258c999e5957d
ainaraRT <ainaretework@gmail.com>
2024-09-11 15:34:26 +0000

    custom language

---
+++ custom/Extension/modules/Trackers/Ext/Language/es_ES.SticLang.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of SinergiaCRM.
+ * SinergiaCRM is a work developed by SinergiaTIC Association, based on SuiteCRM.
+ * Copyright (C) 2013 - 2023 SinergiaTIC Association
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
+ */
+$mod_strings['LNK_LIST'] = 'Mostrar las Monitorizaciones';
+$mod_strings['LBL_TRACKER_USER'] = 'Usuario';
+$mod_strings['LBL_ACTION_DETAILVIEW'] = 'Registro accedido';
+$mod_strings['LBL_ACTION_EDITVIEW'] = 'Registro editado';
+$mod_strings['LBL_ACTION_DELETED'] = 'Registro eliminado';
+$mod_strings['LBL_ACTION_REGISTER'] = 'Registro creado';
