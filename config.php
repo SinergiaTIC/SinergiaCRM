e9dbe213b58bed6966fdfc02c424c724300c830d
jordiSTIC <jordi@sinergiacrm.org>
2024-09-26 08:43:48 +0000

    SinergiaCRM 1.7.0 Release

--- config.php
+++ config.php
@@ -576,7 +576,7 @@ $sugar_config = array(
     
     // STIC Custom 20231124 JBL - SemVer in SinergiaCRM
     // STIC#1319
-    'sinergiacrm_version' => '1.7.0',
+    'sinergiacrm_version' => '1.7.0', 
     'js_custom_version' => 10,
     'stic_show_update_alert' => 1,
     // END STIC
