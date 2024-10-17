674cc12273038df22d96806f9890150f437b0836
ainaraRT <ainaretework@gmail.com>
2024-10-17 08:46:10 +0000

    vardefs in modules

--- modules/Trackers/vardefs.php
+++ modules/Trackers/vardefs.php
@@ -52,6 +52,10 @@ $dictionary['Tracker'] = array(
             'isnull' => 'false',
             'auto_increment' => true,
             'reportable'=>true,
+            // STIC Custom 20241016 ART - Tracker Module
+            // We add the properties, since in extension does not pick them up correctly 
+            'inline_edit' => 0,
+            // END STIC Custom
         ),
         'monitor_id'=>array(
             'name' => 'monitor_id',
@@ -59,6 +63,10 @@ $dictionary['Tracker'] = array(
             'type' => 'id',
             'required'=>true,
             'reportable'=>false,
+            // STIC Custom 20241016 ART - Tracker Module
+            // We add the properties, since in extension does not pick them up correctly 
+            'inline_edit' => 0,
+            // END STIC Custom
         ),
         'user_id'=>array(
             'name' => 'user_id',
@@ -66,11 +74,23 @@ $dictionary['Tracker'] = array(
             'type' => 'varchar',
             'len' => '36',
             'isnull' => 'false',
+            // STIC Custom 20241016 ART - Tracker Module
+            // We add the properties, since in extension does not pick them up correctly 
+            'module' => 'Users',
+            'id' => 'user_id',
+            'inline_edit' => 0,
+            // END STIC Custom
         ),
         'module_name'=>array(
             'name' => 'module_name',
             'vname' => 'LBL_MODULE_NAME',
-            'type' => 'varchar',
+            // STIC Custom 20241016 ART - Tracker Module
+            // 'type' => 'varchar',
+            // We add the properties, since in extension does not pick them up correctly 
+            'type' => 'enum',
+            'options' => 'moduleList',
+            'inline_edit' => 0,
+            // END STIC Custom
             'len' => '255',
             'isnull' => 'false',
         ),
@@ -80,6 +100,12 @@ $dictionary['Tracker'] = array(
             'type' => 'varchar',
             'len' => '36',
             'isnull' => 'false',
+            // STIC Custom 20241016 ART - Tracker Module
+            // We add the properties, since in extension does not pick them up correctly 
+            'inline_edit' => 0,
+            'link' => true,
+            'id' => 'item_id',
+            // END STIC Custom
         ),
         'item_summary'=>array(
             'name' => 'item_summary',
@@ -87,19 +113,37 @@ $dictionary['Tracker'] = array(
             'type' => 'varchar',
             'len' => '255',
             'isnull' => 'false',
+            // STIC Custom 20241016 ART - Tracker Module
+            // We add the properties, since in extension does not pick them up correctly 
+            'inline_edit' => 0,
+            'id' => 'item_id',
+            // END STIC Custom
         ),
         'date_modified'=>array(
             'name' => 'date_modified',
             'vname' => 'LBL_DATE_LAST_ACTION',
             'type' => 'datetime',
             'isnull' => 'false',
+            // STIC Custom 20241016 ART - Tracker Module
+            // We add the properties, since in extension does not pick them up correctly 
+            'options' => 'date_range_search_dom',
+            'enable_range_search' => true,
+            'dbType' => 'datetimecombo',
+            'inline_edit' => 0,
+            // END STIC Custom
         ),
         'action'=>array(
             'name' => 'action',
             'vname' => 'LBL_ACTION',
-            'type' => 'varchar',
             'len' => '255',
             'isnull' => 'false',
+            // STIC Custom 20241016 ART - Tracker Module
+            // 'type' => 'varchar',
+            // We add the properties, since in extension does not pick them up correctly 
+            'type' => 'enum',
+            'options' => 'trackers_actions_list',
+            'inline_edit' => 0,
+            // END STIC Custom
         ),
         'session_id'=>array(
             'name' => 'session_id',
@@ -107,9 +151,11 @@ $dictionary['Tracker'] = array(
             'type' => 'varchar',
             'len' => '36',
             'isnull' => 'true',
-            'exportable' => false
-
-
+            'exportable' => false,
+            // STIC Custom 20241016 ART - Tracker Module
+            // We add the properties, since in extension does not pick them up correctly 
+            'inline_edit' => 0,
+            // END STIC Custom
         ),
         'visible'=>array(
             'name' => 'visible',
@@ -117,6 +163,10 @@ $dictionary['Tracker'] = array(
             'type' => 'bool',
             'len' => '1',
             'default' => '0',
+            // STIC Custom 20241016 ART - Tracker Module
+            // We add the properties, since in extension does not pick them up correctly 
+            'inline_edit' => 0,
+            // END STIC Custom
         ),
         'deleted' =>array(
             'name' => 'deleted',
@@ -124,17 +174,31 @@ $dictionary['Tracker'] = array(
             'type' => 'bool',
             'default' => '0',
             'reportable'=>false,
-            'comment' => 'Record deletion indicator'
+            'comment' => 'Record deletion indicator',
+            // STIC Custom 20241016 ART - Tracker Module
+            // We add the properties, since in extension does not pick them up correctly 
+            'inline_edit' => 0,
+            // END STIC Custom
         ),
         'assigned_user_link'=>array(
             'name' => 'assigned_user_link',
-            'type' => 'link',
-            'relationship' => 'tracker_user_id',
-            'vname' => 'LBL_ASSIGNED_TO_USER',
-            'link_type' => 'one',
             'module'=>'Users',
-            'bean_name'=>'User',
-            'source'=>'non-db',
+            // STIC Custom 20241016 ART - Tracker Module
+            // 'type' => 'link',
+            // 'relationship' => 'tracker_user_id',
+            // 'vname' => 'LBL_ASSIGNED_TO_USER',
+            // 'link_type' => 'one',
+            // 'bean_name'=>'User',
+            // 'source'=>'non-db',
+            // We add the properties, since in extension does not pick them up correctly 
+            'type' => 'enum',
+            'vname' => 'LBL_ASSIGNED_TO',
+            'table' => 'users',
+            'id_name' => 'user_id',
+            'link' => true,
+            'rname' => 'name',
+            'inline_edit' => 0,
+            // END STIC Custom
         ),
         'monitor_id_link'=>array(
             'name' => 'monitor_id_link',
@@ -145,6 +209,10 @@ $dictionary['Tracker'] = array(
             'module'=>'TrackerPerfs',
             'bean_name'=>'TrackerPerf',
             'source'=>'non-db',
+            // STIC Custom 20241016 ART - Tracker Module
+            // We add the properties, since in extension does not pick them up correctly 
+            'inline_edit' => 0,
+            // END STIC Custom
         ),
     ) ,
 
