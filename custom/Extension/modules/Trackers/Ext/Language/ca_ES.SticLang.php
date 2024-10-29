38caf9cd9f0a4d4c877de62fa58380c2d7d9ffb7
Jaume Albaigès <45965117+jalbaiges@users.noreply.github.com>
2024-10-29 17:21:29 +0100

    Improving translation

--- custom/Extension/modules/Trackers/Ext/Language/ca_ES.SticLang.php
+++ custom/Extension/modules/Trackers/Ext/Language/ca_ES.SticLang.php
@@ -20,4 +20,4 @@
  *
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
-$mod_strings['LNK_LIST'] = 'Mostrar las Monitorizaciones';
+$mod_strings['LNK_LIST'] = 'Ver la Monitorización';
