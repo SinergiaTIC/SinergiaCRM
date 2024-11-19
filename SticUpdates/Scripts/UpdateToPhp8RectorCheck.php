ba4bb85f0be1df61efa47a061b634493204dc048
jordiSTIC <jordi@sinergiacrm.org>
2024-11-19 15:47:04 +0000

    Script updated

--- SticUpdates/Scripts/UpdateToPhp8RectorCheck.php
+++ SticUpdates/Scripts/UpdateToPhp8RectorCheck.php
@@ -22,37 +22,38 @@
  */
 
  /**
- * This script contains Executes Rector during the update of SinergiaCRM instances,
+ * This script executes Rector during the update of SinergiaCRM instances,
  * using the Rector configuration defined in SticRectorConfig.php on the root folder
+ * 
+ * Without arguments, saves suggested changes to file
+ * With argument "adapt-code", modifies the code 
+ * 
  */
-// declare (strict_types=1);
-// require_once __DIR__ . '/../../SticInclude/vendor/rector-standalone/vendor/rector/rector/vendor/autoload.php';
-// require_once __DIR__ . '/../../SticInclude/vendor/rector-standalone/vendor/rector/rector/vendor/scoper-autoload.php';
-// require_once __DIR__ . '/../../SticInclude/vendor/rector-standalone/vendor/autoload.php';
 
-// use RectorPrefix202411\Nette\Utils\Json;
-// use Rector\Bootstrap\RectorConfigsResolver;
-// use Rector\ChangesReporting\Output\JsonOutputFormatter;
-// use Rector\Configuration\Option;
-// use Rector\Console\Style\SymfonyStyleFactory;
-// use Rector\DependencyInjection\LazyContainerFactory;
-// use Rector\DependencyInjection\RectorContainerFactory;
-// use Rector\Util\Reflection\PrivatesAccessor;
-// use RectorPrefix202411\Symfony\Component\Console\Application;
-// use RectorPrefix202411\Symfony\Component\Console\Command\Command;
-// use RectorPrefix202411\Symfony\Component\Console\Input\ArgvInput;
-// use Rector\ValueObject\Bootstrap\BootstrapConfigs;
+$output = new RectorCheckResultHelper(__DIR__."/../../rectorOutput.html");
 
-echo "<h2>Rector execution</h2>";
-$rectorFile = __DIR__.'/../../SticInclude/vendor/rector-standalone/vendor/rector/rector/bin/rector.php';
-$rectorConfigFile = __DIR__.'/../../SticRectorConfig.php';
+$rectorFile = __DIR__."/../../SticInclude/vendor/rector-standalone/vendor/rector/rector/bin/rector.php";
+$rectorConfigFile = __DIR__."/../../SticRectorConfig.php";
+
+$output->addInfo($rectorFile, "Rector");
+$output->addInfo($rectorConfigFile, "Rector config");
+
 if (!file_exists($rectorFile)) {
-    echo "<h3>Error</h3>";
-    echo "Rector is not installed. Can not run script<br />";
-    exit(1);
+    $output->addFatalError("Rector is not installed. Can not run script");
+    $output->closeAndExit(1);
 }
 
-$rectorCommand = "php {$rectorFile} process --dry-run --config={$rectorConfigFile}";// --output-format json";
+// Get command arguments
+$arguments = $_SERVER['argv'];
+$scriptName = array_shift($arguments);
+$adaptCode = count($arguments) > 0 && $arguments[0] == "adapt-code";
+
+$rectorCommand = "php {$rectorFile} process --no-progress-bar --config={$rectorConfigFile}";// --output-format json";
+if (!$adaptCode) {
+    $rectorCommand .= " --dry-run";
+}
+$output->addInfo($rectorCommand, "Command");
+
 
 $descriptorspec = [
     1 => ['pipe', 'w'], // stdout
@@ -65,113 +66,109 @@ if (is_resource($process)) {
     // Read standard output
     $stdout = stream_get_contents($pipes[1]);
     $stdout = str_replace("\\n", "\n", $stdout);
+    $output->addResults(htmlspecialchars($stdout));
     fclose($pipes[1]);
 
     // Read error output
     $stderr = stream_get_contents($pipes[2]);
     $stderr = str_replace("\\n", "\n", $stderr);
+    $output->addErrorResults(htmlspecialchars($stderr));
     fclose($pipes[2]);
 
     // Close process
     $returnCode = proc_close($process);
-    echo "<h3>Return code</h3>";
-    echo $returnCode;
-
-    // Show captured output
-    echo "<h3>Output</h3>";
-    echo nl2br(htmlspecialchars($stdout));
-    echo "<h3>Errors</h3>";
-    echo nl2br(htmlspecialchars($stderr));
+    $output->closeAndExit($returnCode);
 } else {
-    echo "<h3>Error</h3>";
-    echo "Can not start Rector process<br />";
+    $output->addFatalError("Can not start Rector process");
+    $output->closeAndExit(1);
 }
 
-// exit();
+class RectorCheckResultHelper {
 
+    private $startDate;
+    private $fileName;
+    private $fatalErrors = [];
+    private $infos = [];
+    private $results = [];
+    private $errorResults = [];
 
+    public function __construct($fileName)
+    {
+        $this->startDate = date('Y-m-d H:i:s');
+        $this->fileName = $fileName;
+    }
 
+    private function getLabelText($text, $label = "", $newLine = false)
+    {
+        if(!empty($label)) {
+            $text = "<strong>{$label}</strong>: {$text}";
+        }
+        return $text. ($newLine ? "\n" : "");
+    }
+    private function getTitle($text, $newLine = false) {
+        return "<h3>{$text}</h3>" . ($newLine ? "\n" : "");
+    }
 
+    public function addInfo($text, $label = "")
+    {
+        $this->infos[] = $this->getLabelText($text, $label);
+    }
 
-// // Prepare rector params
-// $_SERVER['argv'] = [];
-// $_SERVER['argv'][] = $rectorFile;
-// $_SERVER['argv'][] = "process";
-// $_SERVER['argv'][] = "--dry-run";
-// $_SERVER['argv'][] = "--config={$rectorConfigFile}";
-// $_SERVER['argv'][] = "--output-format";
-// $_SERVER['argv'][] = "json";
+    public function addFatalError($text)
+    {
+        $this->fatalErrors[] = $text;
+    }
 
-// ob_start();
-// try {
-//     require_once $rectorFile;
-// } catch (Throwable $e) {
-//     $stderr = ob_get_clean();
-//     echo "Error executing Rector: " . $e->getMessage() . "<br />";
-//     echo "Output:<br />" . $stderr;
-//     exit(1);
-// }
-// $stdout = ob_get_clean();
-// echo "Output:<br />" . $stdout;
+    public function addResults($text)
+    {
+        $this->results = [...$this->results, ...explode("\n", $text)];
+    }
 
-// exit();
+    public function addErrorResults($text)
+    {
+        $this->errorResults = [...$this->errorResults, ...explode("\n", $text)];
+    }
 
+    public function closeAndExit($resultCode)
+    {
+        $content = "Result: {$resultCode} <br />\n";
 
+        $this->addInfo($resultCode, "Result");
 
+        // Last Execution
+        $content .= $this->getTitle("Execution", true);
+        $content .= $this->getLabelText($this->startDate . "<br />", "Start", true);
+        $content .= $this->getLabelText(date('Y-m-d H:i:s') . "<br />", "End", true);
 
+        // Fatal Errors
+        if (count($this->fatalErrors) > 0) {
+            $content .= $this->getTitle("Error", true);
+            $content .= implode("<br />\n", $this->fatalErrors);
+            $content .= "<br />\n";
+        }
+        
+        // Informations
+        if (count($this->infos) > 0) {
+            $content .= $this->getTitle("Information", true);
+            $content .= implode("<br />\n", $this->infos);
+            $content .= "<br />\n";
+        }
 
-// // @ intentionally: continue anyway
-// @\ini_set('memory_limit', '-1');
-// // Performance boost
-// \error_reporting(\E_ALL);
-// \ini_set('display_errors', 'stderr');
-// \gc_disable();
-// \define('__RECTOR_RUNNING__', \true);
+        // Results
+        if (count($this->results) > 0) {
+            $content .= $this->getTitle("Result", true);
+            $content .= implode("<br />\n", $this->results);
+            $content .= "<br />\n";
+        }
 
+        // Error Results
+        if (count($this->results) > 0) {
+            $content .= $this->getTitle("Rector Errors", true);
+            $content .= implode("<br />\n", $this->errorResults);
+            $content .= "<br />\n";
+        }
 
-// //$_SERVER['argv']
-
-// $rectorConfigFile = __DIR__.'/../../SticRectorConfig.php';
-// try {
-//     $bootstrapConfigs = new BootstrapConfigs($rectorConfigFile, []);
-//     $rectorContainerFactory = new RectorContainerFactory();
-//     $container = $rectorContainerFactory->createFromBootstrapConfigs($bootstrapConfigs);
-// } catch (\Throwable $throwable) {
-//     // for json output
-//     $argvInput = new ArgvInput();
-//     $outputFormat = $argvInput->getParameterOption('--' . Option::OUTPUT_FORMAT);
-//     // report fatal error in json format
-//     if ($outputFormat === JsonOutputFormatter::NAME) {
-//         echo Json::encode(['fatal_errors' => [$throwable->getMessage()]]);
-//     } else {
-//         // report fatal errors in console format
-//         $symfonyStyleFactory = new SymfonyStyleFactory(new PrivatesAccessor());
-//         $symfonyStyle = $symfonyStyleFactory->create();
-//         $symfonyStyle->error($throwable->getMessage());
-//     }
-//     exit(Command::FAILURE);
-// }
-// /** @var Application $application */
-// $application = $container->get(Application::class);
-// exit($application->run());
-
-
-
-
-
-
-// $command = escapeshellcmd("php {$rectorFile} process --dry-run --config={$rectorConfigFile} --output-format json");
-// // $command = escapeshellcmd("php {$rectorFile} list-rules --output-format json");
-// // $command = escapeshellcmd("php $rectorFile process --config={$configFile}");
-
-// exec($command, $output, $returnVar);
-
-// if ($returnVar !== 0) {
-//     echo "Error running Rector: {$returnVar}<br />";
-//     if (is_array($output)) {
-//         echo implode("<br />", $output);
-//     }
-//     exit($returnVar);
-// }
-
-// echo "Rector executed withot errors.<br />";
+        file_put_contents($this->fileName, $content);
+        exit($resultCode);
+    }
+}
