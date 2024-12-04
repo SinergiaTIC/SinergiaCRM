1198ff4555fb4a427e33afdc291f5b3026b0071d
PaulaaSTIC <paula@sinergiacrm.org>
2024-12-04 16:45:37 +0000

    Fix custom relationships

--- data/SugarBean.php
+++ data/SugarBean.php
@@ -43,6 +43,8 @@
  * "Supercharged by SuiteCRM" and “Nonprofitized by SinergiaCRM”. 
  */
 
+use Symfony\Component\Validator\Constraints\IsNull;
+
 if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
@@ -2866,23 +2868,21 @@ class SugarBean
         if (!empty($new_rel_id)
             && ( // Relationships with an ID in the _ida field of the relationship different from the parent record
                 (!empty($this->{$new_rel_link}) && is_string($this->{$new_rel_link}) && $new_rel_id != $this->{$new_rel_link})
+                // Special relationships like member_accounts where the parent_id is not equal to the ID of the parent record
+                 || (empty($this->{$new_rel_link}) && !empty($this->parent_id) && $new_rel_id !=$this->parent_id)
                 // STIC-Custom 20231021 PCS - Allow parent_id is not equal to the ID of the parent record needed in the activity subpanel
                 // https://github.com/SinergiaTIC/SinergiaCRM/pull/447
-                // Special relationships like member_accounts where the parent_id is not equal to the ID of the parent record
-                // || (empty($this->{$new_rel_link}) && !empty($this->parent_id) && $new_rel_id !=$this->parent_id)
-                || (
-                    // Check if LHSModule equals RHSModule
-                    ($this->load_relationship($new_rel_link) && !empty($this->$new_rel_link)
-                        && $this->$new_rel_link->getRelationshipObject()->getLHSModule() == $this->$new_rel_link->getRelationshipObject()->getRHSModule()
-                    )
-                )
-                // END STIC-Custom
+                //  )
+                // ) { 
+                 || ($rel_link == $new_rel_link)
             )
-        ) {
-            $new_rel_id = ''; // Assign the empty value to this variable, in the same way as when creating the record from the edit view
+        ) { 
+             if( (!is_null($this->{$new_rel_link}) )  ){
+                $new_rel_id = ''; // Assign the empty value to this variable, in the same way as when creating the record from the edit view
+             }
+            // END STIC-Custom
         }
-            // END STIC_Custom
-
+        // END STIC_Custom
         return array($new_rel_id, $new_rel_link);
     }
 
