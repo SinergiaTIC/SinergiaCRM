6ece6068a50a23b54a98e14b78a25614633aff40
juanSTIC <juan@sinergiacrm.org>
2024-12-16 14:05:19 +0100

    remove files & minor fix

--- custom/Extension/modules/Administration/Ext/Administration/stic_SettingsAdmin.php
+++ custom/Extension/modules/Administration/Ext/Administration/stic_SettingsAdmin.php
@@ -32,7 +32,7 @@ $admin_option_defs['Administration']['stic_settings'] = array(
     'stic-settings',
 );
 
-// Create de Section if not exists. The SinergiaCRM section in the administration section is a shared section with several modules,
+// Create Section if not exists. The SinergiaCRM section in the administration section is a shared section with several modules,
 // therefore, we must check if the section has already been initialized.
 // If you have not already done so, create it again, if it has already been initialized, mix the old options with the new one.
 if (!isset($admin_group_header['LBL_SINERGIACRM_TAB_TITLE']) || !isset($admin_group_header['LBL_SINERGIACRM_TAB_TITLE'][3])) {
--- custom/include/language/ca_ES.lang.php
+++
@@ -1,8 +0,0 @@
-<?php
-$app_list_strings['newsletter_frequency_dom']=array (
-  '' => '',
-  'Weekly' => 'Weekly',
-  'Monthly' => 'Monthly',
-  'Quarterly' => 'Quarterly',
-  'Annually' => 'Annually',
-);
