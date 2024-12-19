61c54c8bd71e3c1dd6df10b1bcd6b363c52cf4db
juanSTIC <juan@sinergiacrm.org>
2024-12-19 09:07:02 +0100

    remove  wrong files

---
+++ custom/include/AdvancedTabConfig.php
@@ -0,0 +1,461 @@
+<?php 
+$GLOBALS["SticTabStructure"] =array (
+  0 => 
+  array (
+    'id' => 'LBL_GROUPTAB_MAIN',
+    'text' => 'Principal',
+    'children' => 
+    array (
+      0 => 
+      array (
+        'id' => 'Contacts',
+        'text' => 'Personas',
+      ),
+      1 => 
+      array (
+        'id' => 'stic_Contacts_Relationships',
+        'text' => 'Relaciones con Personas',
+      ),
+      2 => 
+      array (
+        'id' => 'Accounts',
+        'text' => 'Organizaciones',
+        'children' => 
+        array (
+          0 => 
+          array (
+            'id' => 'stic_Centers',
+            'text' => 'Centros',
+          ),
+        ),
+      ),
+      3 => 
+      array (
+        'id' => 'stic_Accounts_Relationships',
+        'text' => 'Relaciones con Organizaciones',
+      ),
+      4 => 
+      array (
+        'id' => 'Leads',
+        'text' => 'Interesados',
+      ),
+      5 => 
+      array (
+        'id' => 'Project',
+        'text' => 'Proyectos',
+      ),
+    ),
+  ),
+  1 => 
+  array (
+    'id' => 'LBL_GROUPTAB_ACTIVITIES',
+    'text' => 'Actividades',
+    'children' => 
+    array (
+      0 => 
+      array (
+        'id' => 'Calendar',
+        'text' => 'Calendario',
+      ),
+      1 => 
+      array (
+        'id' => 'Calls',
+        'text' => 'Llamadas',
+      ),
+      2 => 
+      array (
+        'id' => 'Meetings',
+        'text' => 'Reuniones',
+      ),
+      3 => 
+      array (
+        'id' => 'Tasks',
+        'text' => 'Tareas',
+      ),
+      4 => 
+      array (
+        'id' => 'Notes',
+        'text' => 'Notas',
+      ),
+      5 => 
+      array (
+        'id' => 'Documents',
+        'text' => 'Documentos',
+      ),
+      6 => 
+      array (
+        'id' => 'Emails',
+        'text' => 'Correos',
+      ),
+      7 => 
+      array (
+        'id' => 'LBL_GROUPTAB_RRHH',
+        'text' => 'RRHH',
+        'children' => 
+        array (
+          0 => 
+          array (
+            'id' => 'stic_Work_Calendar',
+            'text' => 'Calendario laboral',
+          ),
+          1 => 
+          array (
+            'id' => 'stic_Time_Tracker',
+            'text' => 'Registro horario',
+          ),
+        ),
+      ),
+    ),
+  ),
+  2 => 
+  array (
+    'id' => 'LBL_GROUPTAB_CAMPAIGNS',
+    'text' => 'Campañas',
+    'children' => 
+    array (
+      0 => 
+      array (
+        'id' => 'Campaigns',
+        'text' => 'Campañas',
+      ),
+      1 => 
+      array (
+        'id' => 'ProspectLists',
+        'text' => 'Público Objetivo - Listas',
+      ),
+      2 => 
+      array (
+        'id' => 'Surveys',
+        'text' => 'Encuestas',
+      ),
+    ),
+  ),
+  3 => 
+  array (
+    'id' => 'LBL_GROUPTAB_ECONOMY',
+    'text' => 'Economía',
+    'children' => 
+    array (
+      0 => 
+      array (
+        'id' => 'Opportunities',
+        'text' => 'Subvenciones',
+        'children' => 
+        array (
+          0 => 
+          array (
+            'id' => 'stic_Group_Opportunities',
+            'text' => 'Organizaciones subvencionadas',
+          ),
+        ),
+      ),
+      1 => 
+      array (
+        'id' => 'stic_Payment_Commitments',
+        'text' => 'Compromisos de Pago',
+      ),
+      2 => 
+      array (
+        'id' => 'stic_Payments',
+        'text' => 'Pagos',
+      ),
+      3 => 
+      array (
+        'id' => 'stic_Remittances',
+        'text' => 'Remesas',
+      ),
+      4 => 
+      array (
+        'id' => 'LBL_GROUPTAB_SALES',
+        'text' => 'Ventas',
+        'children' => 
+        array (
+          0 => 
+          array (
+            'id' => 'AOS_Products',
+            'text' => 'Productos',
+            'children' => 
+            array (
+              0 => 
+              array (
+                'id' => 'AOS_Product_Categories',
+                'text' => 'Categorías de Productos',
+              ),
+            ),
+          ),
+          1 => 
+          array (
+            'id' => 'AOS_Quotes',
+            'text' => 'Presupuestos',
+            'children' => 
+            array (
+              0 => 
+              array (
+                'id' => 'AOS_Invoices',
+                'text' => 'Facturas',
+              ),
+              1 => 
+              array (
+                'id' => 'AOS_Contracts',
+                'text' => 'Contratos',
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  ),
+  4 => 
+  array (
+    'id' => 'LBL_GROUPTAB_DIRECTCARE',
+    'text' => 'Atención directa',
+    'children' => 
+    array (
+      0 => 
+      array (
+        'id' => 'LBL_GROUPTAB_PERSONAL_INFO',
+        'text' => 'Información personal',
+        'children' => 
+        array (
+          0 => 
+          array (
+            'id' => 'stic_Personal_Environment',
+            'text' => 'Entorno personal',
+          ),
+          1 => 
+          array (
+            'id' => 'stic_Families',
+            'text' => 'Unidades familiares',
+          ),
+          2 => 
+          array (
+            'id' => 'stic_Skills',
+            'text' => 'Competencias',
+          ),
+          3 => 
+          array (
+            'id' => 'stic_Work_Experience',
+            'text' => 'Experiencia laboral',
+          ),
+          4 => 
+          array (
+            'id' => 'stic_Training',
+            'text' => 'Formación',
+          ),
+          5 => 
+          array (
+            'id' => 'stic_Grants',
+            'text' => 'Ayudas',
+          ),
+        ),
+      ),
+      1 => 
+      array (
+        'id' => 'LBL_GROUPTAB_ASSESSMENT_AND_GOALS',
+        'text' => 'Valoraciones y objetivos',
+        'children' => 
+        array (
+          0 => 
+          array (
+            'id' => 'stic_Assessments',
+            'text' => 'Valoraciones',
+          ),
+          1 => 
+          array (
+            'id' => 'stic_Goals',
+            'text' => 'Objetivos',
+          ),
+        ),
+      ),
+      2 => 
+      array (
+        'id' => 'stic_FollowUps',
+        'text' => 'Seguimientos',
+      ),
+      3 => 
+      array (
+        'id' => 'stic_Journal',
+        'text' => 'Diario',
+      ),
+      4 => 
+      array (
+        'id' => 'LBL_GROUPTAB_LABOURINSERTION',
+        'text' => 'Inserción laboral',
+        'children' => 
+        array (
+          0 => 
+          array (
+            'id' => 'stic_Job_Offers',
+            'text' => 'Ofertas laborales',
+          ),
+          1 => 
+          array (
+            'id' => 'stic_Job_Applications',
+            'text' => 'Candidaturas',
+          ),
+          2 => 
+          array (
+            'id' => 'LBL_GROUPTAB_EXTERNAL_APPS',
+            'text' => 'Aplicaciones externas',
+            'children' => 
+            array (
+              0 => 
+              array (
+                'id' => 'stic_Sepe_Actions',
+                'text' => 'Acciones SEPE',
+              ),
+              1 => 
+              array (
+                'id' => 'stic_Sepe_Incidents',
+                'text' => 'Incidencias SEPE',
+              ),
+              2 => 
+              array (
+                'id' => 'stic_Sepe_Files',
+                'text' => 'Generador XML SEPE',
+              ),
+              3 => 
+              array (
+                'id' => 'stic_Incorpora_Locations',
+                'text' => 'Ubicaciones Incorpora',
+              ),
+            ),
+          ),
+        ),
+      ),
+      5 => 
+      array (
+        'id' => 'LBL_GROUPTAB_MEDICATION',
+        'text' => 'Registro de medicación',
+        'children' => 
+        array (
+          0 => 
+          array (
+            'id' => 'stic_Medication_Log',
+            'text' => 'Registro de medicación',
+          ),
+          1 => 
+          array (
+            'id' => 'stic_Prescription',
+            'text' => 'Prescripciones médicas',
+          ),
+          2 => 
+          array (
+            'id' => 'stic_Medication',
+            'text' => 'Medicamentos',
+          ),
+        ),
+      ),
+    ),
+  ),
+  5 => 
+  array (
+    'id' => 'LBL_GROUPTAB_EVENTS',
+    'text' => 'Eventos',
+    'children' => 
+    array (
+      0 => 
+      array (
+        'id' => 'stic_Events',
+        'text' => 'Eventos',
+        'children' => 
+        array (
+          0 => 
+          array (
+            'id' => 'FP_Event_Locations',
+            'text' => 'Ubicaciones',
+          ),
+        ),
+      ),
+      1 => 
+      array (
+        'id' => 'stic_Registrations',
+        'text' => 'Inscripciones',
+      ),
+      2 => 
+      array (
+        'id' => 'stic_Sessions',
+        'text' => 'Sesiones',
+      ),
+      3 => 
+      array (
+        'id' => 'stic_Attendances',
+        'text' => 'Asistencias',
+      ),
+    ),
+  ),
+  6 => 
+  array (
+    'id' => 'LBL_GROUPTAB_DATA_MINING',
+    'text' => 'Info',
+    'children' => 
+    array (
+      0 => 
+      array (
+        'id' => 'AOS_PDF_Templates',
+        'text' => 'Plantillas PDF',
+      ),
+      1 => 
+      array (
+        'id' => 'DHA_PlantillasDocumentos',
+        'text' => 'Mail Merge Reports',
+      ),
+      2 => 
+      array (
+        'id' => 'KReports',
+        'text' => 'KReports',
+      ),
+    ),
+  ),
+  7 => 
+  array (
+    'id' => 'LBL_GROUPTAB_OTHER',
+    'text' => 'Otros',
+    'children' => 
+    array (
+      0 => 
+      array (
+        'id' => 'LBL_GROUPTAB_BOOKINGS',
+        'text' => 'Reservas',
+        'children' => 
+        array (
+          0 => 
+          array (
+            'id' => 'stic_Resources',
+            'text' => 'Recursos',
+          ),
+          1 => 
+          array (
+            'id' => 'stic_Bookings',
+            'text' => 'Reservas',
+          ),
+          2 => 
+          array (
+            'id' => 'stic_Bookings_Calendar',
+            'text' => 'Calendario de Reservas',
+          ),
+        ),
+      ),
+      1 => 
+      array (
+        'id' => 'AOW_WorkFlow',
+        'text' => 'Flujo de trabajo',
+      ),
+      2 => 
+      array (
+        'id' => 'Cases',
+        'text' => 'Casos',
+      ),
+      3 => 
+      array (
+        'id' => 'Bugs',
+        'text' => 'Incidencias',
+      ),
+    ),
+  ),
+  8 => 
+  array (
+    'id' => 'LBL_GROUPTAB_1734595409',
+    'text' => 'Nuevo nodo',
+  ),
+);
