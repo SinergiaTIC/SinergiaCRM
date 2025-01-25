a438dde9357bdfc3dcad1b66158ab76d8ea18205
Enric Prats <enric@sinergiacrm.org>
2025-01-25 11:49:13 +0000

    Added administrationmenu entry

--- custom/Extension/modules/Administration/Ext/Language/ca_ES.SticLang.php
+++ custom/Extension/modules/Administration/Ext/Language/ca_ES.SticLang.php
@@ -32,6 +32,9 @@ $mod_strings['LBL_STIC_VALIDATION_RESULTS_DESCRIPTION'] = 'Gestiona i revisa els
 $mod_strings['LBL_STIC_CUSTOM_VIEWS_LINK_TITLE'] = 'Vistes personalitzades';
 $mod_strings['LBL_STIC_CUSTOM_VIEWS_DESCRIPTION'] = 'Personalització condicional de les vistes dels mòduls.';
 
+$mod_strings['LBL_STIC_MESSAGES_QUEUE_LINK_TITLE'] = 'Cua de missatges de telèfon';
+$mod_strings['LBL_STIC_MESSAGES_QUEUE_DESCRIPTION'] = 'Gestió de la cua de missatges sortints.';
+
 $mod_strings['LBL_SINERGIACRM_TAB_STIC_SETTINGS_LINK_TITLE'] = 'Configuració';
 $mod_strings['LBL_SINERGIACRM_TAB_STIC_SETTINGS_DESCRIPTION'] = 'Opcions de configuració de SinergiaCRM.';
 
