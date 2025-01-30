87c80c9f3161c67c78947cc1a394c7c45b72c331
ainaraRT <ainaretework@gmail.com>
2025-01-30 14:38:55 +0000

    assigned_user_id is null anda action is editview

--- SticUpdates/Migrations/20250130_feature_TrackerModule.sql
+++ SticUpdates/Migrations/20250130_feature_TrackerModule.sql
@@ -1,3 +1,7 @@
--- Actualiza los registros existentes que no tienen el assigned_user_id, añadiéndole el user_id
+-- Actualiza los registros existentes que no tienen el assigned_user_id o es nulo, añadiéndole el user_id
 
-UPDATE `tracker` SET `assigned_user_id` = `user_id` WHERE `assigned_user_id` = '';
+UPDATE `tracker` SET `assigned_user_id` = `user_id` WHERE `assigned_user_id` IS NULL OR `assigned_user_id` = '';
+
+-- Actualiza los registros existentes que el action es editview cambiandolo por save
+
+UPDATE `tracker` SET `action` = 'save' WHERE `action` = 'editview';
