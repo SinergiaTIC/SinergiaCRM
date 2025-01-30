f0e5de67521621283131e71a848a02e8065bf2ec
ainaraRT <ainaretework@gmail.com>
2025-01-30 14:42:59 +0000

    editview change to update

--- SticUpdates/Migrations/20250130_feature_TrackerModule.sql
+++ SticUpdates/Migrations/20250130_feature_TrackerModule.sql
@@ -2,6 +2,6 @@
 
 UPDATE `tracker` SET `assigned_user_id` = `user_id` WHERE `assigned_user_id` IS NULL OR `assigned_user_id` = '';
 
--- Actualiza los registros existentes que el action es editview cambiandolo por save
+-- Actualiza los registros existentes que el action es editview cambiandolo por update
 
-UPDATE `tracker` SET `action` = 'save' WHERE `action` = 'editview';
+UPDATE `tracker` SET `action` = 'update' WHERE `action` = 'editview';
