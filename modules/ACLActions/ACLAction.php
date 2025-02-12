2b3b907d246be6deb66b5979ecd43aac2859e97d
jordiSTIC <jordi@sinergiacrm.org>
2025-02-12 10:49:28 +0000

    Fix errors and warnings in ACL

--- modules/ACLActions/ACLAction.php
+++ modules/ACLActions/ACLAction.php
@@ -408,21 +408,42 @@ class ACLAction extends SugarBean
         }
 
         //only set the session variable if it was a full list;
+        // STIC Custom 20250212 JBL - Fix Warnings and Errors in PHP 8+
+        // https://github.com/SinergiaTIC/SinergiaCRM/pull/477
+        //
+        // if (empty($category) && empty($action)) {
+        //     if (!isset($_SESSION['ACL'])) {
+        //         $_SESSION['ACL'] = array();
+        //     }
+        //     $_SESSION['ACL'][$user_id] = $selected_actions;
+        // } elseif (empty($action) && !empty($category)) {
+        //     if (!empty($type)) {
+        //         $selectedActionCategoryType = isset($selected_actions[$category][$type]) ? $selected_actions[$category][$type] : null;
+        //         $_SESSION['ACL'][$user_id][$category][$type] = $selectedActionCategoryType;
+        //     }
+        //     $selectedActionCategory = isset($selected_actions[$category]) ? $selected_actions[$category] : null;
+        //     $_SESSION['ACL'][$user_id][$category] = $selectedActionCategory;
+        // } elseif (!empty($action) && !empty($category) && !empty($type)) {
+        //     $_SESSION['ACL'][$user_id][$category][$type][$action] = $selected_actions[$category][$action];
+        // }
+        //
+        $_SESSION['ACL'] ??= [];
+        $_SESSION['ACL'][$user_id] ??= [];
         if (empty($category) && empty($action)) {
-            if (!isset($_SESSION['ACL'])) {
-                $_SESSION['ACL'] = array();
-            }
             $_SESSION['ACL'][$user_id] = $selected_actions;
         } elseif (empty($action) && !empty($category)) {
+            // This if is redundant
             if (!empty($type)) {
-                $selectedActionCategoryType = isset($selected_actions[$category][$type]) ? $selected_actions[$category][$type] : null;
-                $_SESSION['ACL'][$user_id][$category][$type] = $selectedActionCategoryType;
+                $_SESSION['ACL'][$user_id][$category] ??= [];
+                $_SESSION['ACL'][$user_id][$category][$type] = $selected_actions[$category][$type] ?? null;
             }
-            $selectedActionCategory = isset($selected_actions[$category]) ? $selected_actions[$category] : null;
-            $_SESSION['ACL'][$user_id][$category] = $selectedActionCategory;
+            $_SESSION['ACL'][$user_id][$category] = $selected_actions[$category] ?? null;
         } elseif (!empty($action) && !empty($category) && !empty($type)) {
-            $_SESSION['ACL'][$user_id][$category][$type][$action] = $selected_actions[$category][$action];
+            $_SESSION['ACL'][$user_id][$category] ??= [];
+            $_SESSION['ACL'][$user_id][$category][$type] ??=[];
+            $_SESSION['ACL'][$user_id][$category][$type][$action] = $selected_actions[$category][$action] ?? null;
         }
+        // END STIC Custom
 
         // Sort by translated categories
         uksort($selected_actions, 'ACLAction::langCompare');
