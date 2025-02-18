73a216fb5e390fcfcb59b89d47bc7e4f37a006cd
jordiSTIC <jordi@sinergiacrm.org>
2025-02-18 08:55:55 +0000

    Check $_POST["duplicateSave"] isset

--- modules/stic_Custom_Views/stic_Custom_Views.php
+++ modules/stic_Custom_Views/stic_Custom_Views.php
@@ -91,7 +91,7 @@ class stic_Custom_Views extends Basic
 
         $id = parent::save($check_notify);
 
-        if (($_POST["duplicateSave"] && $_POST["duplicateSave"] == "true")) {
+        if (isset($_POST["duplicateSave"]) && $_POST["duplicateSave"] == "true") {
             // Duplicate Customizations
             $originalBean = BeanFactory::getBean("stic_Custom_Views", $_POST["duplicateId"]);
             $customizationBeans = getRelatedBeanObjectArray($originalBean, 'stic_custom_views_stic_custom_view_customizations');
