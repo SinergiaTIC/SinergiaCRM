57e4f1db2ff41581a79175a2f31ea74046551cf0
jordiSTIC <jordi@sinergiacrm.org>
2025-02-24 17:12:15 +0000

    Fix touch error

--- include/utils/sugar_file_utils.php
+++ include/utils/sugar_file_utils.php
@@ -306,8 +306,8 @@ function sugar_touch($filename, $time = null, $atime = null)
     // We need to check if is a local file, it can be a stream wrapper (ex: upload://myfile.txt)
     // touch is only available in local files
     $result = false;
-    if (is_string($filename) && !empty($filename) &&
-        stream_is_local($filename) && @file_exists($filename)) {
+    if (is_string($filename) && !empty($filename) && 
+        stream_is_local($filename)) {
         if (!empty($atime) && !empty($time)) {
             $result = @touch($filename, $time, $atime);
         } elseif (!empty($time)) {
