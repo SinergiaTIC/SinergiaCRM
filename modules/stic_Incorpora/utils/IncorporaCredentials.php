3d342d3e17a3be5ff5e11d09e5317f4edc4a25ae
AlbertoSTIC <alberto@sinergiacrm.org>
2025-05-08 10:00:58 +0000

    url pre

--- modules/stic_Incorpora/utils/IncorporaCredentials.php
+++ modules/stic_Incorpora/utils/IncorporaCredentials.php
@@ -22,7 +22,7 @@
  */
 $incorporaCredentials = array(
     'INCORPORA_URL' => 'https://intranet-incorpora.fundacionlacaixa.org',
-    'INCORPORA_TEST_URL' => 'https://pre.intranet-incorpora.fundacionlacaixa.org:445',
+    'INCORPORA_TEST_URL' => 'https://pre.intranet-incorpora.fundacionlacaixa.org/Incorpora/',
     'INCORPORA_TEST_USER_REFERENCE_GROUP' => '10460',
     'INCORPORA_TEST_USER_REFERENCE_ENTITY' => '216',
     'INCORPORA_TEST_USER_REFERENCE_OFFICER' => '39728',
