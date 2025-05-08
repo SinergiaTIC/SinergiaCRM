5cbb058901aad9f5c53299142f1b76acb845e73e
AlbertoSTIC <alberto@sinergiacrm.org>
2025-05-08 12:07:58 +0000

    fix user

--- modules/stic_Incorpora/utils/IncorporaCredentials.php
+++ modules/stic_Incorpora/utils/IncorporaCredentials.php
@@ -26,8 +26,8 @@ $incorporaCredentials = array(
     'INCORPORA_TEST_USER_REFERENCE_GROUP' => '10460',
     'INCORPORA_TEST_USER_REFERENCE_ENTITY' => '216',
     'INCORPORA_TEST_USER_REFERENCE_OFFICER' => '39728',
-    'INCORPORA_TEST_USER' => 'userinc',
-    'INCORPORA_TEST_PASSWORD' => 'qaH4VCJo0E7Rpy', 
+    'INCORPORA_TEST_USER' => 'formacion.tecnico2',
+    'INCORPORA_TEST_PASSWORD' => '666666', 
     // If there is HTTP authentication, we need these credentials
     // 'INCORPORA_TEST_HTTP_USER' => 'userinc',
     // 'INCORPORA_TEST_HTTP_PASSWORD' => 'qaH4VCJo0E7Rpy',
