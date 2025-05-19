84e7406b68e355095c573f512b28736927871bf2
Enric Prats <enric@sinergiacrm.org>
2025-05-19 10:46:51 +0000

    minor formatting correction

--- modules/stic_Bookings/metadata/detailviewdefs.php
+++ modules/stic_Bookings/metadata/detailviewdefs.php
@@ -68,7 +68,8 @@ array(
                     ),
                     1 => array(
                     ),
-                ),                2 => array(
+                ),
+                2 => array(
                     0 => array(
                         'name' => 'stic_bookings_contacts_name',
                     ),
