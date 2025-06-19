e57cd1989d0448415b0648e0933dfa0856d47017
ainaraRT <ainaretework@gmail.com>
2025-06-19 10:58:12 +0000

    change the migration

--- SticUpdates/Migrations/20250130_feature_TrackerModule.sql
+++ SticUpdates/Migrations/20250130_feature_TrackerModule.sql
@@ -1,7 +1,3 @@
 -- Actualiza los registros existentes que no tienen el assigned_user_id o es nulo, añadiéndole el user_id
 
 UPDATE `tracker` SET `assigned_user_id` = `user_id` WHERE `assigned_user_id` IS NULL OR `assigned_user_id` = '';
-
--- Actualiza los registros existentes que el action es editview cambiandolo por update
-
-UPDATE `tracker` SET `action` = 'update' WHERE `action` = 'editview';
