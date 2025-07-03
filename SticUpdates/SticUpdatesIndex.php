59c8886d3aad1a83bc36b8eab18f42eab5ab15ed
jordiSTIC <jordi@sinergiacrm.org>
2025-07-03 09:17:31 +0000

    Ampliada info en update_index, eliminada carpeta BeforeUpdate

--- SticUpdates/SticUpdatesIndex.php
+++ SticUpdates/SticUpdatesIndex.php
@@ -1,25 +1,4 @@
 <?php
-/**
- * This file is part of SinergiaCRM.
- * SinergiaCRM is a work developed by SinergiaTIC Association, based on SuiteCRM.
- * Copyright (C) 2013 - 2023 SinergiaTIC Association
- *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Affero General Public License version 3 as published by the
- * Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Affero General Public License along with
- * this program; if not, see http://www.gnu.org/licenses or write to the Free
- * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301 USA.
- *
- * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
- */
 
 if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
@@ -37,13 +16,13 @@ $stic_updates_index = [
             // 'repair',
             'SticUpdates/Migrations/20250130_feature_TrackerModule.sql',
             // 'sda_rebuild',
-            'SticUpdates/Migrations/20250617_enhancement_glTranslations.sql'
+            'SticUpdates/Migrations/20250617_enhancement_glTranslations.sql',
         ],
         'finally' => [
             'repair',
             'css',
             'sda_rebuild',
-            'delete_cache',
+            'cache_rebuild', // Borrar y regenerar (para los css de los formularios web)
         ],
     ],
 
@@ -63,7 +42,7 @@ $stic_updates_index = [
             'repair',
             // 'css',
             'sda_rebuild',
-            'delete_cache',
+            'cache_rebuild',
         ],
     ],
 
@@ -82,7 +61,7 @@ $stic_updates_index = [
             'repair',
             'css',
             'sda_rebuild',
-            'delete_cache',
+            'cache_rebuild',
         ],
     ],
 ];
