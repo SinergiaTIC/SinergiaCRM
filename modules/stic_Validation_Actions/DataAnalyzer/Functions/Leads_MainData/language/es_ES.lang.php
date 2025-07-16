15d8308569150bffe1631ec97e6eb44bae8af6cf
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 01:16:28 +0200

    New translations en_us.lang.php (Spanish)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/Leads_MainData/language/es_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/Leads_MainData/language/es_ES.lang.php
@@ -21,7 +21,7 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-    'NAME' => 'Interesados - Revisión de los datos principales',		// Esta etiqueta es obligatoria. Indica el nombre de la función
+    'NAME' => 'Interesados - Revisión de los datos principales',		// Required label. Contains function name
     'NO_VALID_CIF' => 'No es un CIF válido.',
 	'NO_VALID_NIF_NIE' => 'No es un NIF/NIE válido.',
 	'UPDATED' => 'Se ha limpiado el campo para eliminar caràcteres no válidos.',
