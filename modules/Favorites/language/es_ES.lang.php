cad12d7d320510d117f925527c34b8189f62e4d4
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 00:58:04 +0200

    New translations en_us.lang.php (Spanish)

--- modules/Favorites/language/es_ES.lang.php
+++ modules/Favorites/language/es_ES.lang.php
@@ -5,7 +5,7 @@
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ * Copyright (C) 2011 - 2024 SalesAgility Ltd.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -70,5 +70,5 @@ $mod_strings = array(
     'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Actividades',
     'LBL_NEW_FORM_TITLE' => 'Nuevo Favorito',
     'LBL_MARK_FAVORITE' => 'Marcar como Favorito',
-    'LBL_UNMARK_FAVORITE' => 'Desmarcar Favorito',    
+    'LBL_UNMARK_FAVORITE' => 'Desmarcar Favorito',
 );
