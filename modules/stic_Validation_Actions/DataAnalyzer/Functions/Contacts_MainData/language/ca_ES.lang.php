c38d5f6c631b71019635f2644416a1f432ae9e71
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 01:15:39 +0200

    New translations en_us.lang.php (Catalan)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/Contacts_MainData/language/ca_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/Contacts_MainData/language/ca_ES.lang.php
@@ -21,7 +21,7 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-    'NAME' => 'Persones - Revisió de les dades principals',		// Aquesta etiqueta és obligatòria. Indica el nom de la funció
+    'NAME' => 'Persones - Revisió de les dades principals',		// Required label. Contains function name
     'NO_VALID' => 'L\'identificador no és un NIF/NIE vàlid.',
     'UPDATED' => 'S\'ha netejat el camp per esborrar els caràcters no vàlids.',
 	'CIF_NOT_ACCEPTED' => 'El tipus d\'identificació de la persona no pot ser un CIF.',
