4e9f98569871cefbdc223d81b92aa63aafd2d7b1
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 01:17:19 +0200

    New translations en_us.lang.php (Catalan)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/PaymentsCommitments_reportFailedRecurringPC/language/ca_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/PaymentsCommitments_reportFailedRecurringPC/language/ca_ES.lang.php
@@ -22,6 +22,6 @@
  */
 $func_strings = array (
     'NAME' => 'Compromisos de pagament - Revisió de les autoritzacions recurrents incompletes',
-    'NO_ROWS' => "No s'ha trobat cap compromís de pagament .",
+    'NO_ROWS' => 'No s\'ha trobat cap compromís de pagament .',
     'RESULT_INFO' => 'Aquest compromís de pagament recurrent amb @payment_method@ no té la informació necessària de la pasarel·la de pagament i té @payment_count@ pagaments en estat pendent que haurien de ser revisats. És possible que calgui donar de baixa el compromís de pagament.',
 );
