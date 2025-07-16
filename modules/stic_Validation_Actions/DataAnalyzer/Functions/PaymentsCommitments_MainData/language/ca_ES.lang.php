82054a559127be581d1fc66d1a72c7831ac5b1c0
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 01:16:59 +0200

    New translations en_us.lang.php (Catalan)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/PaymentsCommitments_MainData/language/ca_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/PaymentsCommitments_MainData/language/ca_ES.lang.php
@@ -20,9 +20,9 @@
  *
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
-$func_strings = array(
-    'NAME' => 'Compromisos de pagament - Revisió de les dades principals', // Aquesta etiqueta és obligatòria. Indica el nom de la funció.
-    'UPDATED_IBAN' => ' ha estat actualitzat per eliminar els caràcters no vàlids.',
+$func_strings = array (
+    'NAME' => 'Compromisos de pagament - Revisió de les dades principals',		// Required label. Contains function name
     'UPDATED_IBAN' => ' era erroni, s\'ha actualitzat automàticament.',
-    'UPDATED_MANDATE' => ' estava buit o no era vàlid, s\'ha actualitzat automàticament per corregir-lo.',
+    'UPDATED_IBAN' => ' was wrong, it has been automatically updated.',
+    'UPDATED_MANDATE' => ' estava buit o no era vàlid, s\'ha actualitzat automàticament per corregir-lo.',    
 );
