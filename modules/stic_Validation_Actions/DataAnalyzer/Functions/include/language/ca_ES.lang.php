e6a12dc9f6f6814bf5bc952982995bd2cfac65d4
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 01:19:09 +0200

    New translations en_us.lang.php (Catalan)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/include/language/ca_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/include/language/ca_ES.lang.php
@@ -20,12 +20,12 @@
  *
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
-$func_strings = array (
+$func_strings = array(
     'NO_ROWS' => 'No s\'ha trobat cap registre per revisar.',
     'NO_ERRORS' => 'No s\'ha detectat cap error als registres revisats.',
     'ERR_FIELD' => ' amb valor no vàlid.',
-    'WARN_FIELD' => ' amb valor inferior o igual a zero.',    
+    'WARN_FIELD' => ' amb valor inferior o igual a zero.',
     'EMPTY_REL' => 'Sense relació activa',
     'INVALID_VALUE' => 'Valor erroni',
-    'LIST_VIEW' => 'Vista de llista',    
+    'LIST_VIEW' => 'Vista de llista',
 );
