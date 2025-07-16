9e59cf9c6d6fd197ae434793c0191d96b1d92cd6
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 01:18:58 +0200

    New translations en_us.lang.php (Catalan)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/include/FunctionModel/language/ca_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/include/FunctionModel/language/ca_ES.lang.php
@@ -21,5 +21,5 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-  'NAME' => 'Nom de la funció',		// Aquesta etiqueta és obligatòria. Indica el nom de la funció
+  'NAME' => 'Nom de la funció',		// Required label. Contains function name
 );
