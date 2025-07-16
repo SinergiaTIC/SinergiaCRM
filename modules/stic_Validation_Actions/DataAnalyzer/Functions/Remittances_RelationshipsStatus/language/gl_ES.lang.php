bec6ebc41088dfdc909ad013d9ef84649c6fb5c5
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 01:18:23 +0200

    New translations en_us.lang.php (Galician)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/Remittances_RelationshipsStatus/language/gl_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/Remittances_RelationshipsStatus/language/gl_ES.lang.php
@@ -21,7 +21,7 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array(
-    'NAME' => 'Remesas - Revisión das relacións', // Esta etiqueta é obrigatoria. Indica o nome da función
+    'NAME' => 'Remesas - Revisión das relacións', // Required label. Contains function name
     'NO_ERRORS' => 'Non se atoparon erros.',
     'ERROR_STATUS_1' => 'O estado do pago ',
     'ERROR_STATUS_2' => ' é incoherente co da remesa.',
