55490053e32f527b24d28c9038229d0eeb8b708a
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 01:02:36 +0200

    New translations en_us.lang.php (Spanish)

--- modules/ProjectTask/language/es_ES.lang.php
+++ modules/ProjectTask/language/es_ES.lang.php
@@ -79,6 +79,7 @@ $mod_strings = array(
     'LBL_ACTUAL_EFFORT' => 'Trabajo Real (h):',
     'LBL_UTILIZATION' => 'Ocupación (%):',
     'LBL_DELETED' => 'Eliminada:',
+    'LBL_IMPORT_PROJECT_TASKS' => 'Import Project Tasks',
 
     'LBL_LIST_NAME' => 'Nombre',
     'LBL_LIST_PARENT_NAME' => 'Proyecto',
