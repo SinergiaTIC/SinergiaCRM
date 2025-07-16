cba5c3c26c56b2701ed193b0147ddddc3f869b9b
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 01:14:28 +0200

    New translations en_us.lang.php (Catalan)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/Accounts_Duplicates/language/ca_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/Accounts_Duplicates/language/ca_ES.lang.php
@@ -21,7 +21,7 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-    'NAME' => 'Organitzacions - Cerca de duplicats',		// Aquesta etiqueta és obligatòria. Indica el nom de la funció
+    'NAME' => 'Organitzacions - Cerca de duplicats',		// Required label. Contains function name
     'NO_DUPLICATED' => 'No s\'han detectat duplicats.',
     'POSSIBLE_DUP' => 'Coincideixen al camp ',
 );
