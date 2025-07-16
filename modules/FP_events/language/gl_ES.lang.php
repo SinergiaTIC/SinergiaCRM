5e38dac5213aed18c99bf8bf462ece6651c56229
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 00:58:01 +0200

    New translations en_us.lang.php (Galician)

--- modules/FP_events/language/gl_ES.lang.php
+++ modules/FP_events/language/gl_ES.lang.php
@@ -41,7 +41,7 @@
 if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
-
+                
 $mod_strings = array(
     'LBL_ASSIGNED_TO_ID' => 'Id de usuario asignado',
     'LBL_ASSIGNED_TO_NAME' => 'Asignado a',
@@ -129,5 +129,5 @@ $mod_strings = array(
     // Email links
     'LBL_ACCEPT_LINK' => 'Aceptar',
     'LBL_DECLINE_LINK' => 'Rexeitar',
-
+    
 );
