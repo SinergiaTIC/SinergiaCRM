0780bacd1fa0782f3f677b24326adcdbb1b73e81
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 00:54:30 +0200

    New translations en_us.lang.php (Spanish)

--- modules/Configurator/language/es_ES.lang.php
+++ modules/Configurator/language/es_ES.lang.php
@@ -54,7 +54,9 @@ $mod_strings = array(
     'DEFAULT_SYSTEM_SETTINGS' => 'Interfaz de Usuario',
     'DEFAULT_THEME' => 'Tema predeterminado',
     'DEFAULT_TIME_FORMAT' => 'Formato de hora predeterminado',
+
     'DISPLAY_RESPONSE_TIME' => 'Mostrar los tiempos de respuesta del servidor',
+
     'IMAGES' => 'Logos',
     'LBL_ALLOW_USER_TABS' => 'Permitir a los usuarios ocultar pestañas',
     'LBL_CONFIGURE_SETTINGS_TITLE' => 'Configuración del Sistema',
@@ -86,6 +88,7 @@ $mod_strings = array(
     'LBL_MODULE_TITLE' => 'Interfaz de usuario',
     'LBL_NOTIFY_FROMADDRESS' => 'Dirección del remitente:',
     'LBL_NOTIFY_SUBJECT' => 'Asunto de correo:',
+
     'LBL_PROXY_AUTH' => '¿Autenticación?',
     'LBL_PROXY_HOST' => 'Servidor Proxy',
     'LBL_PROXY_ON_DESC' => 'Configura la dirección del servidor proxy y la configuración de la autenticación',
@@ -186,6 +189,7 @@ $mod_strings = array(
     'LBL_LOGGER_FILENAME' => 'Nombre de Archivo de Traza',
     'LBL_LOGGER_FILE_EXTENSION' => 'Extensión',
     'LBL_LOGGER_MAX_LOG_SIZE' => 'Tamaño máximo de traza',
+    'LBL_STACK_TRACE' => 'Enable stack trace',
     'LBL_LOGGER_DEFAULT_DATE_FORMAT' => 'Formato de fecha por defecto',
     'LBL_LOGGER_LOG_LEVEL' => 'Nivel de Traza',
     'LBL_LEAD_CONV_OPTION' => 'Opciones de conversión del cliente potencial',
@@ -198,121 +202,6 @@ $mod_strings = array(
     'LBL_IMPORT_MAX_RECORDS' => 'Importación - Número máximo de registros:',
     'LBL_IMPORT_MAX_RECORDS_HELP' => 'Especificar cuántas filas se permiten dentro de los archivos a importar.<br>Si el número de filas en un archivo de importación supera este número, el usuario recibirá una alerta.<br>Si no se introduce un valor se tendra un número ilimitado de filas.',
     'vCAL_HELP' => 'Utilice esta opción para determinar el número de meses de antelación sobre la fecha actual con la que se publica la información relativa al estado de Disponible/Ocupado sobre llamadas y reuniones.<BR>Para desactivar la publicación del estado Disponible/Ocupado, introduzca "0".  El mínimo es 1 mes; el máximo 12 meses.',
-    'LBL_PDFMODULE_NAME' => 'Configuración PDF',
-    'SUITEPDF_BASIC_SETTINGS' => 'Propiedades del Documento',
-    'SUITEPDF_ADVANCED_SETTINGS' => 'Configuración Avanzada',
-    'SUITEPDF_LOGO_SETTINGS' => 'Imágenes',
-
-    'PDF_AUTHOR' => 'Autor',
-    'PDF_AUTHOR_INFO' => 'El Autor aparece en las propiedades del documento.',
-
-    'PDF_HEADER_LOGO' => 'Para Documentos PDF de Presupuestos',
-    'PDF_HEADER_LOGO_INFO' => 'Esta imagen aparece en la cabecera por defecto de los documentos PDF de presupuestos.',
-
-    'PDF_NEW_HEADER_LOGO' => 'Seleccione una nueva Imagen para las presupuestos',
-    'PDF_NEW_HEADER_LOGO_INFO' => 'El formato de archivo puede ser .jpg o .png. (Sólo .jpg para EZPDF)<BR>El tamaño recomendado es 867x60 px.',
-
-    'PDF_SMALL_HEADER_LOGO' => 'Para Documentos PDF de Informes',
-    'PDF_SMALL_HEADER_LOGO_INFO' => 'Esta imagen aparece en la Cabecera por defecto de los Documentos PDF de Informes.<br> Esta imagen también aparece en la esquina superior izquierda de la aplicación SuiteCRM.',
-
-    'PDF_NEW_SMALL_HEADER_LOGO' => 'Seleccione una nueva imagen para informes',
-    'PDF_NEW_SMALL_HEADER_LOGO_INFO' => 'El formato de archivo puede ser .jpg o .png. (Sólo .jpg para EZPDF)<BR>El tamaño recomendado es 212x40 px.',
-
-    'PDF_FILENAME' => 'Nombre de Archivo Por Defecto',
-    'PDF_FILENAME_INFO' => 'Nombre de archivo por defecto para los archivos PDF generados',
-
-    'PDF_TITLE' => 'Título',
-    'PDF_TITLE_INFO' => 'El título aparece en las propiedades del documento.',
-
-    'PDF_SUBJECT' => 'Asunto',
-    'PDF_SUBJECT_INFO' => 'El Asunto aparece en las propiedades del documento.',
-
-    'PDF_KEYWORDS' => 'Palabra(s) clave',
-    'PDF_KEYWORDS_INFO' => 'Asociar Palabras clave con el documento, generalmente en la forma "palabra1 palabra2..."',
-
-    'PDF_COMPRESSION' => 'Compresión',
-    'PDF_COMPRESSION_INFO' => 'Activa o desactiva la compresión de página. <br>Cuando ha sido activada, la represnetación interna de cada página se comprime, llevando a niveles de ratios de compresión de aprox. 2 para el documento resultante.',
-
-    'PDF_JPEG_QUALITY' => 'Calidad JPEG (1-100)',
-    'PDF_JPEG_QUALITY_INFO' => 'Establece la calidad de compresión JPEG por defecto (1-100)',
-
-    'PDF_PDF_VERSION' => 'Versión PDF',
-    'PDF_PDF_VERSION_INFO' => 'Establece la versión de PDF (consulte la referencia PDF para valores válidos).',
-
-    'PDF_PROTECTION' => 'Protección de Documento',
-    'PDF_PROTECTION_INFO' => 'Establece la protección de documento<br>- copiar: copiar texto e imágenes al portapapeles<br>- imprimir: imprimir el documento<br>- modificar: modificar el documento (excepto las anotaciones y formularios)<br>- anot.-forms.: añadir anotaciones y formularios<br>Nota: la protección ante la modificación es para gente que posee el producto Acrobat completo.',
-
-    'PDF_USER_PASSWORD' => 'Contraseña de Usuario',
-    'PDF_USER_PASSWORD_INFO' => 'Si no establece ninguna contraseña, el documento se abrirá como de costumbre. <br>Si establece una contraseña de usuario, el visor PDF se la solicitará antes de mostrar el documento. <br>Si la contraseña maestra es diferente de la de usuario podrá utilizarla para obtener acceso completo.',
-
-    'PDF_OWNER_PASSWORD' => 'Contraseña de Propietario',
-    'PDF_OWNER_PASSWORD_INFO' => 'Si no establece ninguna contraseña, el documento se abrirá como de costumbre. <br>Si establece una contraseña de usuario, el visor PDF se la solicitará antes de mostrar el documento. <br>Si la contraseña maestra es diferente de la de usuario podrá utilizarla para obtener acceso completo.',
-
-    'PDF_ACL_ACCESS' => 'Control de Acceso',
-    'PDF_ACL_ACCESS_INFO' => 'Control de Acceso por defecto para la generación del PDF.',
-
-    'K_CELL_HEIGHT_RATIO' => 'Ratio de Altura de la Celda',
-    'K_CELL_HEIGHT_RATIO_INFO' => 'Si la altura de una celda es menor que (Altura de la Fuente x Ratio de Altura de la Celda), entonces se utilizará (Altura de la Fuente x Ratio de Altura de la Celda) como la altura de la celda.<br>(Altura de la Fuente x Ratio de Altura de la Celda) también se utiliza como la altura de la celda cuando no hay ninguna altura definida.',
-
-    'K_SMALL_RATIO' => 'Coeficiente para Fuentes Pequeñas',
-    'K_SMALL_RATIO_INFO' => 'Coeficiente de Reducción para fuentes pequeñas.',
-
-    'PDF_IMAGE_SCALE_RATIO' => 'Ratio de escalado de imagen',
-    'PDF_IMAGE_SCALE_RATIO_INFO' => 'Ratio utilizado para escalar las imágenes',
-
-    'PDF_UNIT' => 'Unidad',
-    'PDF_UNIT_INFO' => 'unidad de medida del documento',
-    'PDF_GD_WARNING' => 'No ha instalado la librería GD para PHP. Sin la librería GD, sólo se mostrarán los logos JPEG en los documentos PDF.',
-    'ERR_EZPDF_DISABLE' => 'Aviso : La clase EZPDF ha sido deshabilitada en la tabla de configuración y está establecida como la clase para PDF. Por favor, "Guarde" este formulario para establecer TCPDF como la Clase PDF y vuelva en un estado estable.',
-    'LBL_IMG_RESIZED' => "(redimensionado para ser mostrado)",
-
-
-    'LBL_FONTMANAGER_BUTTON' => 'Administrador de Fuentes PDF',
-    'LBL_FONTMANAGER_TITLE' => 'Administrador de Fuentes PDF',
-    'LBL_FONT_BOLD' => 'Negrita',
-    'LBL_FONT_ITALIC' => 'Cursiva',
-    'LBL_FONT_BOLDITALIC' => 'Negrita/Cursiva',
-    'LBL_FONT_REGULAR' => 'Normal',
-
-    'LBL_FONT_TYPE_CID0' => 'CID-0',
-    'LBL_FONT_TYPE_CORE' => 'Núcleo',
-    'LBL_FONT_TYPE_TRUETYPE' => 'TrueType',
-    'LBL_FONT_TYPE_TYPE1' => 'Tipo 1',
-    'LBL_FONT_TYPE_TRUETYPEU' => 'TrueTypeUnicode',
-
-    'LBL_FONT_LIST_NAME' => 'Nombre',
-    'LBL_FONT_LIST_FILENAME' => 'Nombre de archivo',
-    'LBL_FONT_LIST_TYPE' => 'Tipo',
-    'LBL_FONT_LIST_STYLE' => 'Estilo',
-    'LBL_FONT_LIST_STYLE_INFO' => 'Estilo de la fuente',
-    'LBL_FONT_LIST_ENC' => 'Codificación',
-    'LBL_FONT_LIST_EMBEDDED' => 'Incrustado',
-    'LBL_FONT_LIST_EMBEDDED_INFO' => 'Marque esta opción para incrustar la fuente en el archivo PDF',
-    'LBL_FONT_LIST_CIDINFO' => 'Información CID',
-    'LBL_FONT_LIST_CIDINFO_INFO' => 'Para ejemplos y más ayuda: www.tcpdf.org',
-    'LBL_FONT_LIST_FILESIZE' => 'Tamaño de Fuente (KB)',
-    'LBL_ADD_FONT' => 'Añadir una fuente',
-    'LBL_BACK' => 'Atrás',
-    'LBL_REMOVE' => 'Quitar',
-    'LBL_JS_CONFIRM_DELETE_FONT' => '¿Está seguro de que desea eliminar esta fuente?',
-
-    'LBL_ADDFONT_TITLE' => 'Añadir una Fuente PDF',
-    'LBL_PDF_ENCODING_TABLE' => 'Tabla de Codificación',
-    'LBL_PDF_ENCODING_TABLE_INFO' => 'Nombre de la tabla de codificación.<br>Esta opción es ignorada en Unicode TrueType, Unicode OpenType y fuentes con símbolos.<br>La codificación define la asociación entre un código (de 0 a 255) y un carácter contenido en la fuente.<br>Los primeros 128 son fijos y se corresponden con ASCII.',
-    'LBL_PDF_FONT_FILE' => 'Archivo de Fuente',
-    'LBL_PDF_FONT_FILE_INFO' => 'archivo .ttf o .otf o .pfb',
-    'LBL_PDF_METRIC_FILE' => 'Archivo de Métrica',
-    'LBL_PDF_METRIC_FILE_INFO' => 'archivo .afm o .ufm',
-    'LBL_ADD_FONT_BUTTON' => 'Añadir',
-    'JS_ALERT_PDF_WRONG_EXTENSION' => 'Este archivo no tiene una extensión de archivo adecuada.',
-
-    'ERR_MISSING_CIDINFO' => 'El campo Información CID Information no puede estar vacío.',
-    'LBL_ADDFONTRESULT_TITLE' => 'Resultado del proceso de adición de fuente',
-    'LBL_STATUS_FONT_SUCCESS' => 'ÉXITO : La fuente ha sido añadida a SuiteCRM.',
-    'LBL_STATUS_FONT_ERROR' => 'ERROR : La fuente no ha sido añadida. Mire en el siguiente registro.',
-
-// Font manager
-    'ERR_PDF_NO_UPLOAD' => 'Error durante la subida del archivo de fuente o de métricas.',
 
 // Wizard
     //Wizard Scenarios
@@ -338,10 +227,4 @@ $mod_strings = array(
     'LBL_WELCOME' => 'Bienvenido' /*for 508 compliance fix*/,
     'LBL_LOGO' => 'Logotipo' /*for 508 compliance fix*/,
     'LBL_ENABLE_HISTORY_CONTACTS_EMAILS' => 'Muestra los emails de contactos relacionados en el subpanel History para módulos',
-
-    // Google auth PR 6146
-    'LBL_GOOGLE_AUTH_TITLE' => 'Autenticación de Google',
-    'LBL_GOOGLE_AUTH_JSON' => 'Archivo JSON',
-    'LBL_GOOGLE_AUTH_JSON_HELP' => 'Cargar el archivo JSON descargado desde consola de los desarrolladores de Google.',
-
 );
