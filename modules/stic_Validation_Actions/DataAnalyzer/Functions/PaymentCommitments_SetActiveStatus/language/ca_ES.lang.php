646a5d91aaaea45338a2a11802b429132b2ebc86
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 01:16:40 +0200

    New translations en_us.lang.php (Catalan)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/PaymentCommitments_SetActiveStatus/language/ca_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/PaymentCommitments_SetActiveStatus/language/ca_ES.lang.php
@@ -21,7 +21,7 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-    'NAME' => 'Compromisos de pagament - Càlcul de registre actiu/inactiu', // Aquesta etiqueta és obligatòria. Indica el nom de la funció
-    'NO_ROWS' => "Tots els compromisos de pagament tenen l'estat actiu/inactiu correcte.",
-    'UPDATED' => "S'ha actualitzat el camp Actiu.",
+    'NAME' => 'Compromisos de pagament - Càlcul de registre actiu/inactiu', // This label is mandatory. Indicates the name of the function
+    'NO_ROWS' => 'Tots els compromisos de pagament tenen l\'estat actiu/inactiu correcte.',
+    'UPDATED' => 'S\'ha actualitzat el camp Actiu.',
 );
