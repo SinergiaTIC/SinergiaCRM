f384930299e7f92d952af36301c3830edc1c2353
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 01:17:56 +0200

    New translations en_us.lang.php (Spanish)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/Registrations_RelationshipsStatus/language/es_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/Registrations_RelationshipsStatus/language/es_ES.lang.php
@@ -21,7 +21,7 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-    'NAME' => 'Inscripciones - Revisión de las relaciones',		// Esta etiqueta es obligatoria. Indica el nombre de la función
+    'NAME' => 'Inscripciones - Revisión de las relaciones',		// Required label. Contains function name
     'NO_LINK' => 'No tiene ninguna persona, organización o interesado vinculado.',
     'NO_EVENT' => 'No tiene ningún evento vinculado.',
 );
