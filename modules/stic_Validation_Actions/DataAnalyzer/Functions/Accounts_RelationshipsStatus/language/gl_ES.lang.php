6fd0c9f208a9d415bd2e0bfee85604eb05365ea4
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 01:14:53 +0200

    New translations en_us.lang.php (Galician)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/Accounts_RelationshipsStatus/language/gl_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/Accounts_RelationshipsStatus/language/gl_ES.lang.php
@@ -21,7 +21,7 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-    'NAME' => 'Organizacións - Revisión do tipo de relación',		// Esta etiqueta é obrigatoria. Indica o nome da función
+    'NAME' => 'Organizacións - Revisión do tipo de relación',		// Required label. Contains function name
     'NO_ERRORS' => 'Todas as organizacións teñen o tipo de relación correcto.',
     'UPDATED' => 'Actualizouse o campo Tipos de relacións actuais:',
 );
