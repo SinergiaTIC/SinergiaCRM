9114476ba4b9c499e8692784f46e51b0e5b4d620
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 01:17:14 +0200

    New translations en_us.lang.php (Galician)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/PaymentsCommitments_RelationshipsStatus/language/gl_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/PaymentsCommitments_RelationshipsStatus/language/gl_ES.lang.php
@@ -21,7 +21,7 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-    'NAME' => 'Compromisos de pago - Revisión das relacións',		// Esta etiqueta é obrigatoria. Indica o nome da función
+    'NAME' => 'Compromisos de pago - Revisión das relacións',		// Required label. Contains function name
     'NO_ROWS' => 'Non hai compromiso de pagar sen unha relación válida.',
     'NO_LINK' => 'Non ten ningunha persoa nin organización vinculada ou a que ten non é válida.',
 );
