a70fab91360318edb194e172b24f636b00b2f9cb
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 01:14:18 +0200

    New translations en_us.lang.php (Catalan)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/AccountsRelationships_SetActiveStatus/language/ca_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/AccountsRelationships_SetActiveStatus/language/ca_ES.lang.php
@@ -21,7 +21,7 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-    'NAME' => 'Relacions amb Organitzacions - Càlcul de registre actiu/inactiu',		// Esta etiqueta es obligatoria. Indica el nombre de la función
-    'NO_ROWS' => "Totes les relacions amb organitzacions tenen l'estat actiu/inactiu correcte.",
-    'UPDATED' => "S'ha actualitzat el camp Actiu.",
+    'NAME' => 'Relacions amb Organitzacions - Càlcul de registre actiu/inactiu', // This label is mandatory.Indicates the name of the function
+    'NO_ROWS' => 'Totes les relacions amb organitzacions tenen l\'estat actiu/inactiu correcte.',
+    'UPDATED' => 'S\'ha actualitzat el camp Actiu.'
 );
