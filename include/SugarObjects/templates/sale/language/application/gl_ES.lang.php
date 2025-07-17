c9002f11f7e5742e047bb5141f95ffd52ffd70bd
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 12:01:47 +0200

    New translations en_us.lang.php (Galician)

--- include/SugarObjects/templates/sale/language/application/gl_ES.lang.php
+++ include/SugarObjects/templates/sale/language/application/gl_ES.lang.php
@@ -46,8 +46,8 @@ $app_list_strings = array(
 
     strtolower($object_name) . '_type_dom' => array(
         '' => '',
-        'Existing Business' => 'Negocios Existentes',
-        'New Business' => 'Novos Negocios',
+        'Existing Business' => 'Existing Business',
+        'New Business' => 'New Business',
     ),
 
 );
