28e2ae4fb0362af98ab1d8242a001d30b491bc25
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 14:04:38 +0200

    New translations es_es.lang.php (Galician)

--- modules/SavedSearch/language/gl_ES.lang.php
+++ modules/SavedSearch/language/gl_ES.lang.php
@@ -43,21 +43,21 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_MODULE_TITLE' => 'My Saved Filters',
-    'LBL_SEARCH_FORM_TITLE' => 'My Saved Filters : Filter',
-    'LBL_LIST_FORM_TITLE' => 'My Saved Filters List',
-    'LBL_DELETE_CONFIRM' => 'Are you sure you want to delete the selected Saved Filter?',
-    'LBL_DELETE_BUTTON_TITLE' => 'Delete this Saved Filter',
-    'LBL_SAVE_BUTTON_TITLE' => 'Save the current filter',
-    'LBL_LIST_NAME' => 'Name',
-    'LBL_LIST_MODULE' => 'Module',
-    'LBL_ORDER_BY_COLUMNS' => 'Order by column:',
-    'LBL_DIRECTION' => 'Direction:',
-    'LBL_SAVE_SEARCH_AS' => 'Save filter as:',
-    'LBL_ASCENDING' => 'Ascending',
-    'LBL_DESCENDING' => 'Descending',
-    'LBL_MODIFY_CURRENT_FILTER' => 'Modify current filter',
+    'LBL_MODULE_TITLE' => 'Os Meus filtros gardados',
+    'LBL_SEARCH_FORM_TITLE' => 'Os Meu filtros gardados: filtro',
+    'LBL_LIST_FORM_TITLE' => 'A Miña lista de filtros gardados',
+    'LBL_DELETE_CONFIRM' => '¿Está seguro de que desexa eliminar o Filtro Gardado Seleccionado?',
+    'LBL_DELETE_BUTTON_TITLE' => 'Eliminar este filtro gardado',
+    'LBL_SAVE_BUTTON_TITLE' => 'Gardar o filtro actual',
+    'LBL_LIST_NAME' => 'Nome',
+    'LBL_LIST_MODULE' => 'Módulo',
+    'LBL_ORDER_BY_COLUMNS' => 'Ordear por columna:',
+    'LBL_DIRECTION' => 'Enderezo:',
+    'LBL_SAVE_SEARCH_AS' => 'Gardar o filtro como:',
+    'LBL_ASCENDING' => 'Ascendente',
+    'LBL_DESCENDING' => 'Descendente',
+    'LBL_MODIFY_CURRENT_FILTER' => 'Modificar filtro actual',
 
-    'LBL_CREATED_BY' => 'Created By',
+    'LBL_CREATED_BY' => 'Creado Por',
 
 );
