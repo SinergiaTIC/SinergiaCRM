aa0f20892711247df78ecdcbb24dfb4bdcbd4f19
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 14:11:07 +0200

    New translations es_es.lang.php (Basque)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/General_DeleteInactiveRelations/language/eu_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/General_DeleteInactiveRelations/language/eu_ES.lang.php
@@ -21,5 +21,5 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-    'NAME' => 'General - Delete outdated relationships', // Required label. Contains function name
- );
+    'NAME' => 'General - Eliminar relaciones obsoletas',		// Esta etiqueta es obligatoria. Indica el nombre de la función
+);
