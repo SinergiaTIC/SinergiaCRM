9268d0b3505008c3d0a61b0cb59af6abb8d169ef
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 13:41:57 +0200

    New translations es_es.lang.php (Galician)

--- modules/EmailAddresses/language/gl_ES.lang.php
+++ modules/EmailAddresses/language/gl_ES.lang.php
@@ -44,12 +44,12 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
     'LBL_EMAIL_ADDRESS_ID' => 'ID',
-    'LBL_EMAIL_ADDRESS' => 'Email Address',
-    'LBL_EMAIL_ADDRESS_CAPS' => 'Email Address caps',
-    'LBL_INVALID_EMAIL' => 'Invalid Email',
-    'LBL_OPT_OUT' => 'Opted Out',
-    'LBL_CONFIRM_OPT_IN' => 'Confirm Opt In',
-    'LBL_DATE_CREATE' => 'Date Create',
-    'LBL_DATE_MODIFIED' => 'Date Modified',
-    'LBL_DELETED' => 'Delete',
+    'LBL_EMAIL_ADDRESS' => 'Enderezo de Email',
+    'LBL_EMAIL_ADDRESS_CAPS' => 'Maiúsculas en Enderezo de Email',
+    'LBL_INVALID_EMAIL' => 'Email non Válido',
+    'LBL_OPT_OUT' => 'Rehusado',
+    'LBL_CONFIRM_OPT_IN' => 'Confirmar Autorización',
+    'LBL_DATE_CREATE' => 'Data de Creación',
+    'LBL_DATE_MODIFIED' => 'Data de Modificación',
+    'LBL_DELETED' => 'Eliminar',
 );
