9d1a338be2030e8fda2f5f18cdd623a31c56d8a1
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 14:11:32 +0200

    New translations es_es.lang.php (Basque)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/PaymentsCommitments_MainData/language/eu_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/PaymentsCommitments_MainData/language/eu_ES.lang.php
@@ -21,8 +21,8 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-    'NAME' => 'Payment commitments - Check main fields',		// Required label. Contains function name
-    'UPDATED_IBAN' => ' has been updated to clean invalid chars',
-    'UPDATED_IBAN' => ' was wrong, it has been automatically updated.',
-    'UPDATED_MANDATE' => ' was empty or invalid, it has been automatically updated.',    
+    'NAME' => 'Compromisos de pago - Revisión de los datos principales',		// Esta etiqueta es obligatoria. Indica el nombre de la función
+    'UPDATED_IBAN' => ' se ha actualizado para eliminar caracteres no válidos.',
+    'UPDATED_IBAN' => ' con valor erróneo, se ha actualizado automáticamente.',
+    'UPDATED_MANDATE' => ' estaba vacío o no era válido, se ha actualizado automáticamente para corregirlo.',
 );
