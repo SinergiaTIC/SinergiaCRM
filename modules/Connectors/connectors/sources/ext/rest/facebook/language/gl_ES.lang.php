24fe9be3d446090e1f5aebabb4b9779baecaa208
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 13:40:55 +0200

    New translations es_es.lang.php (Galician)

--- modules/Connectors/connectors/sources/ext/rest/facebook/language/gl_ES.lang.php
+++ modules/Connectors/connectors/sources/ext/rest/facebook/language/gl_ES.lang.php
@@ -44,10 +44,9 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $connector_strings = array(
     //Vardef labels
-    'LBL_LICENSING_INFO' => '<table border="0" cellspacing="1"><tr><th valign="top" width="35%" class="dataLabel">Facebook Application Information </th></tr>
-                                    <tr><td width="35%" class="dataLabel">You will need to create a facebook application, Get one <a href=https://developers.facebook.com/?ref=pf">here</a>  </td></tr></table>',
+    'LBL_LICENSING_INFO' => '<table border="0" cellspacing="1"> <tr><th valign="top" width="35%" class="dataLabel"> Información da aplicación de Facebook</th></tr> <tr><td width="35%" class="dataLabel"> que terá que crear unha aplicación de facebook, conseguir un <a href=https://developers.facebook.com/?ref=pf"> aquí</a></td></tr></table>',
 
     //Configuration labels
     'appid' => 'Facebook App ID ',
-    'secret' => 'Facebook App Secret ',
+    'secret' => 'Segredo da aplicación de Facebook ',
 );
