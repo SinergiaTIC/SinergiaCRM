b775ab4089726d3219696a8ea9562f364265a921
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 14:11:47 +0200

    New translations es_es.lang.php (Basque)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/Payments_MainData/language/eu_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/Payments_MainData/language/eu_ES.lang.php
@@ -21,10 +21,10 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-    'NAME' => 'Payments - Check main fields',		// Required label. Contains function name
-    'UPDATED_IBAN' => ' has been updated to clean invalid chars.',
-    'UPDATED_IBAN' => ' with wrong value, it has been automatically updated.',
-    'UPDATED_MANDATE' => ' was empty or invalid, it has been automatically updated.',
-	'NOVALID_MANDATE' => ' was empty or invalid, automatic updated can\'t be done.',
-	'INHERIT_MANDATE' => ' was empty or invalid, it has been inherited from its payment method.',
+    'NAME' => 'Pagos - Revisión de los datos principales',		// Esta etiqueta es obligatoria. Indica el nombre de la función
+    'UPDATED_IBAN' => ' se ha actualizado para eliminar caracteres no válidos.',
+    'UPDATED_IBAN' => ' con valor erróneo, se ha actualizado automáticamente.',
+    'UPDATED_MANDATE' => ' estaba vacío o no era válido, se ha actualizado automáticamente.',
+	'NOVALID_MANDATE' => ' está vacío o no es válido y no se puede recuperar desde el compromiso de pago.',
+	'INHERIT_MANDATE' => ' estaba vacío o no era válido, se ha heredado desde el compromiso de pago.',
 );
