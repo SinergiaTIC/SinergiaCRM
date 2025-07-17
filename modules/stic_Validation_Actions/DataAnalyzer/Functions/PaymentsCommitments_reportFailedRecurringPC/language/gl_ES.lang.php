69a0b7a9b6927ecfc2496cb7f1b30011aed5aa51
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 14:11:45 +0200

    New translations es_es.lang.php (Galician)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/PaymentsCommitments_reportFailedRecurringPC/language/gl_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/PaymentsCommitments_reportFailedRecurringPC/language/gl_ES.lang.php
@@ -21,7 +21,7 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-    'NAME' => 'Payment commitments - Incomplete recurring authorizations validation',
-    'NO_ROWS' => 'No payment commitment has been found.',
-    'RESULT_INFO' => 'This @payment_method@ recurring payment commitment does not have the proper payment gateway information and has @payment_count@ pending payments that should be reviewed. Maybe the payment commitment shoud be cancelled.',
+    'NAME' => 'Compromisos de pago - Revisión das autorizacións recurrentes incompletas',
+    'NO_ROWS' => 'Non se encontrou ningún compromiso de pago.',
+    'RESULT_INFO' => 'Este compromiso de pago recurrente con @payment_method@ non ten a información necesaria da pasarela de pago e ten @payment_count@ pagos con estado pendente que deberían ser revisados. É posible que deba darse de baixa o compromiso de pago.',
 );
