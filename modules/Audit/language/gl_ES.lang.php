92ceeb8daa0f36cc85ba346d1b8280bfc6272b4a
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 12:04:40 +0200

    New translations en_us.lang.php (Galician)

--- modules/Audit/language/gl_ES.lang.php
+++ modules/Audit/language/gl_ES.lang.php
@@ -43,11 +43,11 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_FIELD_NAME' => 'Campo',
-    'LBL_OLD_NAME' => 'Valor Anterior',
-    'LBL_NEW_VALUE' => 'Valor Novo',
-    'LBL_CREATED_BY' => 'Cambiado Por',
-    'LBL_LIST_DATE' => 'Data de Cambio',
-    'LBL_AUDITED_FIELDS' => 'Campos auditados neste módulo: ',
-    'LBL_CHANGE_LOG' => 'Rexistro de Cambios',
+    'LBL_FIELD_NAME' => 'Field',
+    'LBL_OLD_NAME' => 'Old Value',
+    'LBL_NEW_VALUE' => 'New Value',
+    'LBL_CREATED_BY' => 'Changed By',
+    'LBL_LIST_DATE' => 'Change Date',
+    'LBL_AUDITED_FIELDS' => 'Fields audited in this module: ',
+    'LBL_CHANGE_LOG' => 'Change Log',
 );
