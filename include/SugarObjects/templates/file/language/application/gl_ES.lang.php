e23df604ac995999c23e9249fe03132fef5b4b87
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 12:01:24 +0200

    New translations en_us.lang.php (Galician)

--- include/SugarObjects/templates/file/language/application/gl_ES.lang.php
+++ include/SugarObjects/templates/file/language/application/gl_ES.lang.php
@@ -46,23 +46,23 @@ $app_list_strings = array(
     strtolower($object_name) . '_category_dom' => array(
         '' => '',
         'Marketing' => 'Marketing',
-        'Knowledege Base' => 'Base de Coñecemento',
-        'Sales' => 'Vendas',
+        'Knowledege Base' => 'Knowledge Base',
+        'Sales' => 'Sales',
     ),
 
     strtolower($object_name) . '_subcategory_dom' => array(
         '' => '',
-        'Marketing Collateral' => 'Impresos de Marketing',
-        'Product Brochures' => 'Folletos de Produto',
+        'Marketing Collateral' => 'Marketing Collateral',
+        'Product Brochures' => 'Product Brochures',
         'FAQ' => 'FAQ',
     ),
 
     strtolower($object_name) . '_status_dom' => array(
-        'Active' => 'Activo',
-        'Draft' => 'Borrador',
+        'Active' => 'Active',
+        'Draft' => 'Draft',
         'FAQ' => 'FAQ',
-        'Expired' => 'Caducado',
-        'Under Review' => 'En Revisión',
-        'Pending' => 'Pendente',
+        'Expired' => 'Expired',
+        'Under Review' => 'Under Review',
+        'Pending' => 'Pending',
     ),
 );
