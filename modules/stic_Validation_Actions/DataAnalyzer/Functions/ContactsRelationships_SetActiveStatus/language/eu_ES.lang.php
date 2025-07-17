29af74d6b05c5c5ee29fa4dc0e44b86588c4ba80
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 14:10:42 +0200

    New translations es_es.lang.php (Basque)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/ContactsRelationships_SetActiveStatus/language/eu_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/ContactsRelationships_SetActiveStatus/language/eu_ES.lang.php
@@ -21,7 +21,7 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-    'NAME' => 'Contacts relationships - Set active/inactive records', // This label is mandatory. Indicates the name of the function
-    'NO_ROWS' => 'All contacts relationships have the right active/inactive status.',
-    'UPDATED' => 'The Active field has been updated.'
+    'NAME' => 'Relaciones con Personas - Cálculo de registro activo/inactivo',		// Esta etiqueta es obligatoria. Indica el nombre de la función
+    'NO_ROWS' => 'Todas las relaciones con personas tienen el estado activo/inactivo correcto.',
+    'UPDATED' => 'Se ha actualizado el campo Activo.',
 );
