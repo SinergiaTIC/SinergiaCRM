ee7cd8b287ec01e5366316880afc4a2a6e44e262
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 14:02:05 +0200

    New translations jotpaddashlet.es_es.lang.php (Basque)

--- modules/Home/Dashlets/JotPadDashlet/eu_ES.lang.php
+++ modules/Home/Dashlets/JotPadDashlet/eu_ES.lang.php
@@ -44,12 +44,12 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $defaultText = "";
 $dashletStrings['JotPadDashlet'] = array(
-    'LBL_TITLE' => 'JotPad',
-    'LBL_DESCRIPTION' => 'A dashlet to keep your notes',
-    'LBL_SAVING' => 'Saving JotPad...',
-    'LBL_SAVED' => 'Saved',
-    'LBL_CONFIGURE_TITLE' => 'Title',
-    'LBL_CONFIGURE_HEIGHT' => 'Height (1 - 300)',
-    'LBL_DBLCLICK_HELP' => 'Double click below to Edit.',
+    'LBL_TITLE' => 'Notas Rápidas',
+    'LBL_DESCRIPTION' => 'Un dashlet para tomar notas',
+    'LBL_SAVING' => 'Guardando nota...',
+    'LBL_SAVED' => 'Guardado',
+    'LBL_CONFIGURE_TITLE' => 'Título',
+    'LBL_CONFIGURE_HEIGHT' => 'Altura (1 - 300)',
+    'LBL_DBLCLICK_HELP' => 'Haga doble clic abajo para Editar.',
     'LBL_DEFAULT_TEXT' => $defaultText,
 );
