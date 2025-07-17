52b440b014b19f493bb90701ff25c89737b24032
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 14:12:34 +0200

    New translations es_es.lang.php (Basque)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/include/FunctionModel/language/eu_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/include/FunctionModel/language/eu_ES.lang.php
@@ -21,5 +21,5 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-  'NAME' => 'Function name',		// Required label. Contains function name
+  'NAME' => 'Nombre de la función',		// Esta etiqueta es obligatoria. Indica el nombre de la función
 );
