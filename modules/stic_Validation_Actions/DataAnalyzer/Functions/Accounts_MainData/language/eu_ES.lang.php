88f0702239f23d6bec0ce2d2d6486e3b5483d587
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 14:10:21 +0200

    New translations es_es.lang.php (Basque)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/Accounts_MainData/language/eu_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/Accounts_MainData/language/eu_ES.lang.php
@@ -21,7 +21,7 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array(
-    'NAME' => 'Accounts - Check main fields', // Required label. Contains function name
-    'NO_VALID' => 'Invalid NIF.',
-    'UPDATED' => 'Field cleaned to remove invalid characters:',
+    'NAME' => 'Organizaciones - Revisión de los datos principales', // Esta etiqueta es obligatoria. Indica el nombre de la función
+    'NO_VALID' => 'No tiene un NIF válido.',
+    'UPDATED' => 'Se ha limpiado el campo para eliminar caràcteres no válidos:',
 );
