7c440b43f044c274b54d777bc8b79b275687b4c9
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-07-17 12:01:38 +0200

    New translations en_us.lang.php (Galician)

--- include/SugarObjects/templates/issue/language/gl_ES.lang.php
+++ include/SugarObjects/templates/issue/language/gl_ES.lang.php
@@ -44,29 +44,29 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
 
-    'LBL_NAME' => 'Nome',
-    'LBL_NUMBER' => 'Número:',
-    'LBL_STATUS' => 'Estado:',
-    'LBL_PRIORITY' => 'Prioridade:',
-    'LBL_DESCRIPTION' => 'Descrición:',
-    'LBL_RESOLUTION' => 'Resolución',
-    'LBL_LAST_MODIFIED' => 'Última Modificación',
-    'LBL_ASSIGNED_TO_ID' => 'Asignado A:',
-    'LBL_ASSIGNED_TO_NAME' => 'Usuario:',
-    'LBL_WORK_LOG' => 'Rexistro de Actividade:',
-    'LBL_CREATED_BY' => 'Creado Por:',
-    'LBL_DATE_ENTERED' => 'Data de Creación:',
-    'LBL_DATE_MODIFIED' => 'Data de Modificación:',
-    'LBL_MODIFIED_BY' => 'Modificado Por:',
-    'LBL_ASSIGNED_USER' => 'Usuario Asignado:',
-    'LBL_TYPE' => 'Tipo:',
-    'LBL_SUBJECT' => 'Asunto:',
+    'LBL_NAME' => 'Name',
+    'LBL_NUMBER' => 'Number:',
+    'LBL_STATUS' => 'Status:',
+    'LBL_PRIORITY' => 'Priority:',
+    'LBL_DESCRIPTION' => 'Description:',
+    'LBL_RESOLUTION' => 'Resolution',
+    'LBL_LAST_MODIFIED' => 'Last Modified',
+    'LBL_ASSIGNED_TO_ID' => 'Assigned To:',
+    'LBL_ASSIGNED_TO_NAME' => 'User:',
+    'LBL_WORK_LOG' => 'Work Log:',
+    'LBL_CREATED_BY' => 'Created by:',
+    'LBL_DATE_ENTERED' => 'Date Created:',
+    'LBL_DATE_MODIFIED' => 'Date Modified:',
+    'LBL_MODIFIED_BY' => 'Last Modified by:',
+    'LBL_ASSIGNED_USER' => 'Assigned User:',
+    'LBL_TYPE' => 'Type:',
+    'LBL_SUBJECT' => 'Subject:',
 
-    'LBL_EDIT_BUTTON' => 'Editar',
+    'LBL_EDIT_BUTTON' => 'Edit',
     // STIC-Custom 20240214 JBL - QuickEdit view
     // https://github.com/SinergiaTIC/SinergiaCRM/pull/93
-    'LBL_QUICKEDIT_BUTTON' => '↙ Editar',
+    'LBL_QUICKEDIT_BUTTON' => '↙ Edit',
     // END STIC-Custom
-    'LBL_REMOVE' => 'Quitar',
+    'LBL_REMOVE' => 'Remove',
 
 );
