daed20c04427d3ce10dde65f4059460c5cd18c61
ainaraRT <ainaretework@gmail.com>
2025-07-23 15:33:10 +0000

    remove strings

--- custom/Extension/application/Ext/Language/ca_ES.SticLang.php
+++ custom/Extension/application/Ext/Language/ca_ES.SticLang.php
@@ -517,7 +517,6 @@ $app_list_strings['stic_languages_list'][''] = '';
 $app_list_strings['stic_languages_list']['catalan'] = 'Català';
 $app_list_strings['stic_languages_list']['spanish'] = 'Castellà';
 $app_list_strings['stic_languages_list']['galician'] = 'Gallec';
-$app_list_strings['stic_languages_list']['basque'] = 'Basc';
 
 // Canals de campanyes
 $app_list_strings['stic_campaign_channels_list'][''] = '';
