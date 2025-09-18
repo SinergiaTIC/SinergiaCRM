e4da9e6c59d7c06d1dc6109880c8ae923e4fe284
Enric Prats <enric@sinergiacrm.org>
2025-09-18 11:13:32 +0000

    Retry action only when there was an error

--- modules/stic_Messages/Utils.js
+++ modules/stic_Messages/Utils.js
@@ -80,15 +80,17 @@ switch (viewType()) {
     // Get record Id 
     recordId = $("#formDetailView input[type=hidden][name=record]").val();
     // Define button content
-    var buttons = {
-      retry: {
-        id: "bt_retry_detailview",
-        title: SUGAR.language.get("stic_Messages", "LBL_MASS_RETRY_MESSAGE_BUTTON_TITTLE"),
-        // onclick: "window.location='index.php?module=stic_Incorpora&action=fromDetailView&record=" + recordId + "&return_module="+ module +"'"
-        onclick: "onClickRetryMessagesButton(recordId)"
-      }
-    };
-    createDetailViewButton(buttons.retry);
+    if ($("#status").val() != 'sent') {
+      var buttons = {
+        retry: {
+          id: "bt_retry_detailview",
+          title: SUGAR.language.get("stic_Messages", "LBL_MASS_RETRY_MESSAGE_BUTTON_TITTLE"),
+          // onclick: "window.location='index.php?module=stic_Incorpora&action=fromDetailView&record=" + recordId + "&return_module="+ module +"'"
+          onclick: "onClickRetryMessagesButton(recordId)"
+        }
+      };
+      createDetailViewButton(buttons.retry);
+    }
     // Field response is only showed when an error is present
     if ($("#status").val() != 'error') {
       $('div[data-field="response"]').hide();
