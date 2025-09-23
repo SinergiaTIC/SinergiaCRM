443456ff3ca2d909f08652e14f6bea43cac41798
Jaume Albaigès <45965117+jalbaiges@users.noreply.github.com>
2025-09-23 16:30:12 +0200

    Improving translation

--- custom/Extension/modules/TemplateSectionLine/Ext/Language/en_us.SticLang.php
+++ custom/Extension/modules/TemplateSectionLine/Ext/Language/en_us.SticLang.php
@@ -50,18 +50,18 @@ $mod_strings['LBL_LIST_FORM_TITLE'] = 'List of Template Section Lines';
 $mod_strings['LBL_MODULE_NAME'] = 'Template Section Lines';
 $mod_strings['LBL_MODULE_TITLE'] = 'Template Section Lines';
 $mod_strings['LBL_HOMEPAGE_TITLE'] = 'My Template Section Lines';
-$mod_strings['LNK_NEW_RECORD'] = 'Create Template Section Line';
+$mod_strings['LNK_NEW_RECORD'] = 'Create a Template Section Line';
 $mod_strings['LNK_LIST'] = 'View Template Section Lines';
 $mod_strings['LBL_SEARCH_FORM_TITLE'] = 'Search Template Section Lines';
 $mod_strings['LBL_STIC_PANEL_RECORD_DETAILS'] = 'Record details';
 
 $mod_strings['LBL_HTMLCODE'] = 'HTML Code';
 $mod_strings['LBL_THUMBNAIL_IMAGE'] = 'Thumbnail image';
-$mod_strings['LBL_THUMBNAIL_IMAGE_HELP'] = 'This image will be displayed in the thumbnail image list in the left sidebar of the Mozaik text editor. You can upload a user-created image or create one automatically via the Create Image button with the content of the Thumbnail Short Name field. If creating the image manually, the following properties are recommended: Format: PNG or JPG, Height: 50px and Width: 225px.';
-$mod_strings['LBL_THUMBNAIL_IMAGE_CREATE_IMAGE_BUTTON'] = 'Create an image with the value of the field: Thumbnail short name.';
-$mod_strings['LBL_THUMBNAIL_IMAGE_CREATE_IMAGE_CONFIRM'] = 'Do you confirm that you want to replace the pre-loaded image with the new one that will be created?';
-$mod_strings['LBL_THUMBNAIL_NAME'] = 'Short name for thumbnail';
-$mod_strings['LBL_THUMBNAIL_NAME_HELP'] = 'The text in this field, limited to 40 characters, can be used as the content of the image created when the create image button is pressed.';
-$mod_strings['LBL_THUMBNAIL_NAME_REQUIRED_TO_CREATE_IMAGE'] = 'Indicate a value in the field: ';
+$mod_strings['LBL_THUMBNAIL_IMAGE_HELP'] = 'This image will be displayed in the thumbnail list on the left of the Mozaik template editor. You can upload an external image or create one automatically by pressing the "Create an image" button. If you upload an external image, PNG or JPG format and dimensions of 225 x 50 pixels are recommended. If it is created automatically, the text in the "Short name for the thumbnail" field will be displayed.';
+$mod_strings['LBL_THUMBNAIL_IMAGE_CREATE_IMAGE_BUTTON'] = 'Create an image';
+$mod_strings['LBL_THUMBNAIL_IMAGE_CREATE_IMAGE_CONFIRM'] = 'Do you want to replace the preloaded image with the new one that will be created?';
+$mod_strings['LBL_THUMBNAIL_NAME'] = 'Short name for the thumbnail';
+$mod_strings['LBL_THUMBNAIL_NAME_HELP'] = 'The text in this field, limited to 40 characters, can be used as the content of the image automatically created.';
+$mod_strings['LBL_THUMBNAIL_NAME_REQUIRED_TO_CREATE_IMAGE'] = 'Please, set a value for the field ';
 
 $mod_strings['LNK_CAMPAIGN_LIST'] = 'Campaigns';
