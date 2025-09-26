7cb762f2a197e2320dda5a145ff7eb2c8cb17a75
Jaume Albaigès <45965117+jalbaiges@users.noreply.github.com>
2025-09-26 23:16:02 +0200

    Improving translation

--- modules/stic_Bookings/language/ca_ES.lang.php
+++ modules/stic_Bookings/language/ca_ES.lang.php
@@ -132,6 +132,6 @@ $mod_strings = array(
     // Tooltips help
     'LBL_ALL_DAY_HELP' => 'Si la casella està marcada la reserva es fa en dies complets. Si està desmarcada poden indicar-se les hores inicial i final.',
 
-    // Many to Many filter fields
+    // Camps de filtre en relacions molts a molts
     'LBL_STIC_RESOURCES_STIC_BOOKINGS_NAME' => 'Recurs',
 );
