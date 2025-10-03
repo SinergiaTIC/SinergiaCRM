fb7db3e207585b3a98be062674e3ed45020c66de
sinergiacrm <desarrollo@sinergiacrm.org>
2025-10-03 14:04:27 +0200

    first commit

--- SticUpdates/Migrations/20210622_hotifx_relationshipTypeDefaultValue.sql
+++
@@ -1,3 +0,0 @@
--- Standarize all the empty value of the fields
-UPDATE accounts_cstm SET stic_relationship_type_c = '^^' WHERE stic_relationship_type_c = '' OR stic_relationship_type_c = NULL;
-UPDATE contacts_cstm SET stic_relationship_type_c = '^^' WHERE stic_relationship_type_c = '' OR stic_relationship_type_c = NULL;
