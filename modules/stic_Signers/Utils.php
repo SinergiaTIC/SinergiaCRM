5249d87d02b45646662818ba66219225aa641031
juanSTIC <juan@sinergiacrm.org>
2025-10-17 14:03:32 +0000

    feat: Simplificar consultas de registros de firmantes eliminando el prefijo de la columna date_modified

--- modules/stic_Signers/Utils.php
+++ modules/stic_Signers/Utils.php
@@ -255,7 +255,7 @@ class stic_SignersUtils
                 AND parent_id = '{$contact_id}'
                 AND status in ('pending','signed')
                 AND deleted = 0 
-            ORDER BY stic_signers.date_modified DESC
+            ORDER BY date_modified DESC
         ";
         return $query;
     }
@@ -293,7 +293,7 @@ class stic_SignersUtils
                 AND parent_id = '{$user_id}'
                 AND status in ('pending','signed')
                 AND deleted = 0 
-            ORDER BY stic_signers.date_modified DESC
+            ORDER BY date_modified DESC
         ";
         return $query;
     }
