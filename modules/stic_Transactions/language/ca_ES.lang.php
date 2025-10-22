ee46f4c3d0ae835ba8dbe46175ada9703b955a6d
Jaume Albaigès <45965117+jalbaiges@users.noreply.github.com>
2025-10-22 19:25:06 +0200

    Header repair

--- modules/stic_Transactions/language/ca_ES.lang.php
+++ modules/stic_Transactions/language/ca_ES.lang.php
@@ -1,25 +1,25 @@
 <?php
-/
-  This file is part of SinergiaCRM.
-  SinergiaCRM is a work developed by SinergiaTIC Association, based on SuiteCRM.
-  Copyright (C) 2013 - 2023 SinergiaTIC Association
- 
-  This program is free software; you can redistribute it and/or modify it under
-  the terms of the GNU Affero General Public License version 3 as published by the
-  Free Software Foundation.
- 
-  This program is distributed in the hope that it will be useful, but WITHOUT
-  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-  FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
-  details.
- 
-  You should have received a copy of the GNU Affero General Public License along with
-  this program; if not, see http://www.gnu.org/licenses or write to the Free
-  Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-  02110-1301 USA.
- 
-  You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
- /
+/**
+ * This file is part of SinergiaCRM.
+ * SinergiaCRM is a work developed by SinergiaTIC Association, based on SuiteCRM.
+ * Copyright (C) 2013 - 2023 SinergiaTIC Association
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
+ */
 
 $mod_strings = array (
   'LBL_ASSIGNED_TO_ID' => 'Assignat a (ID)',
@@ -49,10 +49,6 @@ $mod_strings = array (
   'LBL_DEFAULT_PANEL' => 'Dades generals',
   'LBL_ASCENDING' => 'Ascendent',
   'LBL_DESCENDING' => 'Descendent',
-  'LBL_OPT_IN' => 'Autoritzar comunicacions',
-  'LBL_OPT_IN_PENDING_EMAIL_NOT_SENT' => 'Alta voluntària pendent de confirmació. Correu de confirmació no enviat.',
-  'LBL_OPT_IN_PENDING_EMAIL_SENT' => 'Alta voluntària pendent de confirmació. Correu de confirmació enviat.',
-  'LBL_OPT_IN_CONFIRMED' => 'Alta voluntària confirmada',
   'LBL_MODULE_NAME' => 'Transaccions',
   'LBL_MODULE_TITLE' => 'Transaccions',
   'LNK_NEW_DOCUMENT' => 'Crear Document',
