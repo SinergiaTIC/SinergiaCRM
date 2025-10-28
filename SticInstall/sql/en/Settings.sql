85572ae19d105f9d6d4dc4676a3f96b7ba2bd390
Jaume Albaigès <45965117+jalbaiges@users.noreply.github.com>
2025-10-28 14:04:15 +0100

    Improving translation

--- SticInstall/sql/en/Settings.sql
+++ SticInstall/sql/en/Settings.sql
@@ -54,4 +54,4 @@ INSERT INTO `stic_settings` (`id`, `date_entered`, `date_modified`, `modified_us
 ('d9f0118c-b752-444d-9057-456b18f9f360', NOW(), NOW(), '1', '1', 0, '1', 'SEVEN', 'SEVEN_API_KEY', '', 'Key to connect with to Seven, obtained from its control panel.'),
 ('c8d720bd-2c7b-47a6-98ce-9664529559ca', NOW(), NOW(), '1', '1', 0, '1', 'MESSAGES', 'MESSAGES_SENDER', '', 'Sender name that will be shown on sent messages.'),
 ('64b1554a-cf95-423b-ae78-7f75951f3976', NOW(), NOW(), '1', '1', 0, '1', 'MESSAGES', 'MESSAGES_LIMIT', '100', 'Maximum number of messages allowed in a bulk send.'),
-('e5a26eec-b326-11f0-b8b0-f2de65fe24cb', NOW(), NOW(), '1', '1', 0, '1', 'SEVEN', 'SEVEN_MESSAGES_BULK', '5000', "Max number of messages sent per hour when sending campaigns.");
+('e5a26eec-b326-11f0-b8b0-f2de65fe24cb', NOW(), NOW(), '1', '1', 0, '1', 'SEVEN', 'SEVEN_MESSAGES_BULK', '5000', 'Messages per hour on SMS campaigns.');
