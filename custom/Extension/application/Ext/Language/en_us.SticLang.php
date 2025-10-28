1f31f59545717b06cfdb2f73844353b2e7a90953
Jaume Albaigès <45965117+jalbaiges@users.noreply.github.com>
2025-10-28 14:57:21 +0100

    Improving translation

--- custom/Extension/application/Ext/Language/en_us.SticLang.php
+++ custom/Extension/application/Ext/Language/en_us.SticLang.php
@@ -155,7 +155,7 @@ $app_list_strings['moduleList']['stic_Custom_View_Actions'] = 'Actions';
 $app_list_strings['moduleList']['stic_Custom_View_Customizations'] = 'Customizations';
 $app_list_strings['moduleList']['stic_Custom_View_Conditions'] = 'Conditions';
 $app_list_strings['moduleList']['stic_Group_Opportunities'] = 'Granted accounts';
-$app_list_strings['moduleList']['stic_Message_Marketing'] = 'Message Marketing';
+$app_list_strings['moduleList']['stic_Message_Marketing'] = 'Message Sendings';
 $app_list_strings['moduleList']['stic_MessagesMan'] = 'Messages Queue';
 $app_list_strings['moduleList']['stic_Bookings_Places_Calendar'] = 'Places Booking Calendar';
 $app_list_strings['moduleList']['stic_Places'] = 'Places';
@@ -205,7 +205,7 @@ $app_list_strings['moduleListSingular']['stic_Custom_View_Actions'] = 'Action';
 $app_list_strings['moduleListSingular']['stic_Custom_View_Customizations'] = 'Customization';
 $app_list_strings['moduleListSingular']['stic_Custom_View_Conditions'] = 'Condition';
 $app_list_strings['moduleListSingular']['stic_Group_Opportunities'] = 'Granted account';
-$app_list_strings['moduleListSingular']['stic_Message_Marketing'] = 'Message Marketing';
+$app_list_strings['moduleListSingular']['stic_Message_Marketing'] = 'Message Sendings';
 $app_list_strings['moduleListSingular']['stic_MessagesMan'] = 'Messages Queue';
 $app_list_strings['moduleListSingular']['stic_Bookings_Places_Calendar'] = 'Places Booking Calendar';
 $app_list_strings['moduleListSingular']['stic_Places'] = 'Places';
@@ -249,7 +249,7 @@ $app_list_strings['parent_type_display']['stic_Work_Experience'] = 'Work experie
 $app_list_strings['parent_type_display']['stic_Skills'] = 'Skills';
 $app_list_strings['parent_type_display']['stic_Messages'] = 'Messages';
 $app_list_strings['parent_type_display']['stic_Group_Opportunities'] = 'Granted accounts';
-$app_list_strings['parent_type_display']['stic_Message_Marketing'] = 'Message Marketing';
+$app_list_strings['parent_type_display']['stic_Message_Marketing'] = 'Message Sendings';
 $app_list_strings['parent_type_display']['stic_Bookings_Places_Calendar'] = 'Places Booking Calendar';
 $app_list_strings['parent_type_display']['stic_Places'] = 'Places';
 
@@ -292,7 +292,7 @@ $app_list_strings['record_type_display']['stic_Work_Experience'] = 'Work experie
 $app_list_strings['record_type_display']['stic_Skills'] = 'Skills';
 $app_list_strings['record_type_display']['stic_Messages'] = 'Messages';
 $app_list_strings['record_type_display']['stic_Group_Opportunities'] = 'Granted accounts';
-$app_list_strings['record_type_display']['stic_Message_Marketing'] = 'Message Marketing';
+$app_list_strings['record_type_display']['stic_Message_Marketing'] = 'Message Sendings';
 $app_list_strings['record_type_display']['stic_Bookings_Places_Calendar'] = 'Places Booking Calendar';
 $app_list_strings['record_type_display']['stic_Places'] = 'Places';
 
@@ -335,7 +335,7 @@ $app_list_strings['record_type_display_notes']['stic_Work_Experience'] = 'Work e
 $app_list_strings['record_type_display_notes']['stic_Skills'] = 'Skills';
 $app_list_strings['record_type_display_notes']['stic_Messages'] = 'Messages';
 $app_list_strings['record_type_display_notes']['stic_Group_Opportunities'] = 'Granted accounts';
-$app_list_strings['record_type_display_notes']['stic_Message_Marketing'] = 'Message Marketing';
+$app_list_strings['record_type_display_notes']['stic_Message_Marketing'] = 'Message Sendings';
 $app_list_strings['record_type_display_notes']['stic_Bookings_Places_Calendar'] = 'Places Booking Calendar';
 $app_list_strings['record_type_display_notes']['stic_Places'] = 'Places';
 
@@ -3706,7 +3706,7 @@ $app_list_strings['campaign_type_dom']['NewsLetter'] = 'Newsletter';
 $app_list_strings['campaign_type_dom']['Survey'] = 'Survey';
 $app_list_strings['campaign_type_dom']['Notification'] = 'Notification by email';
 $app_list_strings['campaign_type_dom']['Message'] = 'Phone message';
-$app_list_strings['campaign_type_dom']['NotifMsg'] = 'Notification by Message';
+$app_list_strings['campaign_type_dom']['NotifMsg'] = 'Notification by phone message';
 
 // Email Templates: Template types
 $app_list_strings['emailTemplates_type_list_no_workflow'][''] = '';
