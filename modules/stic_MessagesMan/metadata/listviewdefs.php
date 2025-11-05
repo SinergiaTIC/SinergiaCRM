bc3977df6d0c73f60a1cd975db0e8a3529c3d023
Enric Prats <enric@sinergiacrm.org>
2025-11-05 11:56:36 +0000

    Disabling edit in messages queue

--- modules/stic_MessagesMan/metadata/listviewdefs.php
+++ modules/stic_MessagesMan/metadata/listviewdefs.php
@@ -30,37 +30,51 @@ $listViewDefs['stic_MessagesMan'] = array(
         'label' => 'LBL_LIST_CAMPAIGN',
         'link' => true,
         'customCode' => '<a href="index.php?module=Campaigns&action=DetailView&record={$CAMPAIGN_ID}">{$CAMPAIGN_NAME}</a>',
-        'default' => true),
+        'default' => true,
+        'inline_edit' => false
+    ),
     'RECIPIENT_NAME' => array(
         'sortable' => false,
         'width' => '25%',
         'label' => 'LBL_LIST_RECIPIENT_NAME',
         'customCode' => '<a href="index.php?module={$RELATED_TYPE}&action=DetailView&record={$RELATED_ID}">{$RECIPIENT_NAME}</a>',
-        'default' => true),
+        'default' => true,
+    'inline_edit' => false
+    ),
     'RECIPIENT_PHONE' => array(
         'sortable' => false,
         'width' => '10%',
         'label' => 'LBL_LIST_RECIPIENT_PHONE',
         'customCode' => '{$RECIPIENT_PHONE}</a>',
-        'default' => true),
+        'default' => true,
+        'inline_edit' => false
+    ),
     'MESSAGE_NAME' => array(
         'sortable' => false,
         'width' => '20%',
         'label' => 'LBL_LIST_MESSAGE_NAME',
         'customCode' => '<a href="index.php?module=EmailMarketing&action=DetailView&record={$MARKETING_ID}">{$MESSAGE_NAME}</a>',
-        'default' => true),
+        'default' => true,
+        'inline_edit' => false
+    ),
     'SEND_DATE_TIME' => array(
         'width' => '10%',
         'label' => 'LBL_LIST_SEND_DATE_TIME',
-        'default' => true),
+        'default' => true,
+        'inline_edit' => false
+    ),
     'SEND_ATTEMPTS' => array(
         'width' => '5%',
         'label' => 'LBL_LIST_SEND_ATTEMPTS',
-        'default' => true),
+        'default' => true,
+        'inline_edit' => false
+    ),
     'IN_QUEUE' => array(
         'width' => '5%',
         'label' => 'LBL_LIST_IN_QUEUE',
-        'default' => true),
+        'default' => true,
+        'inline_edit' => false
+    ),
     'date_entered' => array(
         'width' => '10%',
         'label' => 'LBL_DATE_ENTERED',
