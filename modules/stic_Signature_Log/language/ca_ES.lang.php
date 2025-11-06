b3e63b1fd3ff0b0640f6c5ecb5315033e6961aa3
juanSTIC <juan@sinergiacrm.org>
2025-11-06 10:30:21 +0000

    Normalize language files

--- modules/stic_Signature_Log/language/ca_ES.lang.php
+++ modules/stic_Signature_Log/language/ca_ES.lang.php
@@ -1,57 +1,59 @@
 <?php
 /**
-* This file is part of SinergiaCRM.
-* SinergiaCRM is a work developed by SinergiaTIC Association, based on SuiteCRM.
-* Copyright (C) 2013 - 2023 SinergiaTIC Association
-*
-* This program is free software; you can redistribute it and/or modify it under
-* the terms of the GNU Affero General Public License version 3 as published by the
-* Free Software Foundation.
-*
-* This program is distributed in the hope that it will be useful, but WITHOUT
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-* FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
-* details.
-*
-* You should have received a copy of the GNU Affero General Public License along with
-* this program; if not, see http://www.gnu.org/licenses or write to the Free
-* Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-* 02110-1301 USA.
-*
-* You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
-*/
+ * This file is part of SinergiaCRM.
+ * SinergiaCRM is a work developed by SinergiaTIC Association, based on SuiteCRM.
+ * Copyright (C) 2013 - 2023 SinergiaTIC Association
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
+ */
 $mod_strings = array(
     'LBL_ASSIGNED_TO_ID' => 'Assignat a (ID)',
     'LBL_ASSIGNED_TO_NAME' => 'Assignat a',
+    'LBL_ASSIGNED_TO' => 'Assignat a',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Assignat a',
+    'LBL_LIST_ASSIGNED_USER' => 'Assignat a',
+    'LBL_CREATED' => 'Creat per',
+    'LBL_CREATED_USER' => 'Creat per',
+    'LBL_CREATED_ID' => 'Creat per (ID)',
+    'LBL_MODIFIED' => 'Modificat per',
+    'LBL_MODIFIED_NAME' => 'Modificat per',
+    'LBL_MODIFIED_USER' => 'Modificat per',
+    'LBL_MODIFIED_ID' => 'Modificat per (ID)',
     'LBL_SECURITYGROUPS' => 'Grups de seguretat',
     'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => 'Grups de seguretat',
     'LBL_ID' => 'ID',
     'LBL_DATE_ENTERED' => 'Data de Creació',
     'LBL_DATE_MODIFIED' => 'Data de Modificació',
-    'LBL_MODIFIED' => 'Modificat per',
-    'LBL_MODIFIED_NAME' => 'Modificat per',
-    'LBL_CREATED' => 'Creat per',
     'LBL_DESCRIPTION' => 'Descripció',
     'LBL_DELETED' => 'Suprimit',
     'LBL_NAME' => 'Nom',
-    'LBL_CREATED_USER' => 'Creat per',
-    'LBL_MODIFIED_USER' => 'Modificat per',
     'LBL_LIST_NAME' => 'Nom',
     'LBL_EDIT_BUTTON' => 'Edita',
-    'LBL_QUICKEDIT_BUTTON' => '↙ Edita',
     'LBL_REMOVE' => 'Desvincula',
     'LBL_ASCENDING' => 'Ascendent',
     'LBL_DESCENDING' => 'Descendent',
-    'LBL_OPT_IN' => 'Autorizar',
-    'LBL_OPT_IN_PENDING_EMAIL_NOT_SENT' => 'Confirmación de Autorización pendiente, Confirmación de Autorización no enviada',
-    'LBL_OPT_IN_PENDING_EMAIL_SENT' => 'Confirmación de Autorización pendiente, Confirmación de Autorización enviada',
-    'LBL_OPT_IN_CONFIRMED' => 'Autorizado',
-    'LBL_LIST_FORM_TITLE' => 'Llista del Registre de firmes',
+    'LBL_DEFAULT_PANEL' => 'Dades generals',
+    'LBL_PANEL_RECORD_DETAILS' => 'Detalls del registre',
+    'LBL_LIST_FORM_TITLE' => 'Llista de Registre de firmes',
     'LBL_MODULE_NAME' => 'Registre de firmes',
     'LBL_MODULE_TITLE' => 'Registre de firmes',
-    'LBL_HOMEPAGE_TITLE' => 'El meu Registre de firmes',
-    'LNK_NEW_RECORD' => 'Crea un Registre de firmes',
-    'LNK_LIST' => 'Mostra el Registre de firmes',
+    'LBL_HOMEPAGE_TITLE' => 'Els meus Registres de firmes',
+    'LNK_NEW_RECORD' => 'Nou Registre de firmes',
+    'LNK_LIST' => 'Mostra els Registres de firmes',
     'LNK_IMPORT_STIC_SIGNATURE_LOG' => 'Importa Registre de firmes',
     'LBL_SEARCH_FORM_TITLE' => 'Cerca Registre de firmes',
     'LBL_HISTORY_SUBPANEL_TITLE' => 'Historial',
--- modules/stic_Signature_Log/language/es_ES.lang.php
+++ modules/stic_Signature_Log/language/es_ES.lang.php
@@ -1,56 +1,58 @@
 <?php
 /**
-* This file is part of SinergiaCRM.
-* SinergiaCRM is a work developed by SinergiaTIC Association, based on SuiteCRM.
-* Copyright (C) 2013 - 2023 SinergiaTIC Association
-*
-* This program is free software; you can redistribute it and/or modify it under
-* the terms of the GNU Affero General Public License version 3 as published by the
-* Free Software Foundation.
-*
-* This program is distributed in the hope that it will be useful, but WITHOUT
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-* FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
-* details.
-*
-* You should have received a copy of the GNU Affero General Public License along with
-* this program; if not, see http://www.gnu.org/licenses or write to the Free
-* Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-* 02110-1301 USA.
-*
-* You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
-*/
+ * This file is part of SinergiaCRM.
+ * SinergiaCRM is a work developed by SinergiaTIC Association, based on SuiteCRM.
+ * Copyright (C) 2013 - 2023 SinergiaTIC Association
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
+ */
 $mod_strings = array(
     'LBL_ASSIGNED_TO_ID' => 'Asignado a (ID)',
     'LBL_ASSIGNED_TO_NAME' => 'Asignado a',
+    'LBL_ASSIGNED_TO' => 'Asignado a',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Asignado a',
+    'LBL_LIST_ASSIGNED_USER' => 'Asignado a',
+    'LBL_CREATED' => 'Creado por',
+    'LBL_CREATED_USER' => 'Creado por',
+    'LBL_CREATED_ID' => 'Creado por (ID)',
+    'LBL_MODIFIED' => 'Modificado por',
+    'LBL_MODIFIED_NAME' => 'Modificado por',
+    'LBL_MODIFIED_USER' => 'Modificado por',
+    'LBL_MODIFIED_ID' => 'Modificado por (ID)',
     'LBL_SECURITYGROUPS' => 'Grupos de seguridad',
     'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => 'Grupos de seguridad',
     'LBL_ID' => 'ID',
     'LBL_DATE_ENTERED' => 'Fecha de Creación',
     'LBL_DATE_MODIFIED' => 'Fecha de Modificación',
-    'LBL_MODIFIED' => 'Modificado por',
-    'LBL_MODIFIED_NAME' => 'Modificado por',
-    'LBL_CREATED' => 'Creado por',
     'LBL_DESCRIPTION' => 'Descripción',
     'LBL_DELETED' => 'Eliminado',
     'LBL_NAME' => 'Nombre',
-    'LBL_CREATED_USER' => 'Creado por',
-    'LBL_MODIFIED_USER' => 'Modificado por',
     'LBL_LIST_NAME' => 'Nombre',
     'LBL_EDIT_BUTTON' => 'Editar',
-    'LBL_QUICKEDIT_BUTTON' => '↙ Editar',
     'LBL_REMOVE' => 'Desvincular',
     'LBL_ASCENDING' => 'Ascendente',
     'LBL_DESCENDING' => 'Descendente',
-    'LBL_OPT_IN' => 'Autorizar',
-    'LBL_OPT_IN_PENDING_EMAIL_NOT_SENT' => 'Confirmación de Autorización pendiente, Confirmación de Autorización no enviada',
-    'LBL_OPT_IN_PENDING_EMAIL_SENT' => 'Confirmación de Autorización pendiente, Confirmación de Autorización enviada',
-    'LBL_OPT_IN_CONFIRMED' => 'Autorizado',
-    'LBL_LIST_FORM_TITLE' => 'Lista del Registro de firmas',
+    'LBL_DEFAULT_PANEL' => 'Datos generales',
+    'LBL_PANEL_RECORD_DETAILS' => 'Detalles del registro',
+    'LBL_LIST_FORM_TITLE' => 'Lista de Registro de firmas',
     'LBL_MODULE_NAME' => 'Registro de firmas',
     'LBL_MODULE_TITLE' => 'Registro de firmas',
-    'LBL_HOMEPAGE_TITLE' => 'Mi Registro de firmas',
-    'LNK_NEW_RECORD' => 'Crear Registro de firmas',
+    'LBL_HOMEPAGE_TITLE' => 'Mis Registros de firmas',
+    'LNK_NEW_RECORD' => 'Crear un Registro de firmas',
     'LNK_LIST' => 'Ver Registro de firmas',
     'LNK_IMPORT_STIC_SIGNATURE_LOG' => 'Importar Registro de firmas',
     'LBL_SEARCH_FORM_TITLE' => 'Buscar Registro de firmas',
