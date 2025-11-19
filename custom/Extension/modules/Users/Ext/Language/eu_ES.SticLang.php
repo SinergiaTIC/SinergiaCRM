0b08bf221652aa1fc8e79ab9890eadd0740e65b7
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-11-19 18:16:32 +0100

    New translations es_es.sticlang.php (Basque)

--- custom/Extension/modules/Users/Ext/Language/eu_ES.SticLang.php
+++ custom/Extension/modules/Users/Ext/Language/eu_ES.SticLang.php
@@ -44,6 +44,8 @@ $mod_strings['LBL_STIC_CLOCK'] = 'Registro horario';
 $mod_strings['LBL_STIC_CLOCK_INFO'] = 'Indica si la funcionalidad de Registro horario está activada para el usuario.';
 $mod_strings['LBL_SDA_ALLOWED'] = 'Acceso a SinergiaDA';
 $mod_strings['LBL_SDA_ALLOWED_INFO'] = 'Indica si el usuario dispone de acceso a Sinergia Data Analytics';
+$mod_strings['LBL_SDA_READONLY'] = 'Acceso de solo lectura a SinergiaDA';
+$mod_strings['LBL_SDA_READONLY_INFO'] = 'Cuando se activa, indica que un usuario con acceso a Sinergia Data Analytics podrá ejecutar los informes y ver sus resultados pero no podrá crear informes nuevos ni modificar los existentes.';
 
 // Virtual Field Kreporter
 $mod_strings['LBL_KREPORTER_EMAILS_LIST'] = 'Lista de direcciones de correo';
