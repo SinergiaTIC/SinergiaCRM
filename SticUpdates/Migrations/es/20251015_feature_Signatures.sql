e7e4f7c17f332f933bfbea2797dc8d1ede35867f
Jaume Albaigès <45965117+jalbaiges@users.noreply.github.com>
2025-11-20 16:58:11 +0100

    Improving translation

--- SticUpdates/Migrations/es/20251015_feature_Signatures.sql
+++ SticUpdates/Migrations/es/20251015_feature_Signatures.sql
@@ -1,5 +1,5 @@
 REPLACE INTO email_templates (id,date_entered,date_modified,modified_user_id,created_by,published,name,description,subject,body,body_html,deleted,assigned_user_id,text_only,`type`) VALUES
-	 ('000005f1-2e4e-3b11-051f-68e3c9e70330','2025-11-04 16:01:43','2025-11-04 15:09:27','2','1','off','Solicitud firma','Plantilla usada para notificar al firmante que se ha solicitado su firma electrónica para un documento.','[Solicitud de firma del documento] $stic_signatures_pdf_template','Firma de documento
+	 ('000005f1-2e4e-3b11-051f-68e3c9e70330','2025-11-04 16:01:43','2025-11-04 15:09:27','2','1','off','Solicitud firma','Plantilla para notificar al firmante que se ha solicitado su firma en un documento.','[Firma de documento] Solicitud de firma: $stic_signatures_pdf_template','Solicitud de firma
 
 Se ha solicitado su firma en el siguiente documento:
 
@@ -27,10 +27,10 @@ $sugarurl/index.php?entryPoint=sticSign&signatureId=$stic_signatures_id&targetId
 <p style="font-size: 16px; color: #555555;">Se ha solicitado su firma en el siguiente documento:</p>
 <div style="background-color: #f9f9f9; padding: 15px; border-radius: 6px; border: 1px solid #eeeeee; margin-top: 20px;">
 <p style="margin: 0; font-size: 16px; color: #333333;"><strong>$stic_signatures_pdf_template</strong></p>
-<p style="margin: 5px 0 0 0; font-size: 14px; color: #888888;"><strong>Firmante: </strong> $contact_full_name$contact_user_full_name</p>
+<p style="margin: 5px 0 0 0; font-size: 14px; color: #888888;"><strong>Firmante:</strong> $contact_full_name$contact_user_full_name</p>
 <p style="margin: 5px 0 0 0; font-size: 14px; color: #888888;"><strong>Fecha de caducidad:</strong> $stic_signatures_expiration_date</p>
 </div>
-<p style="font-size: 16px; color: #555555; margin-top: 20px;">Para firmar el documento, haga clica continuación y será redirigido al portal de firma segura.</p>
+<p style="font-size: 16px; color: #555555; margin-top: 20px;">Para firmar el documento, haga clic a continuación y será redirigido al portal de firma segura.</p>
 <p style="font-size: 16px; color: #555555; margin-top: 20px;"> </p>
 <div style="text-align: center; margin-top: 30px;"><a style="display: inline-block; padding: 12px 24px; font-size: 16px; color: #000000; background-color: #b5bc31; border-radius: 5px; text-decoration: none; font-weight: bold;" href="$sugarurl/index.php?entryPoint=sticSign&signatureId=$stic_signatures_id&targetId=$contact_id$contact_user_id">Firmar el documento</a></div>
 <p style="font-size: 14px; color: #aaaaaa; text-align: center; margin-top: 30px;">Si no puede acceder al portal de firma, copie y pegue el siguiente enlace en su navegador: <br /><a style="color: #b5bc31; text-decoration: none;" href="$sugarurl/index.php?entryPoint=sticSign&signatureId=$stic_signatures_id&targetId=$contact_id$contact_user_id">$sugarurl/index.php?entryPoint=sticSign&signatureId=$stic_signatures_id&targetId=$contact_id$contact_user_id</a></p>
@@ -38,7 +38,7 @@ $sugarurl/index.php?entryPoint=sticSign&signatureId=$stic_signatures_id&targetId
 </div>
 </body>
 </html>',0,'1',0,'notification'),
-	 ('000005f1-2e4e-3b11-051f-68e3c9e70331','2025-11-04 16:01:43','2025-11-04 15:12:34','2','1','off','Documento Firmado','Plantilla usada para enviar la versión final del documento firmado electrónicamente al firmante.','[Documento Firmado] $stic_signatures_pdf_template','Documento firmado
+	 ('000005f1-2e4e-3b11-051f-68e3c9e70331','2025-11-04 16:01:43','2025-11-04 15:12:34','2','1','off','Documento firmado','Plantilla para enviar al firmante el documento firmado.','[Firma de documento] Documento firmado: $stic_signatures_pdf_template','Documento firmado
 
 Adjunto a este correo encontrará la versión final del documento que ha firmado electrónicamente.
 
@@ -63,8 +63,8 @@ Ir al portal de firma: $sugarurl/index.php?entryPoint=sticSign&signerId=$stic_si
 <p style="font-size: 16px; color: #555555;">Adjunto a este correo encontrará la versión final del documento que ha firmado electrónicamente.</p>
 <div style="background-color: #f9f9f9; padding: 15px; border-radius: 6px; border: 1px solid #eeeeee; margin-top: 20px;">
 <p style="margin: 0; font-size: 16px; color: #333333;"><strong>$stic_signatures_pdf_template</strong></p>
-<p style="margin: 5px 0 0 0; font-size: 14px; color: #888888;"><strong>Firmante: </strong> $contact_full_name$contact_user_full_name</p>
-<p style="margin: 5px 0 0 0; font-size: 14px; color: #888888;"><strong>Fecha de la firma: </strong>$stic_signers_signature_date</p>
+<p style="margin: 5px 0 0 0; font-size: 14px; color: #888888;"><strong>Firmante:</strong> $contact_full_name$contact_user_full_name</p>
+<p style="margin: 5px 0 0 0; font-size: 14px; color: #888888;"><strong>Fecha de la firma:</strong> $stic_signers_signature_date</p>
 </div>
 <p style="font-size: 16px; color: #555555; margin-top: 20px;">En el portal de firma podrá acceder al documento firmado siempre que sea necesario.</p>
 <div style="text-align: center; margin-top: 30px;"><a style="display: inline-block; padding: 12px 24px; font-size: 16px; color: #000000; background-color: #b5bc31; border-radius: 5px; text-decoration: none; font-weight: bold;" href="$sugarurl/index.php?entryPoint=sticSign&signerId=$stic_signers_id">Ir al portal de firma</a></div>
@@ -72,7 +72,7 @@ Ir al portal de firma: $sugarurl/index.php?entryPoint=sticSign&signerId=$stic_si
 </div>
 </body>
 </html>',0,'1',0,'notification'),
-	 ('000005f1-2e4e-3b11-051f-68e3c9e70332','2025-11-04 16:01:43','2025-11-04 15:18:28','2','1','off','Codigo OTP para Firma','Plantilla usada para enviar la Contraseña de Un Solo Uso (OTP) para validar al firmante antes de acceder al proceso de firma.','[Código de Verificación] Su clave para firmar el documento','Verificación de firma
+	 ('000005f1-2e4e-3b11-051f-68e3c9e70332','2025-11-04 16:01:43','2025-11-04 15:18:28','2','1','off','Codigo de un solo uso para firmar','Plantilla para enviar el código de un solo uso al firmante antes de poder firmar.','[Firma de documento] Código de verificación para firmar','Verificación de firma
 
 Para garantizar la seguridad y autenticidad de su firma, utilice el código de verificación que se muestra a continuación.
 
@@ -80,8 +80,7 @@ $stic_signatures_pdf_template
 
 $stic_signers_otp
 
-Introduzca este código en el portal de firma para proceder.
-El código es válido durante 10 minutos y solo se puede usar una vez.','<!DOCTYPE html>
+Introduzca este código en el portal de firma para proceder. El código es válido durante 10 minutos y solo se puede usar una vez.','<!DOCTYPE html>
 <html>
 <head>
 </head>
@@ -97,12 +96,12 @@ El código es válido durante 10 minutos y solo se puede usar una vez.','<!DOCTY
 <p style="margin: 0; font-size: 16px; color: #333333;"><strong>$stic_signatures_pdf_template</strong></p>
 <div style="text-align: center; margin-top: 10px; margin-bottom: 10px;"><span style="display: inline-block; padding: 15px 30px; font-size: 28px; color: #000000; background-color: #b5bc31; border-radius: 5px; font-weight: bold; letter-spacing: 5px;">$stic_signers_otp</span></div>
 </div>
-<p style="font-size: 16px; color: #555555; margin-top: 20px;">Introduzca este código  en el portal de firma para proceder. <br /><span style="font-size: 10pt; color: #7e8c8d;">El código es válido durante 10 minutos y solo se puede usar una vez.</span></p>
+<p style="font-size: 16px; color: #555555; margin-top: 20px;">Introduzca este código en el portal de firma para proceder. El código es válido durante 10 minutos y solo se puede usar una vez.</span></p>
 </div>
 </div>
 </body>
 </html>',0,'1',0,'notification'),
-	 ('000005f1-2e4e-3b11-051f-68e3c9e70333','2025-11-03 11:16:29','2025-11-04 15:20:51','2','2','off','Codigo OTP para Firma (SMS)','Plantilla usada para enviar la contraseña de un solo uso (OTP) para validar al firmante antes de acceder al proceso de firma por SMS','[Código de Verificación] Su clave para firmar el documento','$contact_first_name$contact_user_first_name, este es su código para firmar: $stic_signers_otp. Es válido durante 10 minutos.','<!DOCTYPE html>
+	 ('000005f1-2e4e-3b11-051f-68e3c9e70333','2025-11-03 11:16:29','2025-11-04 15:20:51','2','2','off','Código de un solo uso para firmar (SMS)','Plantilla para enviar vía SMS el código de un solo uso al firmante antes de poder firmar','[Firma de documento] Código de verificación para firmar','$contact_first_name$contact_user_first_name, este es su código para firmar: $stic_signers_otp. Es válido durante 10 minutos.','<!DOCTYPE html>
 <html>
 <head>
 </head>
