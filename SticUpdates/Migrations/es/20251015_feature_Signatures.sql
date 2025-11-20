8f186e4b687c0edf46ffe0a68ad91cf9fc0a21ed
Jaume Albaigès <45965117+jalbaiges@users.noreply.github.com>
2025-11-20 17:49:56 +0100

    Minor fixes

--- SticUpdates/Migrations/es/20251015_feature_Signatures.sql
+++ SticUpdates/Migrations/es/20251015_feature_Signatures.sql
@@ -1,5 +1,5 @@
 REPLACE INTO email_templates (id,date_entered,date_modified,modified_user_id,created_by,published,name,description,subject,body,body_html,deleted,assigned_user_id,text_only,`type`) VALUES
-	 ('000005f1-2e4e-3b11-051f-68e3c9e70330','2025-11-04 16:01:43','2025-11-04 15:09:27','2','1','off','Solicitud firma','Plantilla para notificar al firmante que se ha solicitado su firma en un documento.','[Firma de documento] Solicitud de firma: $stic_signatures_pdf_template','Solicitud de firma
+	 ('000005f1-2e4e-3b11-051f-68e3c9e70330','2025-11-04 16:01:43','2025-11-04 15:09:27','2','1','off','Solicitud de firma','Plantilla para notificar al firmante que se ha solicitado su firma en un documento.','[Firma de documento] Solicitud de firma: $stic_signatures_pdf_template','Solicitud de firma
 
 Se ha solicitado su firma en el siguiente documento:
 
@@ -8,11 +8,11 @@ $stic_signatures_pdf_template
 Firmante: $contact_full_name$contact_user_full_name
 Fecha de caducidad: $stic_signatures_expiration_date
 
-Para firmar el documento, haga clic a continuación y será redirigido al portal de firma segura.
+Para firmar el documento, haga clic a continuación y se le redirigirá al portal de firma segura.
 
 Firmar el documento: $sugarurl/index.php?entryPoint=sticSign&signatureId=$stic_signatures_id&targetId=$contact_id$contact_user_id
 
-Si no puede acceder al portal de firma, copie y pegue el siguiente enlace en su navegador:
+Si no puede acceder al portal de firma, copie y pegue el enlace en su navegador:
 $sugarurl/index.php?entryPoint=sticSign&signatureId=$stic_signatures_id&targetId=$contact_id$contact_user_id','<!DOCTYPE html>
 <html>
 <head>
@@ -30,17 +30,17 @@ $sugarurl/index.php?entryPoint=sticSign&signatureId=$stic_signatures_id&targetId
 <p style="margin: 5px 0 0 0; font-size: 14px; color: #888888;"><strong>Firmante:</strong> $contact_full_name$contact_user_full_name</p>
 <p style="margin: 5px 0 0 0; font-size: 14px; color: #888888;"><strong>Fecha de caducidad:</strong> $stic_signatures_expiration_date</p>
 </div>
-<p style="font-size: 16px; color: #555555; margin-top: 20px;">Para firmar el documento, haga clic a continuación y será redirigido al portal de firma segura.</p>
+<p style="font-size: 16px; color: #555555; margin-top: 20px;">Para firmar el documento, haga clic a continuación y se le redirigirá al portal de firma segura.</p>
 <p style="font-size: 16px; color: #555555; margin-top: 20px;"> </p>
 <div style="text-align: center; margin-top: 30px;"><a style="display: inline-block; padding: 12px 24px; font-size: 16px; color: #000000; background-color: #b5bc31; border-radius: 5px; text-decoration: none; font-weight: bold;" href="$sugarurl/index.php?entryPoint=sticSign&signatureId=$stic_signatures_id&targetId=$contact_id$contact_user_id">Firmar el documento</a></div>
-<p style="font-size: 14px; color: #aaaaaa; text-align: center; margin-top: 30px;">Si no puede acceder al portal de firma, copie y pegue el siguiente enlace en su navegador: <br /><a style="color: #b5bc31; text-decoration: none;" href="$sugarurl/index.php?entryPoint=sticSign&signatureId=$stic_signatures_id&targetId=$contact_id$contact_user_id">$sugarurl/index.php?entryPoint=sticSign&signatureId=$stic_signatures_id&targetId=$contact_id$contact_user_id</a></p>
+<p style="font-size: 14px; color: #aaaaaa; text-align: center; margin-top: 30px;">Si no puede acceder al portal de firma, copie y pegue el enlace en su navegador: <br /><a style="color: #b5bc31; text-decoration: none;" href="$sugarurl/index.php?entryPoint=sticSign&signatureId=$stic_signatures_id&targetId=$contact_id$contact_user_id">$sugarurl/index.php?entryPoint=sticSign&signatureId=$stic_signatures_id&targetId=$contact_id$contact_user_id</a></p>
 </div>
 </div>
 </body>
 </html>',0,'1',0,'notification'),
 	 ('000005f1-2e4e-3b11-051f-68e3c9e70331','2025-11-04 16:01:43','2025-11-04 15:12:34','2','1','off','Documento firmado','Plantilla para enviar al firmante el documento firmado.','[Firma de documento] Documento firmado: $stic_signatures_pdf_template','Documento firmado
 
-Adjunto a este correo encontrará la versión final del documento que ha firmado electrónicamente.
+Adjunto a este correo encontrará la versión final del documento que ha firmado:
 
 $stic_signatures_pdf_template
 
@@ -60,7 +60,7 @@ Ir al portal de firma: $sugarurl/index.php?entryPoint=sticSign&signerId=$stic_si
 <h1 style="margin: 0; font-size: 24px;">Documento firmado</h1>
 </div>
 <div style="padding: 20px;">
-<p style="font-size: 16px; color: #555555;">Adjunto a este correo encontrará la versión final del documento que ha firmado electrónicamente.</p>
+<p style="font-size: 16px; color: #555555;">Adjunto a este correo encontrará la versión final del documento que ha firmado:</p>
 <div style="background-color: #f9f9f9; padding: 15px; border-radius: 6px; border: 1px solid #eeeeee; margin-top: 20px;">
 <p style="margin: 0; font-size: 16px; color: #333333;"><strong>$stic_signatures_pdf_template</strong></p>
 <p style="margin: 5px 0 0 0; font-size: 14px; color: #888888;"><strong>Firmante:</strong> $contact_full_name$contact_user_full_name</p>
@@ -74,7 +74,7 @@ Ir al portal de firma: $sugarurl/index.php?entryPoint=sticSign&signerId=$stic_si
 </html>',0,'1',0,'notification'),
 	 ('000005f1-2e4e-3b11-051f-68e3c9e70332','2025-11-04 16:01:43','2025-11-04 15:18:28','2','1','off','Codigo de un solo uso para firmar','Plantilla para enviar el código de un solo uso al firmante antes de poder firmar.','[Firma de documento] Código de verificación para firmar','Verificación de firma
 
-Para garantizar la seguridad y autenticidad de su firma, utilice el código de verificación que se muestra a continuación.
+Para garantizar la seguridad y autenticidad de su firma, utilice el código de verificación que se muestra a continuación:
 
 $stic_signatures_pdf_template
 
@@ -91,7 +91,7 @@ Introduzca este código en el portal de firma para proceder. El código es váli
 <h1 style="margin: 0; font-size: 24px;">Verificación de firma</h1>
 </div>
 <div style="padding: 20px;">
-<p style="font-size: 16px; color: #555555;">Para garantizar la seguridad y autenticidad de su firma, utilice el código de verificación que se muestra a continuación.</p>
+<p style="font-size: 16px; color: #555555;">Para garantizar la seguridad y autenticidad de su firma, utilice el código de verificación que se muestra a continuación:</p>
 <div style="background-color: #f9f9f9; padding: 15px; border-radius: 6px; border: 1px solid #eeeeee; margin-top: 20px;">
 <p style="margin: 0; font-size: 16px; color: #333333;"><strong>$stic_signatures_pdf_template</strong></p>
 <div style="text-align: center; margin-top: 10px; margin-bottom: 10px;"><span style="display: inline-block; padding: 15px 30px; font-size: 28px; color: #000000; background-color: #b5bc31; border-radius: 5px; font-weight: bold; letter-spacing: 5px;">$stic_signers_otp</span></div>
