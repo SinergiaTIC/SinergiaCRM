53aac33cacc24f5616de76bfb52d7c6fbe1fc3eb
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-11-24 17:51:52 +0100

    New translations es_es.sticlang.php (Galician)

--- custom/Extension/modules/AOS_Products/Ext/Language/gl_ES.SticLang.php
+++ custom/Extension/modules/AOS_Products/Ext/Language/gl_ES.SticLang.php
@@ -20,7 +20,7 @@
  *
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
-$mod_strings['LBL_DEFAULT_PANEL'] = 'Datos xerais';
-$mod_strings['LBL_PANEL_RECORD_DETAILS'] = 'Detalles do rexistro';
-$mod_strings['LBL_CONTACT'] = 'Persoa';
+$mod_strings['LBL_DEFAULT_PANEL'] = 'Datos generales';
+$mod_strings['LBL_PANEL_RECORD_DETAILS'] = 'Detalles del registro';
+$mod_strings['LBL_CONTACT'] = 'Persona';
 $mod_strings['LBL_ACCOUNT_NAME'] = 'Organización';
