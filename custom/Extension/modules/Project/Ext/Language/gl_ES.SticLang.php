17cc7bc58011ff157d797c844805db4564066dc1
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-11-24 17:52:58 +0100

    New translations es_es.sticlang.php (Galician)

--- custom/Extension/modules/Project/Ext/Language/gl_ES.SticLang.php
+++ custom/Extension/modules/Project/Ext/Language/gl_ES.SticLang.php
@@ -20,43 +20,43 @@
  *
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
-$mod_strings['LBL_ASSIGNED_TO_ID'] = 'Asignado a';
+$mod_strings['LBL_ASSIGNED_TO_ID'] = 'Asignado a (ID)';
 $mod_strings['LBL_ASSIGNED_TO_NAME'] = 'Asignado a';
 $mod_strings['LBL_ASSIGNED_TO'] = 'Asignado a';
 $mod_strings['LBL_LIST_ASSIGNED_TO_NAME'] = 'Asignado a';
 $mod_strings['LBL_LIST_ASSIGNED_USER'] = 'Asignado a';
 $mod_strings['LBL_ASSIGNED_USER_NAME'] = 'Asignado a';
-$mod_strings['LBL_LIST_ASSIGNED_USER_ID'] = 'Asignado a';
+$mod_strings['LBL_LIST_ASSIGNED_USER_ID'] = 'Asignado a (ID)';
 $mod_strings['LBL_CREATED'] = 'Creado por';
 $mod_strings['LBL_CREATED_USER'] = 'Creado por';
-$mod_strings['LBL_CREATED_ID'] = 'Creado por';
+$mod_strings['LBL_CREATED_ID'] = 'Creado por (ID)';
 $mod_strings['LBL_MODIFIED'] = 'Modificado por';
 $mod_strings['LBL_MODIFIED_NAME'] = 'Modificado por';
 $mod_strings['LBL_MODIFIED_USER'] = 'Modificado por';
-$mod_strings['LBL_MODIFIED_ID'] = 'Modificado por';
-$mod_strings['LNK_PROJECT_LIST'] = 'Ver Proxectos';
-$mod_strings['LBL_OVERVIEW_PANEL'] = 'Datos xerais';
-$mod_strings['LBL_STIC_PANEL_RECORD_DETAILS'] = 'Detalles do rexistro';
-$mod_strings['LBL_STIC_CONTACTS_RELATIONSHIPS_PROJECT_FROM_STIC_CONTACTS_RELATIONSHIPS_TITLE'] = 'Relacións con Persoas';
+$mod_strings['LBL_MODIFIED_ID'] = 'Modificado por (ID)';
+$mod_strings['LNK_PROJECT_LIST'] = 'Ver Proyectos';
+$mod_strings['LBL_OVERVIEW_PANEL'] = 'Datos generals';
+$mod_strings['LBL_STIC_PANEL_RECORD_DETAILS'] = 'Detalles del registro';
+$mod_strings['LBL_STIC_CONTACTS_RELATIONSHIPS_PROJECT_FROM_STIC_CONTACTS_RELATIONSHIPS_TITLE'] = 'Relaciones con Personas';
 $mod_strings['LBL_STIC_PAYMENT_COMMITMENTS_PROJECT_FROM_STIC_PAYMENT_COMMITMENTS_TITLE'] = 'Compromisos de Pago';
 $mod_strings['LBL_STIC_EVENTS_PROJECT_FROM_STIC_EVENTS_TITLE'] = 'Eventos';
-$mod_strings['LBL_STIC_ACCOUNTS_RELATIONSHIPS_PROJECT_FROM_STIC_ACCOUNTS_RELATIONSHIPS_TITLE'] = 'Relacións con Organizacións';
-$mod_strings['LBL_PROJECT_OPPORTUNITIES_1_FROM_OPPORTUNITIES_TITLE'] = 'Subvencións';
-$mod_strings['LBL_OPPORTUNITIES_SUBPANEL_TITLE'] = 'Subvencións';
-$mod_strings['LBL_STIC_FOLLOWUPS_PROJECT_FROM_STIC_FOLLOWUPS_TITLE'] = 'Seguimentos';
-$mod_strings['LBL_STIC_GOALS_PROJECT_FROM_STIC_GOALS_TITLE'] = 'Obxectivos';
-$mod_strings['LBL_STIC_LOCATION'] = 'Ubicacións';
-$mod_strings['LBL_ESTIMATED_END_DATE_ERROR'] = 'A data de finalización debe ser posterior á data de inicio.';
-$mod_strings['LBL_ESTIMATED_START_DATE_ERROR'] = 'A data de inicio debe ser anterior á data de finalización.';
-$mod_strings['LBL_VALIDATE_TASK_NAME'] = 'O nome da tarefa é obrigatorio.';
-$mod_strings['LBL_VALIDATE_START'] = 'A data de inicio da tarea é obrigatoria.';
-$mod_strings['LBL_VALIDATE_DURATION_REQUIRED'] = 'A duración da tarefa é obrigatoria.';
+$mod_strings['LBL_STIC_ACCOUNTS_RELATIONSHIPS_PROJECT_FROM_STIC_ACCOUNTS_RELATIONSHIPS_TITLE'] = 'Relaciones con Organizaciones';
+$mod_strings['LBL_PROJECT_OPPORTUNITIES_1_FROM_OPPORTUNITIES_TITLE'] = 'Subvenciones';
+$mod_strings['LBL_OPPORTUNITIES_SUBPANEL_TITLE'] = 'Subvenciones';
+$mod_strings['LBL_STIC_FOLLOWUPS_PROJECT_FROM_STIC_FOLLOWUPS_TITLE'] = 'Seguimientos';
+$mod_strings['LBL_STIC_GOALS_PROJECT_FROM_STIC_GOALS_TITLE'] = 'Objetivos';
+$mod_strings['LBL_STIC_LOCATION'] = 'Ubicaciones';
+$mod_strings['LBL_ESTIMATED_END_DATE_ERROR'] = 'La fecha de finalización debe ser igual o posterior a la fecha de inicio.';
+$mod_strings['LBL_ESTIMATED_START_DATE_ERROR'] = 'La fecha de inicio debe ser igual o anterior a la fecha de finalización.';
+$mod_strings['LBL_VALIDATE_TASK_NAME'] = 'El nombre de la tarea es obligatorio.';
+$mod_strings['LBL_VALIDATE_START'] = 'La fecha de inicio de la tarea es obligatoria.';
+$mod_strings['LBL_VALIDATE_DURATION_REQUIRED'] = 'La duración de la tarea es obligatoria.';
 $mod_strings['LBL_VALIDATE_DURATION_NUMBER'] = 'Indique un número.';
-$mod_strings['LBL_VALIDATE_COMPLETE_REQUIRED'] = 'A porcentaxe de tarefa completada é obrigatoria.';
+$mod_strings['LBL_VALIDATE_COMPLETE_REQUIRED'] = 'El porcentaje de tarea completado es obligatorio.';
 $mod_strings['LBL_VALIDATE_COMPLETE_NUMBER'] =  'Indique un número.';
-$mod_strings['LBL_VALIDATE_COMPLETE_MAX'] = 'O valor máximo posible é 100.';
-$mod_strings['LBL_DELETE_TASK_CONFIRM'] = '¿Seguro que quere borrar esta tarefa?';
+$mod_strings['LBL_VALIDATE_COMPLETE_MAX'] = 'El valor máximo posible es 100.';
+$mod_strings['LBL_DELETE_TASK_CONFIRM'] = '¿Seguro que quiere borrar esta tarea?';
 
-// Cadeas Ayudas
-$mod_strings['LBL_STIC_GRANTS_PROJECT_FROM_STIC_GRANTS_TITLE'] = 'Axudas';
+// Cadenas Ayudas
+$mod_strings['LBL_STIC_GRANTS_PROJECT_FROM_STIC_GRANTS_TITLE'] = 'Ayudas';
 $mod_strings['LBL_STIC_CENTERS_PROJECT_FROM_STIC_CENTERS_TITLE'] = 'Centros';
