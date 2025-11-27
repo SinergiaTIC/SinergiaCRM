9e047fbda656ad9d5239a2f06219dfc7c18d2ebe
ainaraRT <ainaretework@gmail.com>
2025-11-27 12:45:12 +0000

    wip

--- modules/stic_Transactions/metadata/subpanels/default.php
+++ modules/stic_Transactions/metadata/subpanels/default.php
@@ -34,31 +34,7 @@ $subpanel_layout = array(
 
     'where' => '',
 
-    'list_fields' => array(
-        // STIC-Custom 20240715 MHP - https://github.com/SinergiaTIC/SinergiaCRM/pull/62
-        // Add the file name and options to download and view the file in a new tab in the subpanel of a new module of type File    
-        // 'object_image' => array(
-        //     'widget_class' => 'SubPanelIcon',
-        //     'width' => '2%',
-        //     'image2' => 'attachment',
-        //     'image2_url_field' => array(
-        //         'id_field' => 'selected_revision_id',
-        //         'filename_field' => 'selected_revision_filename'
-        //     ),
-        //     'attachment_image_only' => true,
-
-        // ),
-        // 'document_name' => array(
-        //     'name' => 'document_name',
-        //     'vname' => 'LBL_LIST_DOCUMENT_NAME',
-        //     'widget_class' => 'SubPanelDetailViewLink',
-        //     'width' => '45%',
-        // ),
-        // 'active_date' => array(
-        //     'name' => 'active_date',
-        //     'vname' => 'LBL_DOC_ACTIVE_DATE',
-        //     'width' => '45%',
-        // ),        
+    'list_fields' => array(       
         'document_name' => array(
             'name' => 'document_name',
             'vname' => 'LBL_NAME',
