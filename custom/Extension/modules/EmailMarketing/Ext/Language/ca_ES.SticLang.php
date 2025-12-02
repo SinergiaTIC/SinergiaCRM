b80e60c53119bf3ee4985c00d7abff04cf5cbf65
Jaume Albaigès <45965117+jalbaiges@users.noreply.github.com>
2025-12-02 13:49:41 +0100

    Update email display message for clarity

--- custom/Extension/modules/EmailMarketing/Ext/Language/ca_ES.SticLang.php
+++ custom/Extension/modules/EmailMarketing/Ext/Language/ca_ES.SticLang.php
@@ -21,4 +21,4 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $mod_strings['LBL_SCHEDULE_MESSAGE_EMAILS'] = "Seleccioneu els missatges dels quals vulgueu programar el seu enviament. Recordeu que en aquests enviaments s'aplicaran totes les llistes de públic objectiu d'exclusió associades a la campanya.";
-$mod_strings['LBL_STIC_DISPLAY_HTML'] = "Adjuntar un enllaç al cos del correu electrònic perquè l'usuari pugui veure el contingut del correu electrònic al navegador?";
+$mod_strings['LBL_STIC_DISPLAY_HTML'] = "Inclou un enllaç al missatge perquè el destinatari pugui veure'n el contingut al navegador";
