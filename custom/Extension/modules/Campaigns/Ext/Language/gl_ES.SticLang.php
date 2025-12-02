067a72a069846dec22dc8341dcb1812462b1ed75
Jaume Albaigès <45965117+jalbaiges@users.noreply.github.com>
2025-12-02 13:54:51 +0100

    Improving translation

--- custom/Extension/modules/Campaigns/Ext/Language/gl_ES.SticLang.php
+++ custom/Extension/modules/Campaigns/Ext/Language/gl_ES.SticLang.php
@@ -78,5 +78,5 @@ $mod_strings['LBL_NOTIFICATION_TEMPLATE_ID_HELP'] = "A plantilla de correo elect
 $mod_strings['LBL_NOTIFICATION_OUTBOUND_EMAIL_ID_HELP'] = "Conta de correo que se utilizará para enviar a notificación. Pódese configurar no apartado 'Correo electrónico saínte' da área de Adminstración.";
 $mod_strings['LBL_NOTIFICATION_INBOUND_EMAIL_ID_HELP'] = "Buzón de correo onde recibir as mensaxes de erro que se poidan producir tralo envío. Pódese configurar no apartado 'Correo electrónico entrante' da área de Administración.";
 
-$mod_strings['LBL_NOTIFICATION_NEW_INFO'] = "Ao gardar a notificación, os correos a enviar engadiranse automaticamente á cola de envío.";
-$mod_strings['LBL_STIC_DISPLAY_HTML'] = 'Adjuntar un enlace en el cuerpo del correo electrónico para que el usuario pueda ver el contenido del correo electrónico en el navegador?';
+$mod_strings['LBL_NOTIFICATION_NEW_INFO'] = 'Ao gardar a notificación, os correos a enviar engadiranse automaticamente á cola de envío.';
+$mod_strings['LBL_STIC_DISPLAY_HTML'] = 'Incluir un enlace en el mensaje para ver el contenido en el navegador';
