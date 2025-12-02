ada256001e26ec61aafceffa0a899b327a78a7e6
Jaume Albaigès <45965117+jalbaiges@users.noreply.github.com>
2025-12-02 13:59:25 +0100

    Improving translation

--- custom/Extension/modules/EmailMarketing/Ext/Language/es_ES.SticLang.php
+++ custom/Extension/modules/EmailMarketing/Ext/Language/es_ES.SticLang.php
@@ -21,4 +21,4 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $mod_strings['LBL_SCHEDULE_MESSAGE_EMAILS'] = 'Seleccione los mensajes que desee programar para su envío. Recuerde que en estos envíos se aplicarán todas las listas de público objetivo de exclusión asociadas a la campaña.';
-$mod_strings['LBL_STIC_DISPLAY_HTML'] = 'Adjuntar un enlace en el cuerpo del correo electrónico para que el usuario pueda ver el contenido del correo electrónico en el navegador?';
+$mod_strings['LBL_STIC_DISPLAY_HTML'] = 'Incluir un enlace en el mensaje para ver el contenido en el navegador';
