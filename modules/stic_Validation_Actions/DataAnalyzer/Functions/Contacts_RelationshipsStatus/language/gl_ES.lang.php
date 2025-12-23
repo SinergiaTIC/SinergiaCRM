2013fc39f79b92f44e3d598cdc12c952fa13a323
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2025-12-23 14:34:07 +0100

    New translations es_es.lang.php (Galician)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/Contacts_RelationshipsStatus/language/gl_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/Contacts_RelationshipsStatus/language/gl_ES.lang.php
@@ -21,7 +21,7 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-    'NAME' => 'Persoas - Revisión do tipo de relación',		// Esta etiqueta é obrigatoria. Indica o nome da función
-    'NO_ROWS' => 'Todas as persoas teñen o tipo de relación correcto.',
-    'UPDATED' => 'Actualizouse o tipo de relación.',
+    'NAME' => 'Personas - Revisión del tipo de relación',		// Esta etiqueta es obligatoria. Indica el nombre de la función
+    'NO_ROWS' => 'Todas las personas tienen el tipo de relación correcto.',
+    'UPDATED' => 'Se ha actualizado el tipo de relación.',
 );
