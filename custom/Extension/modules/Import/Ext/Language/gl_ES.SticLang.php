fab0b2eb21e44e71b2d6f626d6e88cb33ffc519f
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-02 13:46:12 +0100

    New translations es_es.sticlang.php (Galician)

--- custom/Extension/modules/Import/Ext/Language/gl_ES.SticLang.php
+++ custom/Extension/modules/Import/Ext/Language/gl_ES.SticLang.php
@@ -20,5 +20,5 @@
  *
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
-$mod_strings['LBL_INSTRUCTION_FILE_VALIDATION'] = 'Antes de iniciar a importación pode validar os datos do ficheiro.';
-$mod_strings['LBL_FILE_VALIDATION_BUTTON'] = 'Validar o ficheiro de importación';
+$mod_strings['LBL_INSTRUCTION_FILE_VALIDATION'] = '';
+$mod_strings['LBL_FILE_VALIDATION_BUTTON'] = '';
