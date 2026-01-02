5f0974012ed06c65d74577df1a5256b5fb77c106
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-02 13:45:35 +0100

    New translations es_es.sticlang.php (Basque)

--- custom/Extension/modules/EmailMarketing/Ext/Language/eu_ES.SticLang.php
+++ custom/Extension/modules/EmailMarketing/Ext/Language/eu_ES.SticLang.php
@@ -20,4 +20,4 @@
  *
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
-$mod_strings['LBL_SCHEDULE_MESSAGE_EMAILS'] = 'Seleccione los mensajes que desee programar para su envío. Recuerde que en estos envíos se aplicarán todas las listas de público objetivo de exclusión asociadas a la campaña.';
+$mod_strings['LBL_SCHEDULE_MESSAGE_EMAILS'] = '';
