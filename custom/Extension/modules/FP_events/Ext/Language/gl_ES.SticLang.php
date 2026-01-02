ee57f76d2bd35e9a28c59971c07319bbf97c036c
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-02 13:46:02 +0100

    New translations es_es.sticlang.php (Galician)

--- custom/Extension/modules/FP_events/Ext/Language/gl_ES.SticLang.php
+++ custom/Extension/modules/FP_events/Ext/Language/gl_ES.SticLang.php
@@ -21,8 +21,8 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 // This module is not used in SinergiaCRM and it's relabelled to avoid confusion with SinergiaCRM Events module
-$mod_strings['LNK_NEW_RECORD'] = 'Crear Evento-FP';
-$mod_strings['LNK_LIST'] = 'Ver Eventos-FP';
-$mod_strings['LBL_LIST_FORM_TITLE'] = 'Lista de Eventos-FP';
-$mod_strings['LBL_SEARCH_FORM_TITLE'] = 'Buscar Eventos-FP';
-$mod_strings['LBL_HOMEPAGE_TITLE'] = 'Os meus Eventos-FP';
+$mod_strings['LNK_NEW_RECORD'] = '';
+$mod_strings['LNK_LIST'] = '';
+$mod_strings['LBL_LIST_FORM_TITLE'] = '';
+$mod_strings['LBL_SEARCH_FORM_TITLE'] = '';
+$mod_strings['LBL_HOMEPAGE_TITLE'] = '';
