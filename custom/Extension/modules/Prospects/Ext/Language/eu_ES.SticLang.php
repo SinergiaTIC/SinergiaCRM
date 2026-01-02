04a330184ee08599f502390d8350bdb314ce3d64
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-02 14:10:22 +0100

    New translations es_es.sticlang.php (Basque)

--- custom/Extension/modules/Prospects/Ext/Language/eu_ES.SticLang.php
+++ custom/Extension/modules/Prospects/Ext/Language/eu_ES.SticLang.php
@@ -20,8 +20,8 @@
  *
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
-$mod_strings['LBL_FP_EVENTS_PROSPECTS_1_FROM_FP_EVENTS_TITLE'] = ''; // This module is not used in SinergiaCRM and it's relabelled to avoid confusion with SinergiaCRM Events module
-$mod_strings['LBL_PROSPECTS_DOCUMENTS_1_FROM_DOCUMENTS_TITLE'] = '';
-$mod_strings['LBL_ATTACHMENT_DESCRIPTION'] = '';
+$mod_strings['LBL_FP_EVENTS_PROSPECTS_1_FROM_FP_EVENTS_TITLE'] = 'Eventos-FP'; // This module is not used in SinergiaCRM and it's relabelled to avoid confusion with SinergiaCRM Events module
+$mod_strings['LBL_PROSPECTS_DOCUMENTS_1_FROM_DOCUMENTS_TITLE'] = 'Documentos';
+$mod_strings['LBL_ATTACHMENT_DESCRIPTION'] = 'Fichero recibido desde un formulario web relacionado con la campaña: ';
 
 
