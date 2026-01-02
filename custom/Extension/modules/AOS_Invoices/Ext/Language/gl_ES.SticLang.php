327e87853eef3e7d1e00d02f53051328c4ff4649
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-02 13:44:22 +0100

    New translations es_es.sticlang.php (Galician)

--- custom/Extension/modules/AOS_Invoices/Ext/Language/gl_ES.SticLang.php
+++ custom/Extension/modules/AOS_Invoices/Ext/Language/gl_ES.SticLang.php
@@ -20,21 +20,21 @@
  *
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
-$mod_strings['LBL_DEFAULT_PANEL'] = 'Datos xerais';
-$mod_strings['LBL_PANEL_RECORD_DETAILS'] = 'Detalles do rexistro';
-$mod_strings['LBL_ACCOUNT'] = 'Organización';
-$mod_strings['LBL_CONTACTS_SUBPANEL_TITLE'] = 'Persoas';
-$mod_strings['LBL_DEFAULT_SUBPANEL_TITLE'] = 'Organizacións';
-$mod_strings['LBL_DUPLICATE'] = 'Posible organización duplicada';
-$mod_strings['LBL_LIST_ACCOUNT_NAME'] = 'Organización';
-$mod_strings['LBL_PARENT_ACCOUNT_ID'] = 'ID da Organización pai';
-$mod_strings['LBL_SAVE_ACCOUNT'] = 'Gardar a Organización';
-$mod_strings['LNK_ACCOUNT_LIST'] = 'Organizacións';
-$mod_strings['LNK_NEW_ACCOUNT'] = 'Nova Organización';
-$mod_strings['MSG_DUPLICATE'] = 'O rexistro que está a punto de crear podería ser un duplicado doutra organización existente. Os rexistros de organización con nomes similares lístanse a continuación. Para confirmar a creación desta organización faga clic en Gardar. En caso contrario, pulse Cancelar.';
-$mod_strings['MSG_SHOW_DUPLICATES'] = 'O rexistro que está a punto de crear podería ser un duplicado doutra organización existente. Os rexistros de organización con nomes similares lístanse a continuación. Para confirmar a creación desta organización faga clic en Gardar. En caso contrario, pulse Cancelar.';
-$mod_strings['LBL_BILLING_ACCOUNT'] = 'Organización';
-$mod_strings['LBL_BILLING_CONTACT'] = 'Persoa';
-$mod_strings['LBL_OPPORTUNITY'] = 'Subvención';
-$mod_strings['LBL_DUE_DATE_ERROR'] = 'A data de vencemento debe ser igual ou posterior á data de facturación.';
-$mod_strings['LBL_INVOICE_DATE_ERROR'] = 'A data de facturación debe ser igual ou anterior á data de vencemento.';
+$mod_strings['LBL_DEFAULT_PANEL'] = '';
+$mod_strings['LBL_PANEL_RECORD_DETAILS'] = '';
+$mod_strings['LBL_ACCOUNT'] = '';
+$mod_strings['LBL_CONTACTS_SUBPANEL_TITLE'] = '';
+$mod_strings['LBL_DEFAULT_SUBPANEL_TITLE'] = '';
+$mod_strings['LBL_DUPLICATE'] = '';
+$mod_strings['LBL_LIST_ACCOUNT_NAME'] = '';
+$mod_strings['LBL_PARENT_ACCOUNT_ID'] = '';
+$mod_strings['LBL_SAVE_ACCOUNT'] = '';
+$mod_strings['LNK_ACCOUNT_LIST'] = '';
+$mod_strings['LNK_NEW_ACCOUNT'] = '';
+$mod_strings['MSG_DUPLICATE'] = '';
+$mod_strings['MSG_SHOW_DUPLICATES'] = '';
+$mod_strings['LBL_BILLING_ACCOUNT'] = '';
+$mod_strings['LBL_BILLING_CONTACT'] = '';
+$mod_strings['LBL_OPPORTUNITY'] = '';
+$mod_strings['LBL_DUE_DATE_ERROR'] = '';
+$mod_strings['LBL_INVOICE_DATE_ERROR'] = '';
