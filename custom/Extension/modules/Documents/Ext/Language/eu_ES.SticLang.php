4096343d4be43ea3f743fcc552ad5d8487240ece
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-02 13:45:30 +0100

    New translations es_es.sticlang.php (Basque)

--- custom/Extension/modules/Documents/Ext/Language/eu_ES.SticLang.php
+++ custom/Extension/modules/Documents/Ext/Language/eu_ES.SticLang.php
@@ -20,32 +20,32 @@
  *
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
-$mod_strings['LBL_ASSIGNED_TO_ID'] = 'Asignado a (ID)';
-$mod_strings['LBL_ASSIGNED_TO_NAME'] = 'Asignado a';
-$mod_strings['LBL_ASSIGNED_TO'] = 'Asignado a';
-$mod_strings['LBL_LIST_ASSIGNED_TO_NAME'] = 'Asignado a';
-$mod_strings['LBL_LIST_ASSIGNED_USER'] = 'Asignado a';
-$mod_strings['LBL_CREATED'] = 'Creado por';
-$mod_strings['LBL_CREATED_USER'] = 'Creado por';
-$mod_strings['LBL_CREATED_ID'] = 'Creado por (ID)';
-$mod_strings['LBL_MODIFIED'] = 'Modificado por';
-$mod_strings['LBL_MODIFIED_NAME'] = 'Modificado por';
-$mod_strings['LBL_MODIFIED_USER'] = 'Modificado por';
-$mod_strings['LBL_MODIFIED_ID'] = 'Modificado por (ID)';
-$mod_strings['LBL_LEADS_DOCUMENTS_1_FROM_LEADS_TITLE'] = 'Interesados';
-$mod_strings['LBL_OVERVIEW_PANEL'] = 'Datos generales';
-$mod_strings['LBL_STIC_PANEL_RECORD_DETAILS'] = 'Detalles del registro';
-$mod_strings['LBL_STIC_SHARED_DOCUMENT_LINK'] = 'Enlace externo';
-$mod_strings['LBL_STIC_SESSIONS_DOCUMENTS_FROM_STIC_SESSIONS_TITLE'] = 'Sesión';
-$mod_strings['LBL_ACCOUNTS_SUBPANEL_TITLE'] = 'Organizaciones';
-$mod_strings['LBL_CONTACTS_SUBPANEL_TITLE'] = 'Personas';
-$mod_strings['LBL_OPPORTUNITIES_SUBPANEL_TITLE'] = 'Subvenciones';
-$mod_strings['LBL_STIC_JOB_APPLICATIONS_DOCUMENTS_FROM_STIC_JOB_APPLICATIONS_TITLE'] = 'Candidaturas';
-$mod_strings['LBL_STIC_JOB_OFFERS_DOCUMENTS_FROM_STIC_JOB_OFFERS_TITLE'] = 'Ofertas laborales';
-$mod_strings['LBL_PROSPECTS_DOCUMENTS_1_FROM_PROSPECTS_TITLE'] = 'Público Objetivo';
-$mod_strings['LBL_STIC_FAMILIES_DOCUMENTS_FROM_STIC_FAMILIES_TITLE'] = 'Unidades familiares';
-$mod_strings['LBL_STIC_GROUP_OPPORTUNITIES_DOCUMENTS_1_FROM_STIC_GROUP_OPPORTUNITIES_TITLE'] = 'Organizaciones subvencionadas';
+$mod_strings['LBL_ASSIGNED_TO_ID'] = '';
+$mod_strings['LBL_ASSIGNED_TO_NAME'] = '';
+$mod_strings['LBL_ASSIGNED_TO'] = '';
+$mod_strings['LBL_LIST_ASSIGNED_TO_NAME'] = '';
+$mod_strings['LBL_LIST_ASSIGNED_USER'] = '';
+$mod_strings['LBL_CREATED'] = '';
+$mod_strings['LBL_CREATED_USER'] = '';
+$mod_strings['LBL_CREATED_ID'] = '';
+$mod_strings['LBL_MODIFIED'] = '';
+$mod_strings['LBL_MODIFIED_NAME'] = '';
+$mod_strings['LBL_MODIFIED_USER'] = '';
+$mod_strings['LBL_MODIFIED_ID'] = '';
+$mod_strings['LBL_LEADS_DOCUMENTS_1_FROM_LEADS_TITLE'] = '';
+$mod_strings['LBL_OVERVIEW_PANEL'] = '';
+$mod_strings['LBL_STIC_PANEL_RECORD_DETAILS'] = '';
+$mod_strings['LBL_STIC_SHARED_DOCUMENT_LINK'] = '';
+$mod_strings['LBL_STIC_SESSIONS_DOCUMENTS_FROM_STIC_SESSIONS_TITLE'] = '';
+$mod_strings['LBL_ACCOUNTS_SUBPANEL_TITLE'] = '';
+$mod_strings['LBL_CONTACTS_SUBPANEL_TITLE'] = '';
+$mod_strings['LBL_OPPORTUNITIES_SUBPANEL_TITLE'] = '';
+$mod_strings['LBL_STIC_JOB_APPLICATIONS_DOCUMENTS_FROM_STIC_JOB_APPLICATIONS_TITLE'] = '';
+$mod_strings['LBL_STIC_JOB_OFFERS_DOCUMENTS_FROM_STIC_JOB_OFFERS_TITLE'] = '';
+$mod_strings['LBL_PROSPECTS_DOCUMENTS_1_FROM_PROSPECTS_TITLE'] = '';
+$mod_strings['LBL_STIC_FAMILIES_DOCUMENTS_FROM_STIC_FAMILIES_TITLE'] = '';
+$mod_strings['LBL_STIC_GROUP_OPPORTUNITIES_DOCUMENTS_1_FROM_STIC_GROUP_OPPORTUNITIES_TITLE'] = '';
 
 // Campos de filtro en relaciones muchos a muchos
-$mod_strings['LBL_STIC_DOCUMENTS_ACCOUNTS_NAME'] = 'Organización';
+$mod_strings['LBL_STIC_DOCUMENTS_ACCOUNTS_NAME'] = '';
 $mod_strings['LBL_STIC_DOCUMENTS_CONTACTS_NAME'] = 'Pertsona';
