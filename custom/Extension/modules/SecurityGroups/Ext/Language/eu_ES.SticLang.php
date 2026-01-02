b80d8e08086b3d4a02d2f4598f4159bb37bbb07a
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-02 13:47:04 +0100

    New translations es_es.sticlang.php (Basque)

--- custom/Extension/modules/SecurityGroups/Ext/Language/eu_ES.SticLang.php
+++ custom/Extension/modules/SecurityGroups/Ext/Language/eu_ES.SticLang.php
@@ -20,19 +20,19 @@
  *
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
-$mod_strings['LBL_ASSIGNED_TO_ID'] = 'Asignado a (ID)';
-$mod_strings['LBL_ASSIGNED_TO_NAME'] = 'Asignado a';
-$mod_strings['LBL_ASSIGNED_TO'] = 'Asignado a';
-$mod_strings['LBL_LIST_ASSIGNED_TO_NAME'] = 'Asignado a';
-$mod_strings['LBL_LIST_ASSIGNED_USER'] = 'Asignado a';
-$mod_strings['LBL_CREATED'] = 'Creado por';
-$mod_strings['LBL_CREATED_USER'] = 'Creado por';
-$mod_strings['LBL_CREATED_ID'] = 'Creado por (ID)';
-$mod_strings['LBL_MODIFIED'] = 'Modificado por';
-$mod_strings['LBL_MODIFIED_NAME'] = 'Modificado por';
-$mod_strings['LBL_MODIFIED_USER'] = 'Modificado por';
-$mod_strings['LBL_MODIFIED_ID'] = 'Modificado por (ID)';
-$mod_strings['LBL_STIC_PANEL_RECORD_DETAILS'] = 'Detalles del registro';
-$mod_strings['LBL_STIC_GENERAL_INFORMATION'] = 'Datos generales';
-$mod_strings['LBL_STIC_SECURITY_GROUPS_RULES_ENABLED'] = 'Habilitar las reglas por módulos';
-$mod_strings['LBL_STIC_SECURITY_GROUPS_RULES_ENABLED_INFO'] = 'Las reglas definidas para cada módulo tendrán preferencia sobre las reglas generales definidas aquí.';
+$mod_strings['LBL_ASSIGNED_TO_ID'] = '';
+$mod_strings['LBL_ASSIGNED_TO_NAME'] = '';
+$mod_strings['LBL_ASSIGNED_TO'] = '';
+$mod_strings['LBL_LIST_ASSIGNED_TO_NAME'] = '';
+$mod_strings['LBL_LIST_ASSIGNED_USER'] = '';
+$mod_strings['LBL_CREATED'] = '';
+$mod_strings['LBL_CREATED_USER'] = '';
+$mod_strings['LBL_CREATED_ID'] = '';
+$mod_strings['LBL_MODIFIED'] = '';
+$mod_strings['LBL_MODIFIED_NAME'] = '';
+$mod_strings['LBL_MODIFIED_USER'] = '';
+$mod_strings['LBL_MODIFIED_ID'] = '';
+$mod_strings['LBL_STIC_PANEL_RECORD_DETAILS'] = '';
+$mod_strings['LBL_STIC_GENERAL_INFORMATION'] = '';
+$mod_strings['LBL_STIC_SECURITY_GROUPS_RULES_ENABLED'] = '';
+$mod_strings['LBL_STIC_SECURITY_GROUPS_RULES_ENABLED_INFO'] = '';
