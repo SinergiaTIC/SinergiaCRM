dccdfe5d4aede59d84bcd5930e324a6ad711b874
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-02 13:41:04 +0100

    New translations es_es.lang.php (Basque)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/AccountsRelationships_RelationshipsStatus/language/eu_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/AccountsRelationships_RelationshipsStatus/language/eu_ES.lang.php
@@ -21,6 +21,6 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-    'NAME' => 'Relaciones Organizaciones - Revisión de las relaciones',		// Esta etiqueta es obligatoria. Indica el nombre de la función
-    'NO_LINK' => 'No tiene ninguna organización vinculada o no es válida.',
+    'NAME' => '',		// Esta etiqueta es obligatoria. Indica el nombre de la función
+    'NO_LINK' => '',
 );
