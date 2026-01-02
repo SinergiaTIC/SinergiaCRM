ff07522da0044378457e89e2297e42d03384705e
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-02 13:51:01 +0100

    New translations es_es.lang.php (Basque)

--- modules/ACL/language/eu_ES.lang.php
+++ modules/ACL/language/eu_ES.lang.php
@@ -43,17 +43,17 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_ROLE' => '',
-    'LBL_NAME' => '',
-    'LBL_DESCRIPTION' => '',
-    'LIST_ROLES' => '',
-    'LBL_USERS_SUBPANEL_TITLE' => '',
-    'LIST_ROLES_BY_USER' => '',
-    'LBL_ROLES_SUBPANEL_TITLE' => '',
-    'LBL_SEARCH_FORM_TITLE' => '',
-    'LBL_NO_ACCESS' => '',
-    'LBL_REDIRECT_TO_HOME' => '',
-    'LBL_SECONDS' => '',
-    'LBL_ADDING' => '',
+    'LBL_ROLE' => 'Rol',
+    'LBL_NAME' => 'Nombre',
+    'LBL_DESCRIPTION' => 'Descripción',
+    'LIST_ROLES' => 'Listar Roles',
+    'LBL_USERS_SUBPANEL_TITLE' => 'Usuarios',
+    'LIST_ROLES_BY_USER' => 'Listar Roles por Usuarios',
+    'LBL_ROLES_SUBPANEL_TITLE' => 'Roles de Usuario',
+    'LBL_SEARCH_FORM_TITLE' => 'Búsqueda',
+    'LBL_NO_ACCESS' => 'No tiene acceso a esta área. Contacte con el Administrador de su sitio web para obtenerlo.',
+    'LBL_REDIRECT_TO_HOME' => 'Redirigir a la página principal en',
+    'LBL_SECONDS' => 'segundos',
+    'LBL_ADDING' => 'Añadiendo para ',
 
 );
