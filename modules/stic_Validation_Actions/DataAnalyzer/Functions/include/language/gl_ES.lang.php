d4c3af738055e6b370ef2cfb816a1d18ee899901
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-02 13:43:35 +0100

    New translations es_es.lang.php (Galician)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/include/language/gl_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/include/language/gl_ES.lang.php
@@ -21,11 +21,11 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-    'NO_ROWS' => 'Non se atopou rexistros a revisar.',
-    'NO_ERRORS' => 'Non se detectou ningún erro nos rexistros revisados.',
-    'ERR_FIELD' => ' con valor non válido.',
-    'WARN_FIELD' => ' cun valor menor ou igual a cero.',    
-    'EMPTY_REL' => 'Sen relación activa',
-    'INVALID_VALUE' => 'Valor erróneo',
-    'LIST_VIEW' => 'Vista de lista',
+    'NO_ROWS' => '',
+    'NO_ERRORS' => '',
+    'ERR_FIELD' => '',
+    'WARN_FIELD' => '',    
+    'EMPTY_REL' => '',
+    'INVALID_VALUE' => '',
+    'LIST_VIEW' => '',
 );
