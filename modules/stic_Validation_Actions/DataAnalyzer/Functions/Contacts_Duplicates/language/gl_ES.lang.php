9073de599781dc241ad183de65146e5b4cee6928
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-02 14:05:29 +0100

    New translations es_es.lang.php (Galician)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/Contacts_Duplicates/language/gl_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/Contacts_Duplicates/language/gl_ES.lang.php
@@ -21,7 +21,7 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-    'NAME' => '',		// Esta etiqueta es obligatoria. Indica el nombre de la función
-    'NO_DUPLICATED' => '',
-    'POSSIBLE_DUP' => '',
+    'NAME' => 'Personas - Búsqueda de duplicados',		// Esta etiqueta es obligatoria. Indica el nombre de la función
+    'NO_DUPLICATED' => 'No se han detectado duplicados.',
+    'POSSIBLE_DUP' => 'Coinciden en el campo ',
 );
