286c625c5a5e76ad0ec8013f96784c11feb025fd
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-02 13:40:59 +0100

    New translations es_es.lang.php (Basque)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/AccountsRelationships_MainData/language/eu_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/AccountsRelationships_MainData/language/eu_ES.lang.php
@@ -21,6 +21,6 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-    'NAME' => 'Relaciones Organizaciones - Revisión de los datos principales',		// Esta etiqueta es obligatoria. Indica el nombre de la función
-    'LEAVING_DATE' => 'La fecha de baja es anterior a la fecha de alta.',
+    'NAME' => '',		// Esta etiqueta es obligatoria. Indica el nombre de la función
+    'LEAVING_DATE' => '',
 );
