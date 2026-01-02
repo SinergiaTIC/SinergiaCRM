57490f75f8039f5e44572775aa672520afc2302b
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-02 13:46:44 +0100

    New translations es_es.sticlang.php (Basque)

--- custom/Extension/modules/ProjectTask/Ext/Language/eu_ES.SticLang.php
+++ custom/Extension/modules/ProjectTask/Ext/Language/eu_ES.SticLang.php
@@ -20,21 +20,21 @@
  *
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
-$mod_strings['LBL_ASSIGNED_TO_ID'] = 'Asignado a (ID)';
-$mod_strings['LBL_ASSIGNED_TO_NAME'] = 'Asignado a';
-$mod_strings['LBL_ASSIGNED_TO'] = 'Asignado a';
-$mod_strings['LBL_LIST_ASSIGNED_TO_NAME'] = 'Asignado a';
-$mod_strings['LBL_LIST_ASSIGNED_USER'] = 'Asignado a';
-$mod_strings['LBL_ASSIGNED_USER_NAME'] = 'Asignado a';
-$mod_strings['LBL_LIST_ASSIGNED_USER_ID'] = 'Asignado a (ID)';
-$mod_strings['LBL_CREATED'] = 'Creado por';
-$mod_strings['LBL_CREATED_USER'] = 'Creado por';
-$mod_strings['LBL_CREATED_ID'] = 'Creado por (ID)';
-$mod_strings['LBL_MODIFIED'] = 'Modificado por';
-$mod_strings['LBL_MODIFIED_NAME'] = 'Modificado por';
-$mod_strings['LBL_MODIFIED_USER'] = 'Modificado por';
-$mod_strings['LBL_MODIFIED_ID'] = 'Modificado por (ID)';
-$mod_strings['LBL_NAME'] = 'Nombre';
-$mod_strings['LBL_DEFAULT_PANEL'] = 'Datos generals';
-$mod_strings['LBL_STIC_PANEL_RECORD_DETAILS'] = 'Detalles del registro';
-$mod_strings['LNK_IMPORT_PROJECTTASK'] = 'Importar Tareas de Proyecto';
+$mod_strings['LBL_ASSIGNED_TO_ID'] = '';
+$mod_strings['LBL_ASSIGNED_TO_NAME'] = '';
+$mod_strings['LBL_ASSIGNED_TO'] = '';
+$mod_strings['LBL_LIST_ASSIGNED_TO_NAME'] = '';
+$mod_strings['LBL_LIST_ASSIGNED_USER'] = '';
+$mod_strings['LBL_ASSIGNED_USER_NAME'] = '';
+$mod_strings['LBL_LIST_ASSIGNED_USER_ID'] = '';
+$mod_strings['LBL_CREATED'] = '';
+$mod_strings['LBL_CREATED_USER'] = '';
+$mod_strings['LBL_CREATED_ID'] = '';
+$mod_strings['LBL_MODIFIED'] = '';
+$mod_strings['LBL_MODIFIED_NAME'] = '';
+$mod_strings['LBL_MODIFIED_USER'] = '';
+$mod_strings['LBL_MODIFIED_ID'] = '';
+$mod_strings['LBL_NAME'] = '';
+$mod_strings['LBL_DEFAULT_PANEL'] = '';
+$mod_strings['LBL_STIC_PANEL_RECORD_DETAILS'] = '';
+$mod_strings['LNK_IMPORT_PROJECTTASK'] = '';
