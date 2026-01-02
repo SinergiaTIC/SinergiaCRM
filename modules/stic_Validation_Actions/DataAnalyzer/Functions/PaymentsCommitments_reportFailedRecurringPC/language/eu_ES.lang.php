30e28bf411d3a195bf5d140f6e447a1f55535fba
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-02 14:06:18 +0100

    New translations es_es.lang.php (Basque)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/PaymentsCommitments_reportFailedRecurringPC/language/eu_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/PaymentsCommitments_reportFailedRecurringPC/language/eu_ES.lang.php
@@ -21,7 +21,7 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-    'NAME' => '',
-    'NO_ROWS' => '',
-    'RESULT_INFO' => '',
+    'NAME' => 'Compromisos de pago - Revisión de las autorizaciones recurrentes incompletas',
+    'NO_ROWS' => 'No se ha encontrado ningún compromiso de pago.',
+    'RESULT_INFO' => 'Este compromiso de pago recurrente con @payment_method@ no tiene la información necesaria de la pasarela de pago y tiene @payment_count@ pagos con estado pendiente que deberían ser revisados. Es posible que deba darse de baja el compromiso de pago.',
 );
