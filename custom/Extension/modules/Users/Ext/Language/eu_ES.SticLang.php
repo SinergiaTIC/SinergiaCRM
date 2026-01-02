1eb62de96ad7ed6bfaaf23a690bff2b7d129825f
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-02 13:47:34 +0100

    New translations es_es.sticlang.php (Basque)

--- custom/Extension/modules/Users/Ext/Language/eu_ES.SticLang.php
+++ custom/Extension/modules/Users/Ext/Language/eu_ES.SticLang.php
@@ -20,81 +20,81 @@
  *
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
-$mod_strings['LBL_ASSIGNED_TO_ID'] = 'Asignado a (ID)';
-$mod_strings['LBL_ASSIGNED_TO_NAME'] = 'Asignado a';
-$mod_strings['LBL_ASSIGNED_TO'] = 'Asignado a';
-$mod_strings['LBL_LIST_ASSIGNED_TO_NAME'] = 'Asignado a';
-$mod_strings['LBL_LIST_ASSIGNED_USER'] = 'Asignado a';
-$mod_strings['LBL_CREATED'] = 'Creado por';
-$mod_strings['LBL_CREATED_USER'] = 'Creado por';
-$mod_strings['LBL_CREATED_ID'] = 'Creado por (ID)';
-$mod_strings['LBL_MODIFIED'] = 'Modificado por';
-$mod_strings['LBL_MODIFIED_NAME'] = 'Modificado por';
-$mod_strings['LBL_MODIFIED_USER'] = 'Modificado por';
-$mod_strings['LBL_MODIFIED_ID'] = 'Modificado por (ID)';
-$mod_strings['LBL_STIC_PROSPECT_LISTS_SUBPANEL_TITLE'] = 'Listas de Público Objetivo';
-$mod_strings['LBL_INC_REFERENCE_GROUP'] = 'Grupo de referencia (Incorpora)';
-$mod_strings['LBL_INC_REFERENCE_ENTITY'] = 'Entidad de referencia (Incorpora)';
-$mod_strings['LBL_INC_REFERENCE_OFFICER'] = 'Técnico de referencia (Incorpora)';
-$mod_strings['LBL_INC_INCORPORA_USER'] = 'Identificador (Incorpora)';
-$mod_strings['LBL_INCORPORA_CONNECTION_PARAMS'] = 'Parámetros de conexión con Incorpora';
-$mod_strings['LBL_STIC_WORK_CALENDAR'] = 'Calendario laboral';
-$mod_strings['LBL_STIC_WORK_CALENDAR_INFO'] = 'Indica si la funcionalidad de Calendario laboral está activada para el usuario.';
-$mod_strings['LBL_STIC_CLOCK'] = 'Registro horario';
-$mod_strings['LBL_STIC_CLOCK_INFO'] = 'Indica si la funcionalidad de Registro horario está activada para el usuario.';
-$mod_strings['LBL_SDA_ALLOWED'] = 'Acceso a SinergiaDA';
-$mod_strings['LBL_SDA_ALLOWED_INFO'] = 'Indica si el usuario dispone de acceso a Sinergia Data Analytics';
+$mod_strings['LBL_ASSIGNED_TO_ID'] = '';
+$mod_strings['LBL_ASSIGNED_TO_NAME'] = '';
+$mod_strings['LBL_ASSIGNED_TO'] = '';
+$mod_strings['LBL_LIST_ASSIGNED_TO_NAME'] = '';
+$mod_strings['LBL_LIST_ASSIGNED_USER'] = '';
+$mod_strings['LBL_CREATED'] = '';
+$mod_strings['LBL_CREATED_USER'] = '';
+$mod_strings['LBL_CREATED_ID'] = '';
+$mod_strings['LBL_MODIFIED'] = '';
+$mod_strings['LBL_MODIFIED_NAME'] = '';
+$mod_strings['LBL_MODIFIED_USER'] = '';
+$mod_strings['LBL_MODIFIED_ID'] = '';
+$mod_strings['LBL_STIC_PROSPECT_LISTS_SUBPANEL_TITLE'] = '';
+$mod_strings['LBL_INC_REFERENCE_GROUP'] = '';
+$mod_strings['LBL_INC_REFERENCE_ENTITY'] = '';
+$mod_strings['LBL_INC_REFERENCE_OFFICER'] = '';
+$mod_strings['LBL_INC_INCORPORA_USER'] = '';
+$mod_strings['LBL_INCORPORA_CONNECTION_PARAMS'] = '';
+$mod_strings['LBL_STIC_WORK_CALENDAR'] = '';
+$mod_strings['LBL_STIC_WORK_CALENDAR_INFO'] = '';
+$mod_strings['LBL_STIC_CLOCK'] = '';
+$mod_strings['LBL_STIC_CLOCK_INFO'] = '';
+$mod_strings['LBL_SDA_ALLOWED'] = '';
+$mod_strings['LBL_SDA_ALLOWED_INFO'] = '';
 
 // Virtual Field Kreporter
-$mod_strings['LBL_KREPORTER_EMAILS_LIST'] = 'Lista de direcciones de correo';
+$mod_strings['LBL_KREPORTER_EMAILS_LIST'] = '';
 
 // Asistente de creación de registros de Calendario laboral
-$mod_strings['LBL_PERIODIC_WORK_CALENDAR_BUTTON'] = 'Generar Calendario laboral';
+$mod_strings['LBL_PERIODIC_WORK_CALENDAR_BUTTON'] = '';
 
 // Autenticación OAuth
-$mod_strings['LBL_OAUTH_AUTH_LOGIN_CONTAINER'] = '<h3>Inicio de sesión alternativo</h3>';
-$mod_strings['LBL_OAUTH_AUTH_ERR_INVALID_EMAIL_1'] = 'La dirección de correo electrónico ';
-$mod_strings['LBL_OAUTH_AUTH_ERR_INVALID_EMAIL_2'] = ' no pertenece a ningún usuario de SinergiaCRM. Si considera que se trata de un error, contacte con un administrador.';
+$mod_strings['LBL_OAUTH_AUTH_LOGIN_CONTAINER'] = '';
+$mod_strings['LBL_OAUTH_AUTH_ERR_INVALID_EMAIL_1'] = '';
+$mod_strings['LBL_OAUTH_AUTH_ERR_INVALID_EMAIL_2'] = '';
 
 // Autenticación OAuth - Google
-$mod_strings['LBL_OAUTH_AUTH_GOOGLE_AUTHENTICATION_TEXT'] = 'Iniciar sesión con Google';
-$mod_strings['LBL_OAUTH_AUTH_GOOGLE_TITLE'] = 'Google';
-$mod_strings['LBL_OAUTH_AUTH_GOOGLE_ENABLE'] = 'Habilitar autenticación con Google';
-$mod_strings['LBL_OAUTH_AUTH_GOOGLE_ENABLE_HELP'] = "Esta opción habilita la autenticación con Google. <a href='https://developers.google.com/identity/protocols/oauth2' target='_blank'>Más información</a>.";
-$mod_strings['LBL_OAUTH_AUTH_GOOGLE_CLIENT_ID'] = 'ID de cliente';
-$mod_strings['LBL_OAUTH_AUTH_GOOGLE_CLIENT_ID_HELP'] = "ID de la aplicación registrada en Google API Console. <a href='https://developers.google.com/identity/protocols/oauth2' target='_blank'>Más información</a>.";
-$mod_strings['LBL_OAUTH_AUTH_GOOGLE_CLIENT_SECRET'] = 'Secreto de cliente';
-$mod_strings['LBL_OAUTH_AUTH_GOOGLE_CLIENT_SECRET_HELP'] = "Secreto de la aplicación registrada en Google API Console. <a href='https://developers.google.com/identity/protocols/oauth2' target='_blank'>Más información</a>.";
+$mod_strings['LBL_OAUTH_AUTH_GOOGLE_AUTHENTICATION_TEXT'] = '';
+$mod_strings['LBL_OAUTH_AUTH_GOOGLE_TITLE'] = '';
+$mod_strings['LBL_OAUTH_AUTH_GOOGLE_ENABLE'] = '';
+$mod_strings['LBL_OAUTH_AUTH_GOOGLE_ENABLE_HELP'] = "";
+$mod_strings['LBL_OAUTH_AUTH_GOOGLE_CLIENT_ID'] = '';
+$mod_strings['LBL_OAUTH_AUTH_GOOGLE_CLIENT_ID_HELP'] = "";
+$mod_strings['LBL_OAUTH_AUTH_GOOGLE_CLIENT_SECRET'] = '';
+$mod_strings['LBL_OAUTH_AUTH_GOOGLE_CLIENT_SECRET_HELP'] = "";
 
 // Autenticación OAuth - Microsoft
-$mod_strings['LBL_OAUTH_AUTH_MICROSOFT_AUTHENTICATION_TEXT'] = 'Iniciar sesión con Microsoft';
-$mod_strings['LBL_OAUTH_AUTH_MICROSOFT_TITLE'] = 'Microsoft';
-$mod_strings['LBL_OAUTH_AUTH_MICROSOFT_ENABLE'] = 'Habilitar autenticación con Microsoft';
-$mod_strings['LBL_OAUTH_AUTH_MICROSOFT_ENABLE_HELP'] = "Esta opción habilita la autenticación con Microsoft. <a href='https://learn.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-auth-code-flow' target='_blank'>Más información</a>.";
-$mod_strings['LBL_OAUTH_AUTH_MICROSOFT_CLIENT_ID'] = 'ID de cliente';
-$mod_strings['LBL_OAUTH_AUTH_MICROSOFT_CLIENT_ID_HELP'] = "ID de la aplicación registrada en Microsoft Azure Portal. <a href='https://learn.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-auth-code-flow' target='_blank'>Más información</a>.";
-$mod_strings['LBL_OAUTH_AUTH_MICROSOFT_TENANT_ID'] = 'ID de tenant';
-$mod_strings['LBL_OAUTH_AUTH_MICROSOFT_TENANT_ID_HELP'] = "ID de tenant de la aplicación registrada en Microsoft Azure Portal. <a href='https://learn.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-auth-code-flow' target='_blank'>Más información</a>.";
-$mod_strings['LBL_OAUTH_AUTH_MICROSOFT_REDIRECT_URI'] = 'URI de redirección';
-$mod_strings['LBL_OAUTH_AUTH_MICROSOFT_REDIRECT_URI_HELP'] = "URI de redirección de la aplicación registrada en Microsoft Azure Portal. <a href='https://learn.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-auth-code-flow' target='_blank'>Más información</a>.";
-$mod_strings['LBL_OAUTH_AUTH_MICROSOFT_SCOPES'] = 'Ámbitos';
-$mod_strings['LBL_OAUTH_AUTH_MICROSOFT_SCOPES_HELP'] = "Ámbitos de la aplicación registrada en Microsoft Azure Portal. <a href='https://learn.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent' target='_blank'>Más información</a>.";
+$mod_strings['LBL_OAUTH_AUTH_MICROSOFT_AUTHENTICATION_TEXT'] = '';
+$mod_strings['LBL_OAUTH_AUTH_MICROSOFT_TITLE'] = '';
+$mod_strings['LBL_OAUTH_AUTH_MICROSOFT_ENABLE'] = '';
+$mod_strings['LBL_OAUTH_AUTH_MICROSOFT_ENABLE_HELP'] = "";
+$mod_strings['LBL_OAUTH_AUTH_MICROSOFT_CLIENT_ID'] = '';
+$mod_strings['LBL_OAUTH_AUTH_MICROSOFT_CLIENT_ID_HELP'] = "";
+$mod_strings['LBL_OAUTH_AUTH_MICROSOFT_TENANT_ID'] = '';
+$mod_strings['LBL_OAUTH_AUTH_MICROSOFT_TENANT_ID_HELP'] = "";
+$mod_strings['LBL_OAUTH_AUTH_MICROSOFT_REDIRECT_URI'] = '';
+$mod_strings['LBL_OAUTH_AUTH_MICROSOFT_REDIRECT_URI_HELP'] = "";
+$mod_strings['LBL_OAUTH_AUTH_MICROSOFT_SCOPES'] = '';
+$mod_strings['LBL_OAUTH_AUTH_MICROSOFT_SCOPES_HELP'] = "";
 
 // Emulación de usuarios - Usuario emulador
-$mod_strings['LBL_IMPERSONATE_USER_BUTTON'] = 'Emular usuario';
-$mod_strings['LBL_IMPERSONATE_MESSAGE_TITLE'] = 'Emulación de usuario';
-$mod_strings['LBL_IMPERSONATE_MESSAGE_DESCRIPTION'] = '¡Atención! Está emulando al usuario: ';
-$mod_strings['LBL_IMPERSONATE_MESSAGE_STOP_DESCRIPTION'] = '<br>Para volver a su propio usuario haga clic <a href="index.php?module=Users&action=stopImpersonation" style="color: #fff; text-decoration: underline;">aquí</a>.';
-$mod_strings['LBL_IMPERSONATE_ORIGINAL_USER'] = 'Usuario original';
-$mod_strings['LBL_IMPERSONATE_TARGET_USER'] = 'Usuario emulado';
-$mod_strings['LBL_IMPERSONATE_STOP_BUTTON'] = 'Finalizar emulación';
+$mod_strings['LBL_IMPERSONATE_USER_BUTTON'] = '';
+$mod_strings['LBL_IMPERSONATE_MESSAGE_TITLE'] = '';
+$mod_strings['LBL_IMPERSONATE_MESSAGE_DESCRIPTION'] = '';
+$mod_strings['LBL_IMPERSONATE_MESSAGE_STOP_DESCRIPTION'] = '';
+$mod_strings['LBL_IMPERSONATE_ORIGINAL_USER'] = '';
+$mod_strings['LBL_IMPERSONATE_TARGET_USER'] = '';
+$mod_strings['LBL_IMPERSONATE_STOP_BUTTON'] = '';
 
 // Emulación de usuarios - Usuario emulado
-$mod_strings['LBL_IMPERSONATION_ALERT_HEADER'] = 'Emulación de usuario';
-$mod_strings['LBL_IMPERSONATION_START_ALERT_DESCRIPTION_TITLE'] = 'Un administrador está emulando su usuario.';
-$mod_strings['LBL_IMPERSONATION_STOP_ALERT_DESCRIPTION_TITLE'] = 'Un administrador ha finalizado la emulación de su usuario.';
-$mod_strings['LBL_IMPERSONATION_ALERT_USER'] = 'Usuario emulador: ';
+$mod_strings['LBL_IMPERSONATION_ALERT_HEADER'] = '';
+$mod_strings['LBL_IMPERSONATION_START_ALERT_DESCRIPTION_TITLE'] = '';
+$mod_strings['LBL_IMPERSONATION_STOP_ALERT_DESCRIPTION_TITLE'] = '';
+$mod_strings['LBL_IMPERSONATION_ALERT_USER'] = '';
 
 // Emulación de usuarios - Monitorización
-$mod_strings['LBL_IMPERSONATION_MONITORING_ITEM_SUMMARY_1'] = 'Emulación del usuario ';
-$mod_strings['LBL_IMPERSONATION_MONITORING_ITEM_SUMMARY_2'] = ' con ID ';
+$mod_strings['LBL_IMPERSONATION_MONITORING_ITEM_SUMMARY_1'] = '';
+$mod_strings['LBL_IMPERSONATION_MONITORING_ITEM_SUMMARY_2'] = '';
