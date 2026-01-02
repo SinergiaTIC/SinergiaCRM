0267c1a6801a60e346d43c2ce10da25f29aab5da
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-02 13:43:15 +0100

    New translations es_es.lang.php (Galician)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/TimeTracker_HoursWorkedInPreviousWeek/language/gl_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/TimeTracker_HoursWorkedInPreviousWeek/language/gl_ES.lang.php
@@ -21,9 +21,9 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-    'NAME' => 'Rexistro horario - Revisión das horas traballadas durante a semana anterior.', // Esta etiqueta é obrigatoria. Indica o nome da función
-    'HOURS_NOT_MATCH' => 'As horas rexistradas durante a semana pasada non coinciden coas horas laborais teóricas indicadas no calendario laboral do usuario: ',
-    'EMAIL_SUBJECT' => 'Revisión das horas traballadas durante a semana anterior',
-    'STIC_TIME_TRACKER_LIST_VIEW' => 'Rexistro horario do usuario',
-    'STIC_WORK_CALENDAR_LIST_VIEW' => 'Calendario laboral do usuario',    
+    'NAME' => '', // Esta etiqueta es obligatoria. Indica el nombre de la función
+    'HOURS_NOT_MATCH' => '',
+    'EMAIL_SUBJECT' => '',
+    'STIC_TIME_TRACKER_LIST_VIEW' => '',
+    'STIC_WORK_CALENDAR_LIST_VIEW' => '',    
 );
