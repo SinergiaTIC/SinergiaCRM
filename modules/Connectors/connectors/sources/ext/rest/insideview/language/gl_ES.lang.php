63cb4a477ef964af842c8f36b294bb708ab5c59a
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-02 13:31:05 +0100

    New translations es_es.lang.php (Galician)

--- modules/Connectors/connectors/sources/ext/rest/insideview/language/gl_ES.lang.php
+++ modules/Connectors/connectors/sources/ext/rest/insideview/language/gl_ES.lang.php
@@ -43,14 +43,14 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $connector_strings = array(
-    'LBL_GET_STARTED' => 'Comezar!',
-    'LBL_TOS1' => 'Terminos of Use',
-    'LBL_TOS3' => 'Política de Privacidade',
-    'iv_description0' => 'Ao facer click en "Comezar!" vostede está de acordo con InsideView\'s',
-    'LBL_OPP' => 'Encontrar Oportunidades',
-    'LBL_OPP_SUB' => 'para alcanzar aos clientes',
-    'LBL_REFERRAL' => 'Obter Referencias',
-    'LBL_REFERRAL_SUB' => 'para decisores clave',
-    'LBL_ENGAGE' => 'Captar Clientes',
-    'LBL_ENGAGE_SUB' => 'con iniciadores de conversacións',
+    'LBL_GET_STARTED' => '',
+    'LBL_TOS1' => '',
+    'LBL_TOS3' => '',
+    'iv_description0' => '',
+    'LBL_OPP' => '',
+    'LBL_OPP_SUB' => '',
+    'LBL_REFERRAL' => '',
+    'LBL_REFERRAL_SUB' => '',
+    'LBL_ENGAGE' => '',
+    'LBL_ENGAGE_SUB' => '',
 );
