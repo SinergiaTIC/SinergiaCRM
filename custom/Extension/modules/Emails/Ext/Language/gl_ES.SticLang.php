cd12895648a4e7d3df6fe52f505c548d88812bbd
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-02 14:09:15 +0100

    New translations es_es.sticlang.php (Galician)

--- custom/Extension/modules/Emails/Ext/Language/gl_ES.SticLang.php
+++ custom/Extension/modules/Emails/Ext/Language/gl_ES.SticLang.php
@@ -20,7 +20,7 @@
  *
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
-$mod_strings['LBL_STIC_PAYMENTS_ACTIVITIES_EMAILS_FROM_STIC_PAYMENTS_TITLE'] = '';
-$mod_strings['LBL_LEADS_SUBPANEL_TITLE'] = '';
-$mod_strings['LBL_EMAIL_INFORMATION'] = '';
-$mod_strings['LBL_STIC_PANEL_RECORD_DETAILS'] = '';
+$mod_strings['LBL_STIC_PAYMENTS_ACTIVITIES_EMAILS_FROM_STIC_PAYMENTS_TITLE'] = 'Actividades:Pagos';
+$mod_strings['LBL_LEADS_SUBPANEL_TITLE'] = 'Interesados';
+$mod_strings['LBL_EMAIL_INFORMATION'] = 'Datos generales';
+$mod_strings['LBL_STIC_PANEL_RECORD_DETAILS'] = 'Detalles del registro';
