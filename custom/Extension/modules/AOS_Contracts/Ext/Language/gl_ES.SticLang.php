e9bb59fd0ca1f02675d9a19cbe03873048651fe0
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-02 14:07:52 +0100

    New translations es_es.sticlang.php (Galician)

--- custom/Extension/modules/AOS_Contracts/Ext/Language/gl_ES.SticLang.php
+++ custom/Extension/modules/AOS_Contracts/Ext/Language/gl_ES.SticLang.php
@@ -20,10 +20,10 @@
  *
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
-$mod_strings['LBL_DEFAULT_PANEL'] = '';
-$mod_strings['LBL_PANEL_RECORD_DETAILS'] = '';
-$mod_strings['LBL_CONTRACT_ACCOUNT'] = '';
-$mod_strings['LBL_OPPORTUNITY'] = '';
-$mod_strings['LBL_CONTACT'] = '';
-$mod_strings['LBL_END_DATE_ERROR'] = '';
-$mod_strings['LBL_START_DATE_ERROR'] = '';
+$mod_strings['LBL_DEFAULT_PANEL'] = 'Datos generales';
+$mod_strings['LBL_PANEL_RECORD_DETAILS'] = 'Detalles del registro';
+$mod_strings['LBL_CONTRACT_ACCOUNT'] = 'Organización';
+$mod_strings['LBL_OPPORTUNITY'] = 'Subvención';
+$mod_strings['LBL_CONTACT'] = 'Persona';
+$mod_strings['LBL_END_DATE_ERROR'] = 'La fecha de finalización debe ser igual o posterior a la fecha de inicio.';
+$mod_strings['LBL_START_DATE_ERROR'] = 'La fecha de inicio debe ser igual o anterior a la fecha de finalización.';
