d4e2e064c71f236a7e425ad13ed46c2629ba78ca
Ainara Ramos <ainara@sinergiacrm.org>
2026-01-13 16:10:54 +0100

    Feature - Economía - Gestión financiera de personas asistidas (#774)

---
+++ modules/stic_Transactions/metadata/listviewdefs.php
@@ -0,0 +1,174 @@
+<?php
+/**
+ * This file is part of SinergiaCRM.
+ * SinergiaCRM is a work developed by SinergiaTIC Association, based on SuiteCRM.
+ * Copyright (C) 2013 - 2023 SinergiaTIC Association
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$module_name = 'stic_Transactions';
+$OBJECT_NAME = 'STIC_TRANSACTIONS';
+$listViewDefs [$module_name] = 
+array (
+  'DOCUMENT_NAME' => 
+  array (
+    'width' => '40%',
+    'label' => 'LBL_NAME',
+    'link' => true,
+    'default' => true,
+  ),
+  'TRANSACTION_DATE' => 
+  array (
+    'type' => 'date',
+    'label' => 'LBL_TRANSACTION_DATE',
+    'width' => '10%',
+    'default' => true,
+  ),
+  'UPLOADFILE' => 
+  array (
+    'type' => 'file',
+    'label' => 'LBL_FILE_UPLOAD',
+    'width' => '10%',
+    'default' => true,
+  ),
+  'STIC_TRANSACTIONS_STIC_FINANCIAL_PRODUCTS_NAME' => 
+  array (
+    'type' => 'relate',
+    'link' => true,
+    'label' => 'LBL_STIC_TRANSACTIONS_STIC_FINANCIAL_PRODUCTS_FROM_STIC_FINANCIAL_PRODUCTS_TITLE',
+    'id' => 'STIC_TRANS4A5BRODUCTS_IDA',
+    'width' => '10%',
+    'default' => true,
+  ),
+  'STATUS' => 
+  array (
+    'type' => 'enum',
+    'studio' => 'visible',
+    'label' => 'LBL_STATUS',
+    'width' => '10%',
+    'default' => true,
+  ),
+  'TYPE' => 
+  array (
+    'type' => 'enum',
+    'studio' => 'visible',
+    'label' => 'LBL_TYPE',
+    'width' => '10%',
+    'default' => true,
+  ),
+  'CATEGORY' => 
+  array (
+    'type' => 'dynamicenum',
+    'studio' => 'visible',
+    'label' => 'LBL_CATEGORY',
+    'width' => '10%',
+    'default' => true,
+  ),
+  'SUBCATEGORY' => 
+  array (
+    'type' => 'dynamicenum',
+    'studio' => 'visible',
+    'label' => 'LBL_SUBCATEGORY',
+    'width' => '10%',
+    'default' => true,
+  ),
+  'AMOUNT' => 
+  array (
+    'type' => 'decimal',
+    'label' => 'LBL_AMOUNT',
+    'width' => '10%',
+    'default' => true,
+  ),
+  'PAYMENT_METHOD' => 
+  array (
+    'type' => 'enum',
+    'studio' => 'visible',
+    'label' => 'LBL_PAYMENT_METHOD',
+    'width' => '10%',
+    'default' => false,
+  ),
+  'DESTINATION_ACCOUNT' => 
+  array (
+    'type' => 'varchar',
+    'label' => 'LBL_DESTINATION_ACCOUNT',
+    'width' => '10%',
+    'default' => false,
+  ),
+  'ACCOUNTING_ACCOUNT' => 
+  array (
+    'type' => 'varchar',
+    'label' => 'LBL_ACCOUNTING_ACCOUNT',
+    'width' => '10%',
+    'default' => false,
+  ),
+  'DESCRIPTION' => 
+  array (
+    'type' => 'text',
+    'label' => 'LBL_DESCRIPTION',
+    'sortable' => false,
+    'width' => '10%',
+    'default' => false,
+  ),
+  'ASSIGNED_USER_NAME' => 
+  array (
+    'link' => true,
+    'type' => 'relate',
+    'label' => 'LBL_ASSIGNED_TO_NAME',
+    'id' => 'ASSIGNED_USER_ID',
+    'width' => '10%',
+    'default' => false,
+  ),
+  'DATE_ENTERED' => 
+  array (
+    'type' => 'datetime',
+    'label' => 'LBL_DATE_ENTERED',
+    'width' => '10%',
+    'default' => false,
+  ),
+  'CREATED_BY_NAME' => 
+  array (
+    'width' => '2%',
+    'label' => 'LBL_LIST_LAST_REV_CREATOR',
+    'default' => false,
+    'sortable' => false,
+  ),
+  'DATE_MODIFIED' => 
+  array (
+    'type' => 'datetime',
+    'label' => 'LBL_DATE_MODIFIED',
+    'width' => '10%',
+    'default' => false,
+  ),
+  'MODIFIED_BY_NAME' => 
+  array (
+    'width' => '10%',
+    'label' => 'LBL_MODIFIED_USER',
+    'module' => 'Users',
+    'id' => 'USERS_ID',
+    'default' => false,
+    'sortable' => false,
+    'related_fields' => 
+    array (
+      0 => 'modified_user_id',
+    ),
+  ),
+);
---
+++ modules/stic_Transactions/vardefs.php
@@ -0,0 +1,310 @@
+<?php
+/**
+ * This file is part of SinergiaCRM.
+ * SinergiaCRM is a work developed by SinergiaTIC Association, based on SuiteCRM.
+ * Copyright (C) 2013 - 2023 SinergiaTIC Association
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
+ */
+
+$dictionary['stic_Transactions'] = array(
+    'table' => 'stic_transactions',
+    'audited' => true,
+    'inline_edit' => true,
+    'fields' => array (
+  'transaction_date' => 
+  array (
+    'required' => true,
+    'name' => 'transaction_date',
+    'vname' => 'LBL_TRANSACTION_DATE',
+    'type' => 'date',
+    'massupdate' => 0,
+    'no_default' => false,
+    'comments' => '',
+    'help' => '',
+    'importable' => 'true',
+    'duplicate_merge' => 'disabled',
+    'duplicate_merge_dom_value' => '0',
+    'audited' => false,
+    'inline_edit' => true,
+    'reportable' => true,
+    'unified_search' => false,
+    'merge_filter' => 'disabled',
+    'size' => '20',
+    'options' => 'date_range_search_dom',
+    'enable_range_search' => true,
+  ),
+  'type' => 
+  array (
+    'required' => true,
+    'name' => 'type',
+    'vname' => 'LBL_TYPE',
+    'type' => 'enum',
+    'massupdate' => 0,
+    'no_default' => false,
+    'comments' => '',
+    'help' => '',
+    'importable' => 'true',
+    'duplicate_merge' => 'disabled',
+    'duplicate_merge_dom_value' => '0',
+    'audited' => true,
+    'inline_edit' => true,
+    'reportable' => true,
+    'unified_search' => false,
+    'merge_filter' => 'disabled',
+    'len' => 100,
+    'size' => '20',
+    'options' => 'stic_payments_transaction_types_list',
+    'studio' => 'visible',
+    'dependency' => false,
+  ),
+  'amount' => 
+  array (
+    'required' => true,
+    'name' => 'amount',
+    'vname' => 'LBL_AMOUNT',
+    'type' => 'decimal',
+    'massupdate' => 0,
+    'no_default' => false,
+    'comments' => '',
+    'help' => '',
+    'importable' => 'true',
+    'duplicate_merge' => 'disabled',
+    'duplicate_merge_dom_value' => '0',
+    'audited' => true,
+    'inline_edit' => true,
+    'reportable' => true,
+    'unified_search' => false,
+    'merge_filter' => 'disabled',
+    'len' => '19',
+    'size' => '20',
+    'enable_range_search' => false,
+    'precision' => '2',
+  ),
+  'status' => 
+  array (
+    'name' => 'status',
+    'vname' => 'LBL_STATUS',
+    'type' => 'enum',
+    'required' => true,
+    'massupdate' => 0,
+    'no_default' => false,
+    'comments' => '',
+    'help' => '',
+    'importable' => 'true',
+    'duplicate_merge' => 'disabled',
+    'duplicate_merge_dom_value' => '0',
+    'audited' => true,
+    'inline_edit' => true,
+    'reportable' => true,
+    'unified_search' => false,
+    'merge_filter' => 'disabled',
+    'len' => 100,
+    'size' => '20',
+    'options' => 'stic_transactions_status_list',
+    'studio' => 'visible',
+    'dependency' => false,
+  ),
+  'payment_method' => 
+  array (
+    'required' => false,
+    'name' => 'payment_method',
+    'vname' => 'LBL_PAYMENT_METHOD',
+    'type' => 'enum',
+    'massupdate' => 0,
+    'no_default' => false,
+    'comments' => '',
+    'help' => '',
+    'importable' => 'true',
+    'duplicate_merge' => 'disabled',
+    'duplicate_merge_dom_value' => '0',
+    'audited' => false,
+    'inline_edit' => true,
+    'reportable' => true,
+    'unified_search' => false,
+    'merge_filter' => 'disabled',
+    'len' => 100,
+    'size' => '20',
+    'options' => 'stic_payments_methods_list',
+    'studio' => 'visible',
+    'dependency' => false,
+  ),
+  'destination_account' => 
+  array (
+    'required' => false,
+    'name' => 'destination_account',
+    'vname' => 'LBL_DESTINATION_ACCOUNT',
+    'type' => 'varchar',
+    'massupdate' => 0,
+    'no_default' => false,
+    'comments' => '',
+    'help' => '',
+    'importable' => 'true',
+    'duplicate_merge' => 'disabled',
+    'duplicate_merge_dom_value' => '0',
+    'audited' => false,
+    'inline_edit' => true,
+    'reportable' => true,
+    'unified_search' => false,
+    'merge_filter' => 'disabled',
+    'len' => '255',
+    'size' => '20',
+    'popupHelp' => 'LBL_DESTINATION_ACCOUNT_INFO',
+  ),
+  'accounting_account' => 
+  array (
+    'required' => false,
+    'name' => 'accounting_account',
+    'vname' => 'LBL_ACCOUNTING_ACCOUNT',
+    'type' => 'varchar',
+    'massupdate' => 0,
+    'no_default' => false,
+    'comments' => '',
+    'help' => '',
+    'importable' => 'true',
+    'duplicate_merge' => 'disabled',
+    'duplicate_merge_dom_value' => '0',
+    'audited' => false,
+    'inline_edit' => true,
+    'reportable' => true,
+    'unified_search' => false,
+    'merge_filter' => 'disabled',
+    'len' => '255',
+    'size' => '20',
+    'popupHelp' => 'LBL_ACCOUNTING_ACCOUNT_INFO',
+  ),
+  'category' => 
+  array (
+    'required' => true,
+    'name' => 'category',
+    'vname' => 'LBL_CATEGORY',
+    'type' => 'dynamicenum',
+    'massupdate' => 0,
+    'no_default' => false,
+    'comments' => '',
+    'help' => '',
+    'importable' => 'true',
+    'duplicate_merge' => 'disabled',
+    'duplicate_merge_dom_value' => '0',
+    'audited' => true,
+    'inline_edit' => true,
+    'reportable' => true,
+    'unified_search' => false,
+    'merge_filter' => 'disabled',
+    'len' => 100,
+    'size' => '20',
+    'options' => 'stic_transactions_categories_list',
+    'studio' => 'visible',
+    'dbType' => 'enum',
+    'parentenum' => 'type',
+    'popupHelp' => 'LBL_CATEGORY_INFO',
+  ),
+  'subcategory' => 
+  array (
+    'required' => false,
+    'name' => 'subcategory',
+    'vname' => 'LBL_SUBCATEGORY',
+    'type' => 'dynamicenum',
+    'massupdate' => 0,
+    'no_default' => false,
+    'comments' => '',
+    'help' => '',
+    'importable' => 'true',
+    'duplicate_merge' => 'disabled',
+    'duplicate_merge_dom_value' => '0',
+    'audited' => false,
+    'inline_edit' => true,
+    'reportable' => true,
+    'unified_search' => false,
+    'merge_filter' => 'disabled',
+    'len' => 100,
+    'size' => '20',
+    'options' => 'stic_transactions_subcategories_list',
+    'studio' => 'visible',
+    'dbType' => 'enum',
+    'parentenum' => 'category',
+    'popupHelp' => 'LBL_CATEGORY_INFO',
+  ),
+  'transaction_hash' =>
+  array (
+    'name' => 'transaction_hash',
+    'vname' => 'LBL_TRANSACTION_HASH',
+    'type' => 'varchar',
+    'len' => 64,
+    'audited' => false,
+    'duplicate_merge' => 'disabled',
+    'reportable' => true,
+    'unified_search' => false,
+    'massupdate' => false,
+    'importable' => 'true',
+    'studio' => true,
+    'required' => false,
+    'popupHelp' => 'LBL_TRANSACTION_HASH_INFO',
+  ),
+
+  // Relationships
+  'stic_transactions_stic_financial_products' =>
+  array (
+    'name' => 'stic_transactions_stic_financial_products',
+    'type' => 'link',
+    'relationship' => 'stic_transactions_stic_financial_products',
+    'source' => 'non-db',
+    'module' => 'stic_Financial_Products',
+    'bean_name' => false,
+    'vname' => 'LBL_STIC_TRANSACTIONS_STIC_FINANCIAL_PRODUCTS_FROM_STIC_FINANCIAL_PRODUCTS_TITLE',
+    'id_name' => 'stic_trans4a5broducts_ida',
+  ),
+  'stic_transactions_stic_financial_products_name' =>
+  array (
+    'name' => 'stic_transactions_stic_financial_products_name',
+    'type' => 'relate',
+    'source' => 'non-db',
+    'vname' => 'LBL_STIC_TRANSACTIONS_STIC_FINANCIAL_PRODUCTS_FROM_STIC_FINANCIAL_PRODUCTS_TITLE',
+    'save' => true,
+    'id_name' => 'stic_trans4a5broducts_ida',
+    'link' => 'stic_transactions_stic_financial_products',
+    'table' => 'stic_financial_products',
+    'module' => 'stic_Financial_Products',
+    'rname' => 'name',
+    'required' => true,
+  ),
+  'stic_trans4a5broducts_ida' =>
+  array (
+    'name' => 'stic_trans4a5broducts_ida',
+    'type' => 'link',
+    'relationship' => 'stic_transactions_stic_financial_products',
+    'source' => 'non-db',
+    'reportable' => false,
+    'side' => 'right',
+    'vname' => 'LBL_STIC_TRANSACTIONS_STIC_FINANCIAL_PRODUCTS_FROM_STIC_TRANSACTIONS_TITLE',
+  ),
+),
+    'relationships' => array (
+),
+    'optimistic_locking' => true,
+    'unified_search' => true,
+);
+if (!class_exists('VardefManager')) {
+        require_once('include/SugarObjects/VardefManager.php');
+}
+VardefManager::createVardef('stic_Transactions', 'stic_Transactions', array('basic','assignable','security_groups','file'));
+
+// Set special values for SuiteCRM base fields
+$dictionary['stic_Transactions']['fields']['document_name']['required'] = '0'; // Name is not required in this module
+$dictionary['stic_Transactions']['fields']['document_name']['importable'] = true; // Name is importable but not required in this module
+$dictionary['stic_Transactions']['fields']['document_name']['inline_edit'] = false; // Name can not edit inline in this module
+$dictionary['stic_Transactions']['fields']['description']['rows'] = '2'; // Make textarea fields shorter
