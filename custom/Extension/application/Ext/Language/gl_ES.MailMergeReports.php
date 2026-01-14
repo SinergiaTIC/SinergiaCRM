1d7c0205dced8f266877d3bb28515a1f2d5a2088
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-14 16:59:38 +0100

    New translations es_es.mailmergereports.php (Galician)

--- custom/Extension/application/Ext/Language/gl_ES.MailMergeReports.php
+++ custom/Extension/application/Ext/Language/gl_ES.MailMergeReports.php
@@ -23,49 +23,49 @@
 
 $app_list_strings['moduleList']['DHA_PlantillasDocumentos'] = 'Mail Merge Reports';
 
-$app_strings['LBL_YES'] = 'Sí';
-$app_strings['LBL_NO'] = 'No';
-$app_strings['LBL_CURRENCY_ID'] = 'ID de Moneda';
-$app_strings['LBL_CURRENCY_NAME'] = 'Nombre de Moneda';
-$app_strings['LBL_CURRENCY_SYMBOL'] = 'Símbolo de Moneda';
-$app_strings['LBL_CURRENCY_CODE'] = 'Código de Moneda';
-$app_strings['LBL_CURRENCY_NAME_BASE'] = 'Nombre de Moneda (MB)';
-$app_strings['LBL_CURRENCY_SYMBOL_BASE'] = 'Símbolo de Moneda (MB)';
-$app_strings['LBL_CURRENCY_CODE_BASE'] = 'Código de Moneda (MB)';
-$app_strings['LBL_CURRENT_DATE'] = 'Fecha actual';
-$app_strings['LBL_CURRENT_WEEKDAY'] = 'Día de la semana actual';
-$app_strings['LBL_DIRECCION_COMPLETA'] = 'Dirección completa';
-$app_strings['LBL_DIRECCION_ALTERNATIVA_COMPLETA'] = 'Dirección alternativa completa';
+$app_strings['LBL_YES'] = 'Si';
+$app_strings['LBL_NO'] = 'Non';
+$app_strings['LBL_CURRENCY_ID'] = 'ID de Moeda';
+$app_strings['LBL_CURRENCY_NAME'] = 'Nome de Moeda';
+$app_strings['LBL_CURRENCY_SYMBOL'] = 'Símbolo de Moeda';
+$app_strings['LBL_CURRENCY_CODE'] = 'Código de Moeda';
+$app_strings['LBL_CURRENCY_NAME_BASE'] = 'Nome de Moeda (MB)';
+$app_strings['LBL_CURRENCY_SYMBOL_BASE'] = 'Símbolo de Moeda (MB)';
+$app_strings['LBL_CURRENCY_CODE_BASE'] = 'Código de Moeda (MB)';
+$app_strings['LBL_CURRENT_DATE'] = 'Data actual';
+$app_strings['LBL_CURRENT_WEEKDAY'] = 'Día da semana actual';
+$app_strings['LBL_DIRECCION_COMPLETA'] = 'Enderezo completo';
+$app_strings['LBL_DIRECCION_ALTERNATIVA_COMPLETA'] = 'Enderezo alternativo completo';
 $app_strings['LBL_EN_LETRAS'] = 'en letras';
-$app_strings['LBL_GENERAR_DOCUMENTO'] = 'Generar Documento';
-$app_strings['LBL_GENERAR_DOCUMENTO_PDF'] = 'Generar Documento en PDF';
-$app_strings['LBL_ADJUNTAR_DOCUMENTO_GENERADO_A_EMAIL'] = 'Adjuntar a correo';
-$app_strings['LBL_ADJUNTAR_DOCUMENTO_GENERADO_A_NOTA'] = 'Adjuntar a nota';
-$app_strings['LBL_NO_HAY_PLANTILLAS_DISPONIBLES_PARA_GENERAR_DOCUMENTO'] = 'No hay plantillas disponibles para la generación de documentos';
+$app_strings['LBL_GENERAR_DOCUMENTO'] = 'Xerar Documento';
+$app_strings['LBL_GENERAR_DOCUMENTO_PDF'] = 'Xerar Documento en PDF';
+$app_strings['LBL_ADJUNTAR_DOCUMENTO_GENERADO_A_EMAIL'] = 'Adxuntar a correo';
+$app_strings['LBL_ADJUNTAR_DOCUMENTO_GENERADO_A_NOTA'] = 'Adxuntar a nota';
+$app_strings['LBL_NO_HAY_PLANTILLAS_DISPONIBLES_PARA_GENERAR_DOCUMENTO'] = 'Non hai plantillas dispoñibles para a xeración de documentos';
 
 $app_list_strings['dha_boolean_list'][''] = '';
-$app_list_strings['dha_boolean_list']['1'] = 'Sí';
-$app_list_strings['dha_boolean_list']['0'] = 'No';
+$app_list_strings['dha_boolean_list']['1'] = 'Si';
+$app_list_strings['dha_boolean_list']['0'] = 'Non';
 
 $app_list_strings['dha_plantillasdocumentos_status_dom'][''] = '';
 $app_list_strings['dha_plantillasdocumentos_status_dom']['Active'] = 'Activo';
 $app_list_strings['dha_plantillasdocumentos_status_dom']['Draft'] = 'Borrador';
 $app_list_strings['dha_plantillasdocumentos_status_dom']['Under Review'] = 'En revisión';
-$app_list_strings['dha_plantillasdocumentos_status_dom']['Pending'] = 'Pendiente';
+$app_list_strings['dha_plantillasdocumentos_status_dom']['Pending'] = 'Pendente';
 $app_list_strings['dha_plantillasdocumentos_status_dom']['Expired'] = 'Expirado';
 $app_list_strings['dha_plantillasdocumentos_status_dom']['Canceled'] = 'Cancelado';
 
 $app_list_strings['dha_plantillasdocumentos_category_dom'][''] = '';
-$app_list_strings['dha_plantillasdocumentos_category_dom']['VENTAS'] = 'Ventas';
+$app_list_strings['dha_plantillasdocumentos_category_dom']['VENTAS'] = 'Vendas';
 $app_list_strings['dha_plantillasdocumentos_category_dom']['MARKETING'] = 'Marketing';
 $app_list_strings['dha_plantillasdocumentos_category_dom']['ACTIVIDADES'] = 'Actividades';
 $app_list_strings['dha_plantillasdocumentos_category_dom']['CALLCENTER'] = 'Call center';
-$app_list_strings['dha_plantillasdocumentos_category_dom']['JURIDICO'] = 'Jurídico';
+$app_list_strings['dha_plantillasdocumentos_category_dom']['JURIDICO'] = 'Xurídico';
 $app_list_strings['dha_plantillasdocumentos_category_dom']['ADMINISTRACION'] = 'Administración';
 $app_list_strings['dha_plantillasdocumentos_category_dom']['COLABORACION'] = 'Colaboración';
 $app_list_strings['dha_plantillasdocumentos_category_dom']['CONFIGURACION'] = 'Configuración';
 $app_list_strings['dha_plantillasdocumentos_category_dom']['SOPORTE'] = 'Soporte';
-$app_list_strings['dha_plantillasdocumentos_category_dom']['OTROS'] = 'Otros';
+$app_list_strings['dha_plantillasdocumentos_category_dom']['OTROS'] = 'Outros';
 
 $app_list_strings['dha_plantillasdocumentos_subcategory_dom'][''] = '';
 
@@ -77,7 +77,7 @@ $app_list_strings['dha_plantillasdocumentos_roles_dom'][''] = '';
 
 $app_list_strings['dha_plantillasdocumentos_idiomas_dom']['es'] = 'Español';
 $app_list_strings['dha_plantillasdocumentos_idiomas_dom']['ca'] = 'Catalán';
-$app_list_strings['dha_plantillasdocumentos_idiomas_dom']['es_AR'] = 'Español (Argentina)';
+$app_list_strings['dha_plantillasdocumentos_idiomas_dom']['es_AR'] = 'Español (Arxentina)';
 $app_list_strings['dha_plantillasdocumentos_idiomas_dom']['es_MX'] = 'Español (México)';
 $app_list_strings['dha_plantillasdocumentos_idiomas_dom']['en_US'] = 'Inglés (Estados Unidos)';
 $app_list_strings['dha_plantillasdocumentos_idiomas_dom']['en_GB'] = 'Inglés (Reino Unido)';
@@ -109,6 +109,6 @@ $app_list_strings['dha_plantillasdocumentos_formatos_ficheros_dom']['ODS'] = 'Op
 $app_list_strings['dha_plantillasdocumentos_formatos_ficheros_dom']['XLSM'] = 'Office Open XML - MS Excel Macros Enabled (.xlsm)';
 
 $app_list_strings['dha_plantillasdocumentos_enable_roles_dom']['ALLOW_ALL'] = 'Todo';
-$app_list_strings['dha_plantillasdocumentos_enable_roles_dom']['ALLOW_DOCX'] = 'Sólo DOCX/ODT/XLSX/ODS';
-$app_list_strings['dha_plantillasdocumentos_enable_roles_dom']['ALLOW_PDF'] = 'Sólo PDF';
+$app_list_strings['dha_plantillasdocumentos_enable_roles_dom']['ALLOW_DOCX'] = 'Só DOCX/ODT/XLSX/ODS';
+$app_list_strings['dha_plantillasdocumentos_enable_roles_dom']['ALLOW_PDF'] = 'Só PDF';
 $app_list_strings['dha_plantillasdocumentos_enable_roles_dom']['ALLOW_NONE'] = 'Nada';
