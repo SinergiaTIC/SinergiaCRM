ae772bf24d8ccda21ff19b4c51ce1dcf4ca88c34
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-14 17:41:11 +0100

    New translations es_es.sticlang.php (Galician)

--- custom/Extension/modules/AOS_Quotes/Ext/Language/gl_ES.SticLang.php
+++ custom/Extension/modules/AOS_Quotes/Ext/Language/gl_ES.SticLang.php
@@ -20,20 +20,20 @@
  *
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
-$mod_strings['LBL_DEFAULT_PANEL'] = 'Datos generales';
-$mod_strings['LBL_PANEL_RECORD_DETAILS'] = 'Detalles del registro';
+$mod_strings['LBL_DEFAULT_PANEL'] = 'Datos xerais';
+$mod_strings['LBL_PANEL_RECORD_DETAILS'] = 'Detalles do rexistro';
 $mod_strings['LBL_ACCOUNT'] = 'Organización';
-$mod_strings['LBL_CONTACTS_SUBPANEL_TITLE'] = 'Personas';
-$mod_strings['LBL_DEFAULT_SUBPANEL_TITLE'] = 'Organizaciones';
+$mod_strings['LBL_CONTACTS_SUBPANEL_TITLE'] = 'Persoas';
+$mod_strings['LBL_DEFAULT_SUBPANEL_TITLE'] = 'Organizacións';
 $mod_strings['LBL_DUPLICATE'] = 'Posible organización duplicada';
 $mod_strings['LBL_LIST_ACCOUNT_NAME'] = 'Organización';
-$mod_strings['LBL_PARENT_ACCOUNT_ID'] = 'ID de la Organización padre';
-$mod_strings['LBL_SAVE_ACCOUNT'] = 'Guardar la Organización';
-$mod_strings['LNK_ACCOUNT_LIST'] = 'Organizaciones';
-$mod_strings['LNK_NEW_ACCOUNT'] = 'Nueva Organización';
-$mod_strings['MSG_DUPLICATE'] = 'El registro que está a punto de crear podría ser un duplicado de otra organización existente. Los registros de organización con nombres similares se listan a continuación. Para confirmar la creación de esta organización haga clic en Guardar. En caso contrario, pulse Cancelar.';
-$mod_strings['MSG_SHOW_DUPLICATES'] = 'El registro que está a punto de crear podría ser un duplicado de otra organización existente. Los registros de organización con nombres similares se listan a continuación. Para confirmar la creación de esta organización haga clic en Guardar. En caso contrario, pulse Cancelar.';
+$mod_strings['LBL_PARENT_ACCOUNT_ID'] = 'ID da Organización pai';
+$mod_strings['LBL_SAVE_ACCOUNT'] = 'Gardar a Organización';
+$mod_strings['LNK_ACCOUNT_LIST'] = 'Organizacións';
+$mod_strings['LNK_NEW_ACCOUNT'] = 'Nova Organización';
+$mod_strings['MSG_DUPLICATE'] = 'O rexistro que está a punto de crear podería ser un duplicado doutra organización existente. Os rexistros de organización con nomes similares lístanse a continuación. Para confirmar a creación desta organización faga clic en Gardar. En caso contrario, pulse Cancelar.';
+$mod_strings['MSG_SHOW_DUPLICATES'] = 'O rexistro que está a punto de crear podería ser un duplicado doutra organización existente. Os rexistros de organización con nomes similares lístanse a continuación. Para confirmar a creación desta organización faga clic en Gardar. En caso contrario, pulse Cancelar.';
 $mod_strings['LBL_BILLING_ACCOUNT'] = 'Organización';
-$mod_strings['LBL_BILLING_CONTACT'] = 'Persona';
+$mod_strings['LBL_BILLING_CONTACT'] = 'Persoa';
 $mod_strings['LBL_OPPORTUNITY'] = 'Subvención';
-$mod_strings['LBL_CREATE_OPPORTUNITY'] = 'Nueva Subvención';
+$mod_strings['LBL_CREATE_OPPORTUNITY'] = 'Nova Subvención';
