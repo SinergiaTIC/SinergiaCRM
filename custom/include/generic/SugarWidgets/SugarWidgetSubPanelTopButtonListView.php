b3ea04ff6520aaace6423dc52cfae6dfb2322d71
Juan Chamizo González <juan@sinergiacrm.org>
2026-01-14 11:42:50 +0000

    Evitar errores en "Ver en vista de lista" en el subpanel firmantes

--- custom/include/generic/SugarWidgets/SugarWidgetSubPanelTopButtonListView.php
+++ custom/include/generic/SugarWidgets/SugarWidgetSubPanelTopButtonListView.php
@@ -55,6 +55,99 @@ class SugarWidgetSubPanelTopButtonListView extends SugarWidgetSubPanelTopButton
 
     }
 
+    // STIC-Custom 20260113 JCH - Override get_subpanel_relationship_name to sanitize the relationship name
+    // This prevents jQuery selector errors when the relationship name contains special characters like "::"
+    // which can occur when using custom functions like "function:stic_SignersUtils::getSticSignersForSignature"
+    // https://github.com/SinergiaTIC/SinergiaCRM/issues/726
+    /**
+     * get_subpanel_relationship_name
+     * Get the relationship name based on the subpanel definition
+     * Sanitizes the name to remove characters invalid for jQuery/CSS selectors
+     * @param mixed $defines The subpanel definition
+     * @return string The sanitized relationship name
+     */
+    public function get_subpanel_relationship_name($defines)
+    {
+        $relationship_name = parent::get_subpanel_relationship_name($defines);
+        // Sanitize the relationship name by replacing :: with underscores
+        // This is necessary because jQuery selectors cannot handle :: in IDs
+        $relationship_name = str_replace('::', '___', $relationship_name);
+        return $relationship_name;
+    }
+    // END STIC-Custom
+
+    // STIC-Custom 20260113 JCH - Helper method to get the search field name for filtering
+    // When using custom functions in get_subpanel_data, we need to find the actual relationship field
+    // https://github.com/SinergiaTIC/SinergiaCRM/issues/726
+    /**
+     * get_search_field_name
+     * Get the correct field name to use in the advanced search form
+     * When a custom function is used, tries to find the actual relationship field
+     * @param mixed $defines The subpanel definition
+     * @param string $relationship_name The sanitized relationship name
+     * @return array Array with 'name' and 'id' field names
+     */
+    protected function get_search_field_name($defines, $relationship_name)
+    {
+        // Check if the relationship_name suggests this is from a custom function
+        // If it contains '___' it means it was sanitized from '::' (custom function pattern)
+        $is_custom_function = (strpos($relationship_name, '___') !== false && 
+                               !isset($defines[$relationship_name]));
+        
+        if ($is_custom_function) {
+            // This is likely a custom function, construct the field name based on table names
+            $parent_table = !empty($defines['focus']->table_name) ? $defines['focus']->table_name : '';
+            $child_module_name = $defines['child_module_name'];
+            
+            if (!empty($child_module_name) && !empty($parent_table)) {
+                $childBean = BeanFactory::newBean($child_module_name);
+                $child_table = !empty($childBean->table_name) ? $childBean->table_name : '';
+                
+                if (!empty($child_table) && !empty($childBean->field_defs)) {
+                    // SuiteCRM pattern for many-to-many relationships: {parent_table}_{child_table}
+                    $relationship_base = $parent_table . '_' . $child_table;
+                    $field_name = $relationship_base . '_name';
+                    $field_id = $relationship_base . $parent_table . '_ida';
+                    
+                    // First try: exact match with constructed field names  
+                    if (!empty($childBean->field_defs[$field_name]) && !empty($childBean->field_defs[$field_id])) {
+                        return array(
+                            'name' => $field_name,
+                            'id' => $field_id
+                        );
+                    }
+                    
+                    // Second try: use id_name from the relate field definition
+                    if (!empty($childBean->field_defs[$field_name]) && !empty($childBean->field_defs[$field_name]['id_name'])) {
+                        return array(
+                            'name' => $field_name,
+                            'id' => $childBean->field_defs[$field_name]['id_name']
+                        );
+                    }
+                    
+                    // Third try: search for any relate field that points to the parent table
+                    foreach ($childBean->field_defs as $fname => $fdef) {
+                        if (!empty($fdef['type']) && $fdef['type'] == 'relate' 
+                            && !empty($fdef['table']) && $fdef['table'] == $parent_table
+                            && !empty($fdef['id_name'])) {
+                            return array(
+                                'name' => $fname,
+                                'id' => $fdef['id_name']
+                            );
+                        }
+                    }
+                }
+            }
+        }
+        
+        // Default: use the relationship_name with _name suffix
+        return array(
+            'name' => $relationship_name . '_name',
+            'id' => $relationship_name . '_id'
+        );
+    }
+    // END STIC-Custom
+
     public function &_get_form($defines, $additionalFormFields = null, $asUrl = false)
     {
         global $app_strings;
@@ -179,10 +272,19 @@ class SugarWidgetSubPanelTopButtonListView extends SugarWidgetSubPanelTopButton
         $formValues['action'] = "EditView";
 
         // STIC-Custom 20230706 AAM - Adding parameters that will bring the user to the Filtered List View
+        // STIC-Custom 20260113 JCH - Use the correct search field name for custom functions
+        // https://github.com/SinergiaTIC/SinergiaCRM/issues/726
         $formValues['action'] = "ListView";
         $formValues['query'] = "true";
         $formValues['searchFormTab'] = "advanced_search";
-        $formValues[$relationship_name.'_name_advanced'] = $name;
+        $search_fields = $this->get_search_field_name($defines, $relationship_name);
+        
+        if (!empty($search_fields['name'])) {
+            $formValues[$search_fields['name'].'_advanced'] = $name;
+        }
+        if (!empty($search_fields['id']) && !empty($defines['focus']->id)) {
+            $formValues[$search_fields['id'].'_advanced'] = $defines['focus']->id;
+        }
         // END STIC-Custom
 
         if ($asUrl) {
