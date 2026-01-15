b285c4b03442b22ce6be11da9f8b4a8785ce981b
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-15 10:51:48 +0100

    New translations es_es.sticlang.php (Galician)

--- custom/Extension/modules/Prospects/Ext/Language/gl_ES.SticLang.php
+++ custom/Extension/modules/Prospects/Ext/Language/gl_ES.SticLang.php
@@ -22,6 +22,6 @@
  */
 $mod_strings['LBL_FP_EVENTS_PROSPECTS_1_FROM_FP_EVENTS_TITLE'] = 'Eventos-FP'; // This module is not used in SinergiaCRM and it's relabelled to avoid confusion with SinergiaCRM Events module
 $mod_strings['LBL_PROSPECTS_DOCUMENTS_1_FROM_DOCUMENTS_TITLE'] = 'Documentos';
-$mod_strings['LBL_ATTACHMENT_DESCRIPTION'] = 'Fichero recibido desde un formulario web relacionado con la campaña: ';
+$mod_strings['LBL_ATTACHMENT_DESCRIPTION'] = 'Ficheiro recibido desde un formulario web relacionado coa campaña: ';
 
 
