e5127f227092dfda3c319f0215d5d0c461c3e83d
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-15 14:01:03 +0100

    New translations es_es.lang.php (Galician)

--- modules/OptimisticLock/language/gl_ES.lang.php
+++ modules/OptimisticLock/language/gl_ES.lang.php
@@ -45,10 +45,10 @@ if (!defined('sugarEntry') || !sugarEntry) {
 $mod_strings = array(
     'LBL_YOURS' => 'Suyo',
     'LBL_IN_DATABASE' => 'En Base de datos',
-    'LBL_CONFLICT_EXISTS' => 'Hay un Conflicto Para - ',
+    'LBL_CONFLICT_EXISTS' => 'Hai un Conflito Para - ',
     'LBL_ACCEPT_DATABASE' => 'Aceptar Base de datos',
     'LBL_ACCEPT_YOURS' => 'Aceptar Suyo',
-    'LBL_RECORDS_MATCH' => 'Los Registros Coinciden',
-    'LBL_NO_LOCKED_OBJECTS' => 'No hay Objetos Bloqueados',
+    'LBL_RECORDS_MATCH' => 'Os Rexistros Coinciden',
+    'LBL_NO_LOCKED_OBJECTS' => 'Non hai Obxectos Bloqueados',
 
 );
