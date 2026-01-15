c861f1aa1787d6cf9f4127dd327b2b44332fe151
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-15 10:51:03 +0100

    New translations es_es.sticlang.php (Galician)

--- custom/Extension/modules/Home/Ext/Language/gl_ES.SticLang.php
+++ custom/Extension/modules/Home/Ext/Language/gl_ES.SticLang.php
@@ -24,15 +24,15 @@
 $mod_strings['LBL_DASHLET_STIC_NEWS'] = 'Noticias SinergiaCRM';
 
 // Acerca de
-$mod_strings['LBL_ABOUT_SINERGIACRM_TITLE'] = 'SinergiaCRM - CRM de código abierto para organizaciones no lucrativas';
+$mod_strings['LBL_ABOUT_SINERGIACRM_TITLE'] = 'SinergiaCRM - CRM de código aberto para organizacións non lucrativas';
 $mod_strings['LBL_ABOUT_SINERGIACRM'] = 'Acerca de SinergiaCRM';
-$mod_strings['LBL_ABOUT_SINERGIACRM_1'] = 'SinergiaCRM es un desarrollo de la Asociación SinergiaTIC basado en SuiteCRM y publicado bajo la licencia de código abierto AGPLv3.';
-$mod_strings['LBL_ABOUT_SINERGIACRM_2'] = 'La documentación de SinergiaCRM está disponible en <a target="_blank" href="https://wikisuite.sinergiacrm.org">el wiki del producto</a>.';
+$mod_strings['LBL_ABOUT_SINERGIACRM_1'] = 'SinergiaCRM é un desenvolvemento da Asociación SinergiaTIC baeado en SuiteCRM e publicado baixo a licenza de código aberto AGPLv3.';
+$mod_strings['LBL_ABOUT_SINERGIACRM_2'] = 'A documentación de SinergiaCRM está dispoñible en <a target="_blank" href="https://wikisuite.sinergiacrm.org">o wiki do produto</a>.';
 
-$mod_strings['LBL_ABOUT_SINERGIACRM_CONTRIBUTORS'] = 'SinergiaCRM incluye';
+$mod_strings['LBL_ABOUT_SINERGIACRM_CONTRIBUTORS'] = 'SinergiaCRM inclúe';
 $mod_strings['LBL_ABOUT_SINERGIACRM_KREPORTER'] = 'Kreporter de Christian Knoll';
 $mod_strings['LBL_ABOUT_SINERGIACRM_MAIL_MERGE'] = 'Mail Merge Reports de Izertis';
-$mod_strings['LBL_ABOUT_SINERGIACRM_OTHER'] = 'Otras liberías de código abierto que se encuentran en las carpetas <i>/vendor</i> y <i>/SticInclude/vendor</i>. La información de licencia de estas librerías puede encontrarse en sus propios ficheros.';
+$mod_strings['LBL_ABOUT_SINERGIACRM_OTHER'] = 'Outras librarías de código aberto que se atopan nas carpetas <i>/vendor</i> e <i>/SticInclude/vendor</i>. A información de licenza destas librarías pódese atopar nos seus propios ficheiros.';
 
 // SinergiaDA
 $mod_strings['LBL_STIC_SINERGIADA_NOT_ALLOWED'] = 'No tiene habilitado el acceso a SinergiaDA. Contacte con el administrador.'; 
