26b7c2edba6cc1651ccf80513284bde35242d2b7
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-15 13:03:20 +0100

    New translations es_es.lang.php (Galician)

--- modules/Connectors/connectors/sources/ext/rest/twitter/language/gl_ES.lang.php
+++ modules/Connectors/connectors/sources/ext/rest/twitter/language/gl_ES.lang.php
@@ -44,8 +44,8 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $connector_strings = array(
     //Vardef labels
-    'LBL_LICENSING_INFO' => '<table border="0" cellspacing="1"> <tr><th valign="top" width="35%" class="dataLabel"> Información de la aplicación de Twitter</th></tr> <tr><td width="35%" class="dataLabel">, necesitará crear una cuenta de desarrolladores de Twitter y aplicación Para registrarse</td></tr></table>',
+    'LBL_LICENSING_INFO' => '<table border="0" cellspacing="1"> <tr><th valign="top" width="35%" class="dataLabel"> Información da aplicación de Twitter</th></tr> <tr><td width="35%" class="dataLabel">, necesitará crear unha conta de desarrolladores de Twitter e aplicación Para rexgistrarse</td></tr></table>',
     //Configuration labels
-    'consumer_key' => 'Clave del Consumidor',
-    'consumer_secret' => 'Secreto del Consumidor',
+    'consumer_key' => 'Clave do Consumidor',
+    'consumer_secret' => 'Segredo do Consumidor',
 );
