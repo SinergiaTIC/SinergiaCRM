f852d269a6b9706691e0e58e7e71cb16634672bd
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2026-01-15 11:04:24 +0100

    New translations es_es.lang.php (Galician)

--- include/SugarObjects/templates/basic/language/gl_ES.lang.php
+++ include/SugarObjects/templates/basic/language/gl_ES.lang.php
@@ -44,17 +44,17 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
     'LBL_ID' => 'ID',
-    'LBL_DATE_ENTERED' => 'Fecha de Creación',
-    'LBL_DATE_MODIFIED' => 'Fecha de Modificación',
+    'LBL_DATE_ENTERED' => 'Data de Creación',
+    'LBL_DATE_MODIFIED' => 'Data de Modificación',
     'LBL_MODIFIED' => 'Modificado Por',
-    'LBL_MODIFIED_NAME' => 'Modificado Por Nombre',
+    'LBL_MODIFIED_NAME' => 'Modificado Por Nome',
     'LBL_CREATED' => 'Creado por',
-    'LBL_DESCRIPTION' => 'Descripción',
+    'LBL_DESCRIPTION' => 'Descrición',
     'LBL_DELETED' => 'Eliminado',
-    'LBL_NAME' => 'Nombre',
-    'LBL_CREATED_USER' => 'Creado por el Usuario',
-    'LBL_MODIFIED_USER' => 'Modificado por el Usuario',
-    'LBL_LIST_NAME' => 'Nombre',
+    'LBL_NAME' => 'Nome',
+    'LBL_CREATED_USER' => 'Creado polo Usuario',
+    'LBL_MODIFIED_USER' => 'Modificado polo Usuario',
+    'LBL_LIST_NAME' => 'Nome',
     'LBL_EDIT_BUTTON' => 'Editar',
     // STIC-Custom 20240214 JBL - QuickEdit view
     // https://github.com/SinergiaTIC/SinergiaCRM/pull/93
@@ -66,7 +66,7 @@ $mod_strings = array(
     'LBL_DESCENDING' => 'Descendente',
     // Emails
     'LBL_OPT_IN' => 'Autorizar',
-    'LBL_OPT_IN_PENDING_EMAIL_NOT_SENT' => 'Confirmación de Autorización pendiente, Confirmación de Autorización no enviada',
-    'LBL_OPT_IN_PENDING_EMAIL_SENT' => 'Confirmación de Autorización pendiente, Confirmación de Autorización enviada',
+    'LBL_OPT_IN_PENDING_EMAIL_NOT_SENT' => 'Confirmación de Autorización pendente, Confirmación de Autorización non enviada',
+    'LBL_OPT_IN_PENDING_EMAIL_SENT' => 'Confirmación de Autorización pendente, Confirmación de Autorización enviada',
     'LBL_OPT_IN_CONFIRMED' => 'Autorizado',
 );
