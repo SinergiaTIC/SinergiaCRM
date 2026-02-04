7fce1ad292ff0d8fbb2af95363e82eb749eb30c7
AlbertoSTIC <alberto@sinergiacrm.org>
2026-02-04 13:51:50 +0000

    Adding comments

--- jssource/src_files/modules/ACLRoles/ACLRoles.js
+++ jssource/src_files/modules/ACLRoles/ACLRoles.js
@@ -1,10 +1,12 @@
 /**
- *
  * SugarCRM Community Edition is a customer relationship management program developed by
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
  * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ *
+ * SinergiaCRM is a work developed by SinergiaTIC Association, based on SuiteCRM.
+ * Copyright (C) 2013 - 2023 SinergiaTIC Association
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -26,17 +28,19 @@
  * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
  * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
  *
+ * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
+ * 
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU Affero General Public License version 3.
  *
  * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
- * reasonably feasible for technical reasons, the Appropriate Legal Notices must
- * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ * SugarCRM" logo, "Supercharged by SuiteCRM" logo and “Nonprofitized by SinergiaCRM” logo. 
+ * If the display of the logos is not reasonably feasible for technical reasons, 
+ * the Appropriate Legal Notices must display the words "Powered by SugarCRM", 
+ * "Supercharged by SuiteCRM" and “Nonprofitized by SinergiaCRM”. 
  */
-
 var aclviewer = function () {
   var lastDisplay = '';
   return {
@@ -54,11 +58,21 @@ var aclviewer = function () {
         'success': aclviewer.postSave,
         'failure': aclviewer.failed
       });
-      ajaxStatus.showStatus(SUGAR.language.get('app_strings', 'LBL_SAVING'));
+      // STIC-Custom AAM 20260204 - showStatus not working properly with async calls
+      // ajaxStatus.showStatus(SUGAR.language.get('app_strings', 'LBL_SAVING'));
+      aclviewer.showMessageACLRoles(SUGAR.language.get("app_strings", "LBL_SAVING")+"...");
+      // END STIC-Custom
     },
     postSave: function (o) {
       SUGAR.util.globalEval(o.responseText);
       aclviewer.view(result['role_id'], result['module']);
+      // STIC-Custom AAM 20260204 - showStatus not working properly with async calls
+      aclviewer.hideMessageACLRoles();
+      aclviewer.showMessageACLRoles(SUGAR.language.get("app_strings", "LBL_REQUEST_PROCESSED"));
+      setTimeout(() => {
+        aclviewer.hideMessageACLRoles();
+      }, 2000); 
+      // END STIC-Custom
     },
     display: function (o) {
       aclviewer.lastDisplay = '';
@@ -92,7 +106,27 @@ var aclviewer = function () {
     showDisplay: function (id) {
       document.getElementById(id).style.display = '';
       document.getElementById(id + 'link').style.display = 'none';
-    }
+    },
+    // STIC-Custom AAM 20260204 - showStatus not working properly with async calls
+    showMessageACLRoles: function(message) {
+        SUGAR.ajaxUI.loadingPanel = new YAHOO.widget.Panel("ajaxloading",
+            {
+                width:"240px",
+                fixedcenter:true,
+                close:false,
+                draggable:false,
+                constraintoviewport:false,
+                modal:true,
+                visible:false
+            });
+        SUGAR.ajaxUI.loadingPanel.setBody('<div class="acl-saving-message"><b>' + message + '</b></div>');
+        SUGAR.ajaxUI.loadingPanel.render(document.body);
+        SUGAR.ajaxUI.loadingPanel.show();
+    },
+    hideMessageACLRoles: function() {
+        SUGAR.ajaxUI.loadingPanel.hide();
+    },
+    // END STIC-Custom
 
 
   };
--- modules/ACLRoles/ACLRoles.js
+++ modules/ACLRoles/ACLRoles.js
@@ -1,10 +1,12 @@
 /**
- *
  * SugarCRM Community Edition is a customer relationship management program developed by
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
  * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ *
+ * SinergiaCRM is a work developed by SinergiaTIC Association, based on SuiteCRM.
+ * Copyright (C) 2013 - 2023 SinergiaTIC Association
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -26,88 +28,17 @@
  * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
  * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
  *
+ * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU Affero General Public License version 3.
  *
  * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
- * reasonably feasible for technical reasons, the Appropriate Legal Notices must
- * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
- */ var aclviewer = (function () {
-  var lastDisplay = "";
-  return {
-    view: function (role_id, role_module) {
-      YAHOO.util.Connect.asyncRequest(
-        "POST",
-        "index.php",
-        { success: aclviewer.display, failure: aclviewer.failed },
-        "module=ACLRoles&action=EditRole&record=" + role_id + "&category_name=" + role_module
-      );
-    //   ajaxStatus.showStatus(SUGAR.language.get("app_strings", "LBL_REQUEST_PROCESSED"));
-    },
-    save: function (form_name) {
-      var formObject = document.getElementById(form_name);
-      YAHOO.util.Connect.setForm(formObject);
-      YAHOO.util.Connect.asyncRequest("POST", "index.php", { success: aclviewer.postSave, failure: aclviewer.failed });
-    //   ajaxStatus.showStatus(SUGAR.language.get("app_strings", "LBL_SAVING"));
-      aclviewer.showMessageACLRoles(SUGAR.language.get("app_strings", "LBL_SAVING")+"...");
-
-    },
-    postSave: function (o) {
-      aclviewer.hideMessageACLRoles();
-      SUGAR.util.globalEval(o.responseText);
-      aclviewer.view(result["role_id"], result["module"]);
-      aclviewer.showMessageACLRoles(SUGAR.language.get("app_strings", "LBL_REQUEST_PROCESSED"));
-      setTimeout(() => {
-        aclviewer.hideMessageACLRoles();
-      }, 2000); 
-    },
-    display: function (o) {
-      aclviewer.lastDisplay = "";
-      ajaxStatus.flashStatus("Done");
-      document.getElementById("category_data").innerHTML = o.responseText;
-    },
-    failed: function () {
-      ajax.flashStatus("Could Not Connect");
-    },
-    toggleDisplay: function (id) {
-      if (aclviewer.lastDisplay != "" && typeof aclviewer.lastDisplay != "undefined") {
-        aclviewer.hideDisplay(aclviewer.lastDisplay);
-      }
-      if (aclviewer.lastDisplay != id) {
-        aclviewer.showDisplay(id);
-        aclviewer.lastDisplay = id;
-      } else {
-        aclviewer.lastDisplay = "";
-      }
-    },
-    hideDisplay: function (id) {
-      document.getElementById(id).style.display = "none";
-      document.getElementById(id + "link").style.display = "";
-    },
-    showDisplay: function (id) {
-      document.getElementById(id).style.display = "";
-      document.getElementById(id + "link").style.display = "none";
-    },
-    showMessageACLRoles: function(message) {
-        SUGAR.ajaxUI.loadingPanel = new YAHOO.widget.Panel("ajaxloading",
-            {
-                width:"240px",
-                fixedcenter:true,
-                close:false,
-                draggable:false,
-                constraintoviewport:false,
-                modal:true,
-                visible:false
-            });
-        SUGAR.ajaxUI.loadingPanel.setBody('<div class="acl-saving-message"><b>' + message + '</b></div>');
-        SUGAR.ajaxUI.loadingPanel.render(document.body);
-        SUGAR.ajaxUI.loadingPanel.show();
-    },
-    hideMessageACLRoles: function() {
-        SUGAR.ajaxUI.loadingPanel.hide();
-    },
-  };
-})();
+ * SugarCRM" logo, "Supercharged by SuiteCRM" logo and “Nonprofitized by SinergiaCRM” logo.
+ * If the display of the logos is not reasonably feasible for technical reasons,
+ * the Appropriate Legal Notices must display the words "Powered by SugarCRM",
+ * "Supercharged by SuiteCRM" and “Nonprofitized by SinergiaCRM”.
+ */var aclviewer=function(){var lastDisplay='';return{view:function(role_id,role_module){YAHOO.util.Connect.asyncRequest('POST','index.php',{'success':aclviewer.display,'failure':aclviewer.failed},'module=ACLRoles&action=EditRole&record='+role_id+'&category_name='+role_module);ajaxStatus.showStatus(SUGAR.language.get('app_strings','LBL_REQUEST_PROCESSED'));},save:function(form_name){var formObject=document.getElementById(form_name);YAHOO.util.Connect.setForm(formObject);YAHOO.util.Connect.asyncRequest('POST','index.php',{'success':aclviewer.postSave,'failure':aclviewer.failed});aclviewer.showMessageACLRoles(SUGAR.language.get("app_strings","LBL_SAVING")+"...");},postSave:function(o){SUGAR.util.globalEval(o.responseText);aclviewer.view(result['role_id'],result['module']);aclviewer.hideMessageACLRoles();aclviewer.showMessageACLRoles(SUGAR.language.get("app_strings","LBL_REQUEST_PROCESSED"));setTimeout(()=>{aclviewer.hideMessageACLRoles();},2000);},display:function(o){aclviewer.lastDisplay='';ajaxStatus.flashStatus('Done');document.getElementById('category_data').innerHTML=o.responseText;},failed:function(){ajax.flashStatus('Could Not Connect');},toggleDisplay:function(id){if(aclviewer.lastDisplay!=''&&typeof(aclviewer.lastDisplay)!='undefined'){aclviewer.hideDisplay(aclviewer.lastDisplay);}
+if(aclviewer.lastDisplay!=id){aclviewer.showDisplay(id);aclviewer.lastDisplay=id;}else{aclviewer.lastDisplay='';}},hideDisplay:function(id){document.getElementById(id).style.display='none';document.getElementById(id+'link').style.display='';},showDisplay:function(id){document.getElementById(id).style.display='';document.getElementById(id+'link').style.display='none';},showMessageACLRoles:function(message){SUGAR.ajaxUI.loadingPanel=new YAHOO.widget.Panel("ajaxloading",{width:"240px",fixedcenter:true,close:false,draggable:false,constraintoviewport:false,modal:true,visible:false});SUGAR.ajaxUI.loadingPanel.setBody('<div class="acl-saving-message"><b>'+message+'</b></div>');SUGAR.ajaxUI.loadingPanel.render(document.body);SUGAR.ajaxUI.loadingPanel.show();},hideMessageACLRoles:function(){SUGAR.ajaxUI.loadingPanel.hide();},};}();
