bd3d60b525f95bf218c7b114a018dadd918864af
Juan Chamizo González <juan@sinergiacrm.org>
2026-02-04 15:41:05 +0000

    Feature - Añadir campos relacionados con activos en los módulos de Seguimientos y mejorar la búsqueda

--- modules/stic_FollowUps/metadata/searchdefs.php
+++ modules/stic_FollowUps/metadata/searchdefs.php
@@ -1,25 +1,4 @@
 <?php
-/**
- * This file is part of SinergiaCRM.
- * SinergiaCRM is a work developed by SinergiaTIC Association, based on SuiteCRM.
- * Copyright (C) 2013 - 2023 SinergiaTIC Association
- *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Affero General Public License version 3 as published by the
- * Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Affero General Public License along with
- * this program; if not, see http://www.gnu.org/licenses or write to the Free
- * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301 USA.
- *
- * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
- */
 $module_name = 'stic_FollowUps';
 $searchdefs[$module_name] =
 array(
@@ -261,6 +240,15 @@ array(
                 'default' => true,
                 'name' => 'color',
             ),
+            'stic_assets_stic_followups_name' => array(
+                'type' => 'relate',
+                'link' => true,
+                'label' => 'LBL_STIC_ASSETS_STIC_FOLLOWUPS_FROM_STIC_ASSETS_TITLE',
+                'id' => 'STIC_ASSETS_STIC_FOLLOWUPSSTIC_ASSETS_IDA',
+                'width' => '10%',
+                'default' => true,
+                'name' => 'stic_assets_stic_followups_name',
+            ),
             'description' => array(
                 'type' => 'text',
                 'label' => 'LBL_DESCRIPTION',
@@ -322,5 +310,3 @@ array(
         ),
     ),
 );
-
-?>
