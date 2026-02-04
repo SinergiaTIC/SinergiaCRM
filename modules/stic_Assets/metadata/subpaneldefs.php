7968ac0fa49433451e7c951274bfd5a18006f5b9
Juan Chamizo González <juan@sinergiacrm.org>
2026-02-04 12:52:53 +0000

    Feature - Añadir subpanel de grupos de seguridad y mejorar la configuración de subpaneles para documentos y seguimientos en el módulo de Activos

--- modules/stic_Assets/metadata/subpaneldefs.php
+++ modules/stic_Assets/metadata/subpaneldefs.php
@@ -21,46 +21,53 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 
-$layout_defs["stic_Assets"]["subpanel_setup"]['stic_assets_documents'] = array (
-  'order' => 100,
-  'module' => 'Documents',
-  'subpanel_name' => 'default',
-  'sort_order' => 'asc',
-  'sort_by' => 'id',
-  'title_key' => 'LBL_STIC_ASSETS_DOCUMENTS_FROM_DOCUMENTS_TITLE',
-  'get_subpanel_data' => 'stic_assets_documents',
-  'top_buttons' => 
-  array (
-    0 => 
-    array (
-      'widget_class' => 'SubPanelTopButtonQuickCreate',
-    ),
-    1 => 
-    array (
-      'widget_class' => 'SubPanelTopSelectButton',
-      'mode' => 'MultiSelect',
-    ),
-  ),
+$layout_defs['stic_Assets']['subpanel_setup']['securitygroups'] = array(
+    'top_buttons' => array(array('widget_class' => 'SubPanelTopSelectButton', 'popup_module' => 'SecurityGroups', 'mode' => 'MultiSelect')),
+    'order' => 900,
+    'sort_by' => 'name',
+    'sort_order' => 'asc',
+    'module' => 'SecurityGroups',
+    'refresh_page' => 1,
+    'subpanel_name' => 'default',
+    'get_subpanel_data' => 'SecurityGroups',
+    'add_subpanel_data' => 'securitygroup_id',
+    'title_key' => 'LBL_SECURITYGROUPS_SUBPANEL_TITLE',
 );
 
-$layout_defs["stic_Assets"]["subpanel_setup"]['stic_assets_stic_followups'] = array (
-  'order' => 100,
-  'module' => 'stic_FollowUps',
-  'subpanel_name' => 'default',
-  'sort_order' => 'asc',
-  'sort_by' => 'id',
-  'title_key' => 'LBL_STIC_ASSETS_STIC_FOLLOWUPS_FROM_STIC_FOLLOWUPS_TITLE',
-  'get_subpanel_data' => 'stic_assets_stic_followups',
-  'top_buttons' => 
-  array (
-    0 => 
-    array (
-      'widget_class' => 'SubPanelTopButtonQuickCreate',
+$layout_defs["stic_Assets"]["subpanel_setup"]['stic_assets_documents'] = array(
+    'order' => 100,
+    'module' => 'Documents',
+    'subpanel_name' => 'default',
+    'sort_order' => 'asc',
+    'sort_by' => 'id',
+    'title_key' => 'LBL_STIC_ASSETS_DOCUMENTS_FROM_DOCUMENTS_TITLE',
+    'get_subpanel_data' => 'stic_assets_documents',
+    'top_buttons' => array(
+        0 => array(
+            'widget_class' => 'SubPanelTopButtonQuickCreate',
+        ),
+        1 => array(
+            'widget_class' => 'SubPanelTopSelectButton',
+            'mode' => 'MultiSelect',
+        ),
     ),
-    1 => 
-    array (
-      'widget_class' => 'SubPanelTopSelectButton',
-      'mode' => 'MultiSelect',
+);
+
+$layout_defs["stic_Assets"]["subpanel_setup"]['stic_assets_stic_followups'] = array(
+    'order' => 100,
+    'module' => 'stic_FollowUps',
+    'subpanel_name' => 'default',
+    'sort_order' => 'asc',
+    'sort_by' => 'id',
+    'title_key' => 'LBL_STIC_ASSETS_STIC_FOLLOWUPS_FROM_STIC_FOLLOWUPS_TITLE',
+    'get_subpanel_data' => 'stic_assets_stic_followups',
+    'top_buttons' => array(
+        0 => array(
+            'widget_class' => 'SubPanelTopButtonQuickCreate',
+        ),
+        1 => array(
+            'widget_class' => 'SubPanelTopSelectButton',
+            'mode' => 'MultiSelect',
+        ),
     ),
-  ),
 );
