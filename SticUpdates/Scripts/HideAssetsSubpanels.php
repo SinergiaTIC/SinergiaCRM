091e7cb1c03aa832849b5b4cbacc74d7298d9d72
enricsinergia <57436616+enricsinergia@users.noreply.github.com>
2026-03-10 11:25:00 +0100

    Hotfix - General - Ejecución reparación tablas (SticUpdate) (#1016)

--- SticUpdates/Scripts/HideAssetsSubpanels.php
+++ SticUpdates/Scripts/HideAssetsSubpanels.php
@@ -80,16 +80,6 @@ $current_user = new User();
 $current_user->getSystemUser();
 
 // Reparación de roles para garantizar que los usuarios no administradores pueden acceder a los módulos
-echo '<h3>Repairing roles</h3>';
+// echo '<h3>Repairing roles</h3>';
 require_once 'modules/ACL/install_actions.php';
 $GLOBALS['log']->fatal('Line ' . __LINE__ . ': ' . __METHOD__ . ':  Repairing roles');
-
-// Reparamos también relaciones e índices para evitar incidencias con los nuevos módulos
-echo '<h3>Rebuilding relationships</h3>';
-require_once 'modules/Administration/RebuildRelationship.php';
-$GLOBALS['log']->fatal('Line ' . __LINE__ . ': ' . __METHOD__ . ':  Rebuilding relationships');
-
-echo '<h3>Repairing indexes</h3>';
-require_once "modules/Administration/RepairIndex.php";
-$GLOBALS['log']->fatal('Line ' . __LINE__ . ': ' . __METHOD__ . ':  Repairing indexes');
-
