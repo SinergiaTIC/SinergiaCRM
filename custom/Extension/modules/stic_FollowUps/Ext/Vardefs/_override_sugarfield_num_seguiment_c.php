af6c4cc5126a35a330258821c9782a47ea26a8ce
LluisSerrano <136106881+LluisSerrano@users.noreply.github.com>
2026-03-16 17:21:22 +0100

    Create _override_sugarfield_num_seguiment_c.php

---
+++ custom/Extension/modules/stic_FollowUps/Ext/Vardefs/_override_sugarfield_num_seguiment_c.php
@@ -0,0 +1,6 @@
+<?php
+ // created: 2026-03-13 08:05:48
+$dictionary['stic_FollowUps']['fields']['num_seguiment_c']['inline_edit']='1';
+$dictionary['stic_FollowUps']['fields']['num_seguiment_c']['labelValue']='Número de seguiment';
+
+ ?>
