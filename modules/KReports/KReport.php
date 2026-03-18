d2b44e1ea5f96d7feefc10595f03f1eeb75590c4
Enric Prats <enric@sinergiacrm.org>
2026-03-18 17:23:25 +0000

    Update PR reference

--- modules/KReports/KReport.php
+++ modules/KReports/KReport.php
@@ -1435,6 +1435,7 @@ class KReport extends SugarBean {
             //$formulaRaw = urldecode(base64_decode($thisListEntry ['formulavalue'], true));
             $formulaRaw = rawurldecode(base64_decode($thisListEntry ['formulavalue'], true));
             // STIC-Custom 20260318 EPS - Special characters in formulas are not properly handled - convert to UTF-8
+            // https://github.com/SinergiaTIC/SinergiaCRM/pull/1030
             $formulaRaw = mb_convert_encoding($formulaRaw, 'UTF-8', 'ISO-8859-1');
             // END STIC
 
