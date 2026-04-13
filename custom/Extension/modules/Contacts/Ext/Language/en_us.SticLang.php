3195c0cebf8875d642e798e71a8998ccea140a34
Jaume Albaigès <45965117+jalbaiges@users.noreply.github.com>
2026-04-13 13:56:06 +0200

    Minor fixes

--- custom/Extension/modules/Contacts/Ext/Language/en_us.SticLang.php
+++ custom/Extension/modules/Contacts/Ext/Language/en_us.SticLang.php
@@ -190,10 +190,10 @@ $mod_strings['LBL_STIC_CURRENT_PROJECTS_CONTACTS_NAME'] = 'Current project';
 $mod_strings['LBL_STIC_FINANCIAL_PRODUCTS_CONTACTS_FROM_STIC_FINANCIAL_PRODUCTS_TITLE'] = 'Financial products';
 
 // Private Area Strings
-$mod_strings['LBL_STIC_PA_USERNAME'] = 'Private Area User';
-$mod_strings['LBL_STIC_PA_PASSWORD'] = 'Private Area Password';
+$mod_strings['LBL_STIC_PA_USERNAME'] = 'Private Area user';
+$mod_strings['LBL_STIC_PA_PASSWORD'] = 'Private Area password';
 $mod_strings['LBL_STIC_PA_ENABLE'] = 'Enable Private Area';
-$mod_strings['LBL_STIC_PA_PASSWORD_ENCRYPTED'] = 'Private Area Password Encrypted (MD5)';
+$mod_strings['LBL_STIC_PA_PASSWORD_ENCRYPTED'] = 'Private Area encrypted password (MD5)';
 
 // Conversations
 $mod_strings['LBL_STIC_CONVERSATIONS_SUBPANEL_TITLE'] = 'Conversations';
