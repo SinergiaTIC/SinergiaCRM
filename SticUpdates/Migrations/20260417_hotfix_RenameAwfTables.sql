e885e74b7b008318d300bc4b8926d84c347d98b5
jordiSTIC <jordi@sinergiacrm.org>
2026-04-27 08:27:15 +0000

    Added IF EXISTS and rename audit tables

--- SticUpdates/Migrations/20260417_hotfix_RenameAwfTables.sql
+++ SticUpdates/Migrations/20260417_hotfix_RenameAwfTables.sql
@@ -1,6 +1,15 @@
-RENAME TABLE stic_AWF_Forms TO stic_awf_forms;
-RENAME TABLE stic_AWF_Responses TO stic_awf_responses;
-RENAME TABLE stic_AWF_Response_Details TO stic_awf_response_details;
-RENAME TABLE stic_AWF_Links TO stic_awf_links;
-RENAME TABLE stic_AWF_Deferred_Tickets TO stic_awf_deferred_tickets;
-RENAME TABLE stic_AWF_Incoming_Events TO stic_awf_incoming_events;
+-- Renaming AWF tables to lowercase
+RENAME TABLE IF EXISTS stic_AWF_Forms TO stic_awf_forms;
+RENAME TABLE IF EXISTS stic_AWF_Responses TO stic_awf_responses;
+RENAME TABLE IF EXISTS stic_AWF_Response_Details TO stic_awf_response_details;
+RENAME TABLE IF EXISTS stic_AWF_Links TO stic_awf_links;
+RENAME TABLE IF EXISTS stic_AWF_Deferred_Tickets TO stic_awf_deferred_tickets;
+RENAME TABLE IF EXISTS stic_AWF_Incoming_Events TO stic_awf_incoming_events;
+
+-- Renaming AWF audit tables to lowercase
+RENAME TABLE IF EXISTS stic_AWF_Forms_audit TO stic_awf_forms_audit ; 
+RENAME TABLE IF EXISTS stic_AWF_Responses_audit TO stic_awf_responses_audit;
+RENAME TABLE IF EXISTS stic_AWF_Response_Details_audit TO stic_awf_response_details_audit;
+RENAME TABLE IF EXISTS stic_AWF_Links_audit TO stic_awf_links_audit;
+RENAME TABLE IF EXISTS stic_AWF_Deferred_Tickets_audit TO stic_awf_deferred_tickets_audit;
+RENAME TABLE IF EXISTS stic_AWF_Incoming_Events_audit TO stic_awf_incoming_events_audit;
