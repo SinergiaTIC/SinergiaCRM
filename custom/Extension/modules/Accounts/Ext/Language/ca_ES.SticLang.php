7cffc30e27581a6b39c12c566224b441be8b6310
Jaume Albaigès <45965117+jalbaiges@users.noreply.github.com>
2026-05-14 17:40:22 +0200

    Improving translation

--- custom/Extension/modules/Accounts/Ext/Language/ca_ES.SticLang.php
+++ custom/Extension/modules/Accounts/Ext/Language/ca_ES.SticLang.php
@@ -185,8 +185,8 @@ $mod_strings['LBL_STIC_TRAINING_ACCOUNTS_FROM_STIC_TRAINING_TITLE'] = 'Formació
 $mod_strings['LBL_STIC_WORK_EXPERIENCE_ACCOUNTS_FROM_STIC_WORK_EXPERIENCE_TITLE'] = 'Experiència laboral';
 
 // Cadenes Àrea Privada
-$mod_strings['LBL_STIC_PA_USERNAME'] = "Usuari de l'Àrea Privada";
-$mod_strings['LBL_STIC_PA_PASSWORD'] = "Contrasenya de l'Àrea Privada";
-$mod_strings['LBL_STIC_PA_ENABLE'] = "Activa l'Àrea Privada";
-$mod_strings['LBL_STIC_PA_ENABLE_HELP'] = "Al activar aquesta opció, s'habilitarà l'accés a l'Àrea Privada per a aquesta organització. Si es desactiva aquesta opció, es revocarà l'accés a l'Àrea Privada. A més, per poder procedir amb l'enviament de les credencials d'accés a l'Àrea Privada, és necessari que s'activi la configuració 'PRIVATEAREA_SEND_CREDENTIALS_ON_ENABLE'. En cas contrari, no es podran enviar les credencials d'accés a l'Àrea Privada.";
-$mod_strings['LBL_STIC_PA_PASSWORD_HELP'] = "Es podrà introduir/canviar la contrasenya de l'Àrea Privada només si l'opció d'\"Activar Àrea Privada\" està habilitada.";
+$mod_strings['LBL_STIC_PA_USERNAME'] = "Usuari de l'àrea privada";
+$mod_strings['LBL_STIC_PA_PASSWORD'] = "Contrasenya de l'àrea privada";
+$mod_strings['LBL_STIC_PA_ENABLE'] = "Activa l'àrea privada";
+$mod_strings['LBL_STIC_PA_ENABLE_HELP'] = "Per tal que l'organització pugui accedir a l'àrea privada cal que aquest camp estigui marcat. Si no ho està, no s'hi podrà accedir encara que es disposi de nom d'usuari i contrasenya. En marcar aquest camp, si el paràmetre de configuració <a href='index.php?module=stic_Settings&action=DetailView&record=f8b4a3a2-9f40-4c2e-9f8d-5e76a20f5d11' target='_blank'>PRIVATEAREA_SEND_CREDENTIALS_ON_ENABLE</a> està activat, s'enviaran automàticament les credencials d'accés a l'organització. En cas contrari, caldrà fer-ho de forma manual.";
+$mod_strings['LBL_STIC_PA_PASSWORD_HELP'] = "Per tal de poder editar la contrasenya cal que el camp 'Activa l'àrea privada' estigui marcat.";
