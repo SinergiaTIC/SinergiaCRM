6e80bb028fb379dad971d67b8ad29d9cd8bf882f
Juan <juan@sinergiatic.org>
2026-05-18 11:10:27 +0000

    Eliminar índice único en el número de factura para permitir duplicados

--- custom/Extension/modules/AOS_Invoices/Ext/Vardefs/SticVardefs.php
+++ custom/Extension/modules/AOS_Invoices/Ext/Vardefs/SticVardefs.php
@@ -514,9 +514,3 @@ $dictionary['AOS_Invoices']['fields']['verifactu_previous_status_c'] = array(
     'popupHelp' => 'LBL_VERIFACTU_PREVIOUS_STATUS_HELP',
 );
 
-// Unique index on number to prevent duplicate invoice numbers
-$dictionary['AOS_Invoices']['indices'][] = array(
-    'name' => 'idx_invoice_number_unique',
-    'type' => 'unique',
-    'fields' => array('number'),
-);
