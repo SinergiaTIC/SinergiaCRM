f31a2a76d78b7dd86adb921e14a9735ec6d44e8a
LluisSerrano <lluis@sinergiatic.org>
2026-05-26 12:20:58 +0200

    Revert "Limitar acceso por ID en parámetro en Configuración, y otras mejoras"
    This reverts commit 3b3a527e2ddbbf8fd33de09f2104f76b6d926798.

--- custom/Extension/modules/Administration/Ext/Administration/SticWhistleblowingAdmin.php
+++ custom/Extension/modules/Administration/Ext/Administration/SticWhistleblowingAdmin.php
@@ -9,17 +9,17 @@ $admin_option_defs['Administration']['WhistleblowingConfig_Admin'] = [
     'index.php?module=Administration&action=WhistleblowingConfig_Admin'
 ];
 
-// if (!isset($admin_group_header['LBL_SINERGIACRM_TAB_TITLE']) || !isset($admin_group_header['LBL_SINERGIACRM_TAB_TITLE'][3])) {
-//     $admin_group_header['LBL_SINERGIACRM_TAB_TITLE'] = array(
-//         'LBL_SINERGIACRM_TAB_TITLE',
-//         '',
-//         false,
-//         $admin_option_defs,
-//         'LBL_SINERGIACRM_TAB_DESCRIPTION',
-//     );
-// } else {
-//     $admin_group_header['LBL_SINERGIACRM_TAB_TITLE'][3] = array_replace_recursive(
-//         $admin_group_header['LBL_SINERGIACRM_TAB_TITLE'][3], 
-//         $admin_option_defs
-//     );
-// }
+if (!isset($admin_group_header['LBL_SINERGIACRM_TAB_TITLE']) || !isset($admin_group_header['LBL_SINERGIACRM_TAB_TITLE'][3])) {
+    $admin_group_header['LBL_SINERGIACRM_TAB_TITLE'] = array(
+        'LBL_SINERGIACRM_TAB_TITLE',
+        '',
+        false,
+        $admin_option_defs,
+        'LBL_SINERGIACRM_TAB_DESCRIPTION',
+    );
+} else {
+    $admin_group_header['LBL_SINERGIACRM_TAB_TITLE'][3] = array_replace_recursive(
+        $admin_group_header['LBL_SINERGIACRM_TAB_TITLE'][3], 
+        $admin_option_defs
+    );
+}
