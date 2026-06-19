27234eece3d0957aca7bc883a965809f944d5d87
Jaume Albaigès <45965117+jalbaiges@users.noreply.github.com>
2026-06-19 18:19:53 +0200

    Replacing SuiteCRM by SinergiaCRM module names

--- custom/Extension/modules/Emails/Ext/Language/es_ES.SticLang.php
+++ custom/Extension/modules/Emails/Ext/Language/es_ES.SticLang.php
@@ -21,6 +21,23 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $mod_strings['LBL_STIC_PAYMENTS_ACTIVITIES_EMAILS_FROM_STIC_PAYMENTS_TITLE'] = 'Actividades:Pagos';
-$mod_strings['LBL_LEADS_SUBPANEL_TITLE'] = 'Interesados';
 $mod_strings['LBL_EMAIL_INFORMATION'] = 'Datos generales';
 $mod_strings['LBL_STIC_PANEL_RECORD_DETAILS'] = 'Detalles del registro';
+$mod_strings['LBL_ACCOUNTS_SUBPANEL_TITLE'] = 'Organizaciones';
+$mod_strings['LBL_CONTACTS_SUBPANEL_TITLE'] = 'Personas';
+$mod_strings['LBL_LEADS_SUBPANEL_TITLE'] = 'Interesados';
+$mod_strings['LBL_OPPORTUNITY_SUBPANEL_TITLE'] = 'Subvenciones';
+$mod_strings['LBL_LIST_CONTACT'] = 'Personas';
+$mod_strings['LBL_LIST_LEAD'] = 'Interesados';
+$mod_strings['LBL_CONTACT_NAME'] = 'Persona:';
+$mod_strings['LBL_CREATE_CONTACT'] = 'Nueva Persona';
+$mod_strings['LBL_CREATE_LEAD'] = 'Nuevo Interesado';
+$mod_strings['LBL_CREATE_CONTACTS'] = 'Crear Persona' /*for 508 compliance fix*/;
+$mod_strings['LBL_CREATE_LEADS'] = 'Crear Interesado' /*for 508 compliance fix*/;
+$mod_strings['LBL_EMAILS_ACCOUNTS_REL'] = 'Emails:Organizaciones';
+$mod_strings['LBL_EMAILS_CONTACTS_REL'] = 'Emails:Personas';
+$mod_strings['LBL_EMAILS_LEADS_REL'] = 'Emails:Interesados';
+$mod_strings['LBL_EMAILS_OPPORTUNITIES_REL'] = 'Emails:Subvenciones';
+$mod_strings['LBL_INSERT_ACCOUNT_EMAIL'] = 'Insertar la dirección de correo desde una organización';
+$mod_strings['LBL_INSERT_CONTACT_EMAIL'] = 'Insertar la dirección de correo desde una persona';
+$mod_strings['LBL_INSERT_LEAD_EMAIL'] = 'Insertar la dirección de correo desde un interesado';
