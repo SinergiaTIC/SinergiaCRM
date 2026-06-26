5ed25f68c66485d480c8c4fa7e3dcc5e939944c3
Jaume Albaigès <45965117+jalbaiges@users.noreply.github.com>
2026-06-26 12:27:50 +0200

    Minor fixes

--- modules/stic_AWF_Forms/language/ca_ES.lang.php
+++ modules/stic_AWF_Forms/language/ca_ES.lang.php
@@ -480,13 +480,13 @@ $mod_strings = array (
 
   // -- DEFERRED ACTIONS --
   'LBL_PARAM_EXPIRATION_DAYS' => "Dies d'expiració",
-  'LBL_PARAM_EXPIRATION_DAYS_DESC' => "Nombre de dies abans que el l'acció diferida caduqui",
+  'LBL_PARAM_EXPIRATION_DAYS_DESC' => "Nombre de dies abans que l'acció diferida caduqui.",
   'LBL_PARAM_EXPIRED_TITLE' => 'Títol per enllaç caducat',
-  'LBL_PARAM_EXPIRED_TITLE_DESC' => "Títol de l'avís que es mostrarà quan s'accedeixi a l'enllaç caducat",
+  'LBL_PARAM_EXPIRED_TITLE_DESC' => "Títol de l'avís que es mostrarà quan s'accedeixi a l'enllaç caducat.",
   'LBL_PARAM_EXPIRED_TITLE_DEFAULT' => 'Enllaç caducat',
   'LBL_PARAM_EXPIRED_TEXT' => "Text de l'avís per enllaç caducat",
-  'LBL_PARAM_EXPIRED_TEXT_DESC' => "Text que es mostrarà quan s'accedeixi a l'enllaç caducat",
-  'LBL_PARAM_EXPIRED_TEXT_DEFAULT' => 'Aquest enllaç ha caducat per motius de seguretat',
+  'LBL_PARAM_EXPIRED_TEXT_DESC' => "Text que es mostrarà quan s'accedeixi a l'enllaç caducat.",
+  'LBL_PARAM_EXPIRED_TEXT_DEFAULT' => 'Aquest enllaç ha caducat per motius de seguretat.',
 
   // EmailConfirmationAction 
   'LBL_EMAIL_CONFIRMATION_ACTION_TITLE' => 'Confirma el correu electrònic', 
