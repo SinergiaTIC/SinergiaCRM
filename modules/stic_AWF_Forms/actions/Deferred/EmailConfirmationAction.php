81079f72097981a99025627348c03084c60188dc
jordiSTIC <jordi@sinergiacrm.org>
2026-06-29 07:34:00 +0000

    Set TicketId before DeferredContextData creation

--- modules/stic_AWF_Forms/actions/Deferred/EmailConfirmationAction.php
+++ modules/stic_AWF_Forms/actions/Deferred/EmailConfirmationAction.php
@@ -118,6 +118,9 @@ class EmailConfirmationAction extends DeferredBeanActionDefinition
 
         // Create a deferred ticket
         $ticket = BeanFactory::newBean('stic_AWF_Deferred_Tickets');
+        $ticket->id = create_guid(); // Set Id for the ContextData
+        $ticket->new_with_id = true;
+
         $ticket->name = 'Email Confirmation: ' . $emailAddress . ' - ' . date('Y-m-d H:i:s');
         $ticket->stic_awf_responses_id_c = $context->responseId;
         $ticket->token_hash = bin2hex(random_bytes(32)); 
