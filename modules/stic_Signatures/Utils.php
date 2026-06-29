efa507ad6212ca674b6646517ac75d7243a3059c
juanSTIC <juan@sinergiacrm.org>
2026-06-29 15:40:27 +0200

    Agregar soporte para función de análisis personalizada en stic_SignaturesUtils

--- modules/stic_Signatures/Utils.php
+++ modules/stic_Signatures/Utils.php
@@ -441,6 +441,12 @@ class stic_SignaturesUtils
         // The parse_template function requires an array of beans
         $beanArray = [$bean->module_dir => $bean->id];
 
+        // AAES - BFD - Custom Edit
+        if (file_exists('custom/modules/stic_Signatures/customParseTemplate.php')){
+            require_once 'custom/modules/stic_Signatures/customParseTemplate.php';
+        }
+        //END AAES - BFD - Custom Edit
+
         // First parse: Parse the template using the record's data
         $converted = templateParser::parse_template($text, $beanArray);
         $header = templateParser::parse_template($header, $beanArray);
