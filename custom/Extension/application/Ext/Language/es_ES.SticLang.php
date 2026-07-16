7d870ec2c45b20c4f7e40e08d194c77a07b39b3b
Jaume Albaigès <45965117+jalbaiges@users.noreply.github.com>
2026-07-16 20:13:26 +0200

    Minor fixes
    Removed TODO comments for undefined statuses in the Spanish language file.

--- custom/Extension/application/Ext/Language/es_ES.SticLang.php
+++ custom/Extension/application/Ext/Language/es_ES.SticLang.php
@@ -4067,10 +4067,6 @@ $app_list_strings['stic_awf_links_record_action_list']['skipped'] = 'Ignorado';
 $app_list_strings['stic_awf_links_record_action_list']['metadata'] = 'Tratado';
 $app_list_strings['stic_awf_links_record_action_list']['unchanged'] = 'Sin cambios';
 
-// TODO:
-// Define: stic_awf_deferred_tickets_status
-// Define: stic_awf_incoming_events_status
-
 // Conversaciones: Tipos
 $app_list_strings['stic_conversations_types_list'][''] = '';
 $app_list_strings['stic_conversations_types_list']['type1'] = 'Tipo 1';
