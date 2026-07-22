ad554a1f6ca330ca6b6e9125e28240d9228844ed
Jaume Albaigès <45965117+jalbaiges@users.noreply.github.com>
2026-07-22 14:14:18 +0200

    Minor fix

--- custom/Extension/application/Ext/Language/gl_ES.SticLang.php
+++ custom/Extension/application/Ext/Language/gl_ES.SticLang.php
@@ -3999,7 +3999,7 @@ $app_list_strings['stic_awf_forms_field_subtype_in_form_list']['rating_emoji'] =
 $app_list_strings['stic_awf_forms_field_subtype_in_form_list']['rating_lights'] = '🚦 Semáforo';
 $app_list_strings['stic_awf_forms_field_subtype_in_form_list']['rating_thumbs'] = '👍 Polgares';
 $app_list_strings['stic_awf_forms_field_subtype_in_form_list']['rating_nps']   = '🔟 Escala 0-10 (NPS)';
-$app_list_strings['stic_awf_forms_field_subtype_in_form_list']['file_upload'] = '📎 Adjuntar fichero';
+$app_list_strings['stic_awf_forms_field_subtype_in_form_list']['file_upload'] = '📎 Fichero adjunto';
 
 // Advanced Web Forms: configuration->data_block->field->value_type
 $app_list_strings['stic_awf_forms_field_value_type_list']['editable'] = 'Editable';
