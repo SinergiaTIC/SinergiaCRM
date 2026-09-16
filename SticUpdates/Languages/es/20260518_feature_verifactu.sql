9daee855ffd5409e9b34f2280ea8045532a97a2a
juanSTIC <juan@sinergiacrm.org>
2026-09-16 19:18:06 +0200

    NO test mode #5

--- SticUpdates/Languages/es/20260518_feature_verifactu.sql
+++ SticUpdates/Languages/es/20260518_feature_verifactu.sql
@@ -13,6 +13,5 @@
 
 REPLACE INTO `stic_settings` (`id`, `date_entered`, `date_modified`, `modified_user_id`, `created_by`, `deleted`, `assigned_user_id`, `type`, `name`, `value`, `description`)
 VALUES
-('f47ac10b-58cc-4372-a567-0e02b2c3d480', NOW(), NOW(), '1', '1', 0, '1', 'VERIFACTU', 'VERIFACTU_TEST', 1, 'Indica el modo de trabajo (0 = Real, 1 = Test).'),
 ('f47ac10b-58cc-4372-a567-0e02b2c3d482', NOW(), NOW(), '1', '1', 0, '1', 'VERIFACTU', 'VERIFACTU_TAX_TYPE', '01', 'Tipo de impuesto por defecto (01=IVA, 02=IPSI, 03=IGIC).'),
 ('f47ac10b-58cc-4372-a567-0e02b2c3d481', NOW(), NOW(), '1', '1', 0, '1', 'VERIFACTU', 'VERIFACTU_ACTIVATED', 0, 'Activa o desactiva la integración con Verifactu AEAT (0 = modo legacy sin envío, 1 = Verifactu activo con envío automático a AEAT).');
